7b82bfb477efe5ddec5247aff78e7117e77e5558
aothms <t.krijnen@gmail.com>
2015-10-05 13:08:24 +0200

    Update log messages in parser

--- src/ifcparse/IfcParse.cpp
+++ src/ifcparse/IfcParse.cpp
@@ -969,7 +969,7 @@ bool IfcFile::Init(IfcParse::IfcSpfStream* s) {
 					const std::string guid = ifc_root->GlobalId();
 					if ( byguid.find(guid) != byguid.end() ) {
 						std::stringstream ss;
-						ss << "Overwriting entity with guid " << guid;
+						ss << "Instance encountered with non-unique GlobalId " << guid;
 						Logger::Message(Logger::LOG_WARNING,ss.str());
 					}
 					byguid[guid] = ifc_root;
@@ -991,7 +991,7 @@ bool IfcFile::Init(IfcParse::IfcSpfStream* s) {
 			
 			if ( byid.find(currentId) != byid.end() ) {
 				std::stringstream ss;
-				ss << "Overwriting entity with id " << currentId;
+				ss << "Overwriting instance with name #" << currentId;
 				Logger::Message(Logger::LOG_WARNING,ss.str());
 			}
 			byid[currentId] = entity;
