766d45e0cbd0796a518cc32ad12574e94c3ec7b2
aothms <t.krijnen@gmail.com>
2016-02-19 14:25:56 +0100

    Add missing include

---
+++ src/ifcparse/Hdf5Settings.h
@@ -0,0 +1,57 @@
+/********************************************************************************
+ *                                                                              *
+ * This file is part of IfcOpenShell.                                           *
+ *                                                                              *
+ * IfcOpenShell is free software: you can redistribute it and/or modify         *
+ * it under the terms of the Lesser GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3.0 of the License, or          *
+ * (at your option) any later version.                                          *
+ *                                                                              *
+ * IfcOpenShell is distributed in the hope that it will be useful,              *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 *
+ * Lesser GNU General Public License for more details.                          *
+ *                                                                              *
+ * You should have received a copy of the Lesser GNU General Public License     *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.         *
+ *                                                                              *
+ ********************************************************************************/
+
+#ifndef HDF5SETTINGS_H
+#define HDF5SETTINGS_H
+
+namespace IfcParse {
+	
+	struct Hdf5Settings {
+	private:
+		bool compress_, fix_cartesian_point_, fix_global_id_, instantiate_select_, instantiate_inverse_;
+		unsigned int chunk_size_;
+	public:
+		Hdf5Settings()
+			: compress_(false)
+			, fix_cartesian_point_(false)
+			, fix_global_id_(false)
+			, instantiate_select_(false)
+			, instantiate_inverse_(false)
+			, chunk_size_(false)
+		{}
+
+		bool compress() const { return compress_; }
+		bool fix_cartesian_point() const { return fix_cartesian_point_; }
+		bool fix_global_id() const { return fix_global_id_; }
+		bool instantiate_select() const { return instantiate_select_; }
+		bool instantiate_inverse() const { return instantiate_inverse_; }
+
+		bool& compress() { return compress_; }
+		bool& fix_cartesian_point() { return fix_cartesian_point_; }
+		bool& fix_global_id() { return fix_global_id_; }
+		bool& instantiate_select() { return instantiate_select_; }
+		bool& instantiate_inverse() { return instantiate_inverse_; }
+
+		unsigned int chunk_size() const { return chunk_size_; }
+		unsigned int& chunk_size() { return chunk_size_; }
+	};
+
+}
+
+#endif
