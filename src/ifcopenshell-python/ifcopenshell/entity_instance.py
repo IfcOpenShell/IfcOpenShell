4faa66f9b824f921ebb1d0354bc45f6c660732e9
Thomas Krijnen <t.krijnen@gmail.com>
2017-03-09 12:04:07 +0100

    Fix for python 2.7

--- src/ifcopenshell-python/ifcopenshell/entity_instance.py
+++ src/ifcopenshell-python/ifcopenshell/entity_instance.py
@@ -16,6 +16,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.        #
 #                                                                             #
 ###############################################################################
+from __future__ import print_function
 
 import functools
 import numbers
