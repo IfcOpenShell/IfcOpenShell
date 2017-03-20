8090bb4c0070650b8d5a30313392b0b448846656
Ken Arroyo Ohori <g.a.k.arroyoohori@tudelft.nl>
2017-03-20 16:35:14 -0600

    Shapes with styles in separate file

--- src/ifcgeom/kernels/cgal/CgalConversionFunctions.cpp
+++ src/ifcgeom/kernels/cgal/CgalConversionFunctions.cpp
@@ -1,27 +1,6 @@
 #include "../../../ifcparse/IfcParse.h"
 
 #include "CgalKernel.h"
-#include "CgalConversionResult.h"
-
-bool IfcGeom::CgalKernel::convert(const IfcSchema::IfcRepresentation* l, ConversionResults& shapes) {
-	IfcSchema::IfcRepresentationItem::list::ptr items = l->Items();
-	bool part_succes = false;
-	if (items->size()) {
-		for (IfcSchema::IfcRepresentationItem::list::it it = items->begin(); it != items->end(); ++it) {
-			IfcSchema::IfcRepresentationItem* representation_item = *it;
-			if (shape_type(representation_item) == ST_SHAPELIST) {
-				part_succes |= convert_shapes(*it, shapes);
-			} else {
-				cgal_shape_t s;
-				if (convert_shape(representation_item, s)) {
-					shapes.push_back(ConversionResult(new CgalShape(s), get_style(representation_item)));
-					part_succes |= true;
-				}
-			}
-		}
-	}
-	return part_succes;
-}
 
 bool IfcGeom::CgalKernel::convert(const IfcSchema::IfcCartesianPoint* l, cgal_point_t& point) {
   std::vector<double> xyz = l->Coordinates();
