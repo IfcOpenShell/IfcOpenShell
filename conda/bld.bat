caa6056f01341e303e1956255397d1fa0b35545a
Thomas Krijnen <t.krijnen@gmail.com>
2018-08-14 17:09:00 +0200

    Conda bld.bat python version

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,5 +1,9 @@
 mkdir build && cd build
 
+REM Remove dot from PY_VER for use in library name
+REM From https://github.com/tpaviot/pythonocc-core/blob/master/ci/conda/bld.bat
+set MY_PY_VER=%PY_VER:.=%
+
 cmake -G "NMake Makefiles" ^
  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
  -DCMAKE_BUILD_TYPE=Release ^
@@ -7,7 +11,7 @@ cmake -G "NMake Makefiles" ^
  -DCMAKE_SYSTEM_PREFIX_PATH="%LIBRARY_PREFIX%" ^
  -DPYTHON_EXECUTABLE="%PYTHON%" ^
  -DPYTHON_INCLUDE_DIR="%PREFIX%"/include ^
- -DPYTHON_LIBRARY="%PREFIX%"/libs/python%PY_MAJOR%%PY_MINOR%.lib ^
+ -DPYTHON_LIBRARY="%PREFIX%"/libs/python%MY_PY_VER%.lib ^
  -DBOOST_LIBRARYDIR="%LIBRARY_PREFIX%\lib" ^
  -DBOOST_INCLUDEDIR="%LIBRARY_PREFIX%\include" ^
  -DOCC_INCLUDE_DIR="%LIBRARY_PREFIX%\include\oce" ^
