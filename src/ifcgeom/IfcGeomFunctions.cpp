2cfe74ce098759a8891850b84981b910ccac7951
Otso Alho <otso.alho@gmail.com>
2019-05-24 14:57:29 +0300

    reverse styles also when reversing surfaces

--- src/ifcgeom/IfcGeomFunctions.cpp
+++ src/ifcgeom/IfcGeomFunctions.cpp
@@ -2027,6 +2027,7 @@ bool IfcGeom::Kernel::convert_layerset(const IfcSchema::IfcProduct* product, std
 	}
 
 	if (positive) {
+		std::reverse(styles.begin(), styles.end());
 		std::reverse(surfaces.begin(), surfaces.end());
 	}
 
