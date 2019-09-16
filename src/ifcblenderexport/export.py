3ff3ce46ad139364ed4ca372b1f9a1f110f8a2f3
Dion Moult <dion@thinkmoult.com>
2019-09-16 16:53:34 +1000

    Add support for footprint and reference subcontexts

--- src/ifcblenderexport/export.py
+++ src/ifcblenderexport/export.py
@@ -1115,7 +1115,7 @@ class IfcExportSettings:
     def __init__(self):
         self.has_representations = True
         self.has_quantities = True
-        self.subcontexts = ['Body', 'Axis', 'Clearance']
+        self.subcontexts = ['Axis', 'FootPrint', 'Reference', 'Body', 'Clearance']
 
 print('# Starting export')
 start = time.time()
