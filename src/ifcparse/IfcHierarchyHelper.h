05a5c3c3f9a82ff2a2d74d0b0704b10478bdd3eb
Adrien Scho <adrien.scholastique@gmail.com>
2019-12-16 01:32:14 +0100

    fix: #734 - No XML serializer registered for IFC4X1

--- src/ifcparse/IfcHierarchyHelper.h
+++ src/ifcparse/IfcHierarchyHelper.h
@@ -79,6 +79,13 @@ namespace {
 		return t->RelatedElements()->generalize();
 	}
 
+	IfcEntityList::ptr get_children_of_relation(Ifc4x1::IfcRelContainedInSpatialStructure* t) {
+		return t->RelatedElements()->generalize();
+	}
+
+	IfcEntityList::ptr get_children_of_relation(Ifc4x2::IfcRelContainedInSpatialStructure* t) {
+		return t->RelatedElements()->generalize();
+	}
 	void set_children_of_relation(IfcUtil::IfcBaseClass* t, IfcEntityList::ptr& cs) {
 		IfcWrite::IfcWriteArgument* attr = new IfcWrite::IfcWriteArgument;
 		attr->set(cs);
@@ -95,8 +102,15 @@ namespace {
 	void set_children_of_relation(Ifc4::IfcRelContainedInSpatialStructure* t, IfcEntityList::ptr& cs) {
 		t->setRelatedElements(cs->as<Ifc4::IfcProduct>());
 	}
-}
+	
+	void set_children_of_relation(Ifc4x1::IfcRelContainedInSpatialStructure* t, IfcEntityList::ptr& cs) {
+		t->setRelatedElements(cs->as<Ifc4x1::IfcProduct>());
+	}
 
+	void set_children_of_relation(Ifc4x2::IfcRelContainedInSpatialStructure* t, IfcEntityList::ptr& cs) {
+		t->setRelatedElements(cs->as<Ifc4x2::IfcProduct>());
+	}
+}
 template <typename Schema>
 class IFC_PARSE_API IfcHierarchyHelper : public IfcParse::IfcFile {
 public:
