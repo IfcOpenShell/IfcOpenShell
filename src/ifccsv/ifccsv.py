bd3f363604026d5920af4b7b3b6670cab4e70886
Dion Moult <dion@thinkmoult.com>
2020-03-16 14:54:45 +1100

    Add icon and embed common.lark for packaging IFCCSV

--- src/ifccsv/ifccsv.py
+++ src/ifccsv/ifccsv.py
@@ -35,10 +35,32 @@ class IfcSelectorParser():
                     morethan: ">"
                     lessthan: "<"
 
-                    %import common.WORD   // imports from terminal library
-                    %import common.ESCAPED_STRING
-                    %import common.WS
-                    %ignore WS           // Disregard spaces in text
+                    // Embed common.lark for packaging
+                    DIGIT: "0".."9"
+                    HEXDIGIT: "a".."f"|"A".."F"|DIGIT
+                    INT: DIGIT+
+                    SIGNED_INT: ["+"|"-"] INT
+                    DECIMAL: INT "." INT? | "." INT
+                    _EXP: ("e"|"E") SIGNED_INT
+                    FLOAT: INT _EXP | DECIMAL _EXP?
+                    SIGNED_FLOAT: ["+"|"-"] FLOAT
+                    NUMBER: FLOAT | INT
+                    SIGNED_NUMBER: ["+"|"-"] NUMBER
+                    _STRING_INNER: /.*?/
+                    _STRING_ESC_INNER: _STRING_INNER /(?<!\\\\)(\\\\\\\\)*?/
+                    ESCAPED_STRING : "\\"" _STRING_ESC_INNER "\\""
+                    LCASE_LETTER: "a".."z"
+                    UCASE_LETTER: "A".."Z"
+                    LETTER: UCASE_LETTER | LCASE_LETTER
+                    WORD: LETTER+
+                    CNAME: ("_"|LETTER) ("_"|LETTER|DIGIT)*
+                    WS_INLINE: (" "|/\\t/)+
+                    WS: /[ \\t\\f\\r\\n]/+
+                    CR : /\\r/
+                    LF : /\\n/
+                    NEWLINE: (CR? LF)+
+
+                    %ignore WS // Disregard spaces in text
                  ''')
 
         start = l.parse(self.query)
