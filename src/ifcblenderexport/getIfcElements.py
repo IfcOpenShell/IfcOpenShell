1d7865c55843d0381d59129354925eeb2c2ab418
Dion Moult <dion@thinkmoult.com>
2020-05-11 15:17:39 +1000

    Add support for IfcAnnotation elements

--- src/ifcblenderexport/getIfcElements.py
+++ src/ifcblenderexport/getIfcElements.py
@@ -156,7 +156,8 @@ filename_filters = {
     'IfcMaterialDefinition_IFC4.json': ['IfcMaterialDefinition'],
     'IfcParameterizedProfileDef_IFC4.json': ['IfcParameterizedProfileDef'],
     'IfcBoundaryCondition_IFC4.json': ['IfcBoundaryCondition'],
-    'IfcElementType_IFC4.json': ['IfcElementType']
+    'IfcElementType_IFC4.json': ['IfcElementType'],
+    'IfcAnnotation_IFC4.json': ['IfcAnnotation']
 }
 
 for filename, filters in filename_filters.items():
