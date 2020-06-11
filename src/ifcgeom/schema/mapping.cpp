2c680a1a3b523fba8958f84144cac1578086d691
Thomas Krijnen <t.krijnen@gmail.com>
2020-06-11 20:49:53 +0200

    fix radius

--- src/ifcgeom/schema/mapping.cpp
+++ src/ifcgeom/schema/mapping.cpp
@@ -1765,7 +1765,7 @@ taxonomy::item* mapping::map_impl(const IfcSchema::IfcTrimmedCurve* inst) {
 taxonomy::item* mapping::map_impl(const IfcSchema::IfcCircle* inst) {
 	auto c = new taxonomy::circle;
 	c->matrix = as<taxonomy::matrix4>(map(inst->Position()));
-	c->radius = inst->Radius();
+	c->radius = inst->Radius() * length_unit_;
 	return c;
 }
 
