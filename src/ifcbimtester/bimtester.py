635cb2f22c7f5aba01ae2d656a75b0b7085ef659
Dion Moult <dion@thinkmoult.com>
2020-07-23 13:42:41 +1000

    Fix bug where COBie convert fails on empty names of related objects for assemblies

--- src/ifcbimtester/bimtester.py
+++ src/ifcbimtester/bimtester.py
@@ -168,41 +168,42 @@ class TestPurger:
             return False
 
 
-parser = argparse.ArgumentParser(
-    description='Runs unit tests for BIM data')
-parser.add_argument(
-    '-p',
-    '--purge',
-    action='store_true',
-    help='Purge tests of deleted elements')
-parser.add_argument(
-    '-r',
-    '--report',
-    action='store_true',
-    help='Generate a HTML report')
-parser.add_argument(
-    '-c',
-    '--console',
-    action='store_true',
-    help='Show results in the console')
-parser.add_argument(
-    '-f',
-    '--feature',
-    type=str,
-    help='Specify a feature file to test',
-    default='')
-parser.add_argument(
-    '-a',
-    '--advanced-arguments',
-    type=str,
-    help='Specify your own arguments to Python\'s Behave',
-    default='')
-args = vars(parser.parse_args())
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Runs unit tests for BIM data')
+    parser.add_argument(
+        '-p',
+        '--purge',
+        action='store_true',
+        help='Purge tests of deleted elements')
+    parser.add_argument(
+        '-r',
+        '--report',
+        action='store_true',
+        help='Generate a HTML report')
+    parser.add_argument(
+        '-c',
+        '--console',
+        action='store_true',
+        help='Show results in the console')
+    parser.add_argument(
+        '-f',
+        '--feature',
+        type=str,
+        help='Specify a feature file to test',
+        default='')
+    parser.add_argument(
+        '-a',
+        '--advanced-arguments',
+        type=str,
+        help='Specify your own arguments to Python\'s Behave',
+        default='')
+    args = vars(parser.parse_args())
 
-if args['purge']:
-    TestPurger().purge()
-elif args['report']:
-    generate_report()
-else:
-    run_tests(args)
-print('# All tasks are complete :-)')
+    if args['purge']:
+        TestPurger().purge()
+    elif args['report']:
+        generate_report()
+    else:
+        run_tests(args)
+    print('# All tasks are complete :-)')
