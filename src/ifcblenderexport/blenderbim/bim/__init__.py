6416d1307b6e9b26eef90de466b7dc0a1610d29e
Dion Moult <dion@thinkmoult.com>
2020-08-08 13:19:56 +1000

    Split object psets and qto into their own panels for usability

--- src/ifcblenderexport/blenderbim/bim/__init__.py
+++ src/ifcblenderexport/blenderbim/bim/__init__.py
@@ -234,6 +234,8 @@ if bpy is not None:
         ui.BIM_PT_material,
         ui.BIM_PT_mesh,
         ui.BIM_PT_object,
+        ui.BIM_PT_object_psets,
+        ui.BIM_PT_object_qto,
         ui.BIM_PT_representations,
         ui.BIM_PT_classification_references,
         ui.BIM_PT_documents,
