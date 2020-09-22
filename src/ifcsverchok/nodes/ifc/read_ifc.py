c01fd01c9244b4bf837d75bcb2cbbf0f541c85b7
Dion Moult <dion@thinkmoult.com>
2020-09-22 21:22:10 +1000

    Implement read, create, and write for IfcSverchok

---
+++ src/ifcsverchok/nodes/ifc/read_ifc.py
@@ -0,0 +1,31 @@
+import bpy
+import ifcopenshell
+import ifcsverchok.helper
+from bpy.props import StringProperty
+from sverchok.node_tree import SverchCustomTreeNode
+from sverchok.data_structure import updateNode
+
+
+class SvReadIfc(bpy.types.Node, SverchCustomTreeNode, ifcsverchok.helper.SvIfcCore):
+    bl_idname = 'SvReadIfc'
+    bl_label = 'Read IFC'
+    path: StringProperty(name='path', update=updateNode)
+
+    def sv_init(self, context):
+        self.inputs.new('SvStringsSocket', 'path').prop_name = 'path'
+        self.outputs.new('SvVerticesSocket', 'file')
+
+    def process(self):
+        self.sv_input_names = ['path']
+        super().process()
+
+    def process_ifc(self, path):
+        guid = ifcopenshell.guid.new()
+        ifcsverchok.helper.ifc_files[guid] = ifcopenshell.open(path)
+        self.outputs['file'].sv_set([[ifcsverchok.helper.ifc_files[guid]]])
+
+def register():
+    bpy.utils.register_class(SvReadIfc)
+
+def unregister():
+    bpy.utils.unregister_class(SvReadIfc)
