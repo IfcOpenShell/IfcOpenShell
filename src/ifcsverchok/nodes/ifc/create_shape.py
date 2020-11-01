2c9d6a47f4d24a923069775206bf734feeb14830
htlcnn <hoangthanhlong@gmail.com>
2020-11-01 19:24:01 +0700

    black ifcsverchok

--- src/ifcsverchok/nodes/ifc/create_shape.py
+++ src/ifcsverchok/nodes/ifc/create_shape.py
@@ -11,54 +11,55 @@ from sverchok.data_structure import updateNode
 class SvIfcCreateShapeRefresh(bpy.types.Operator):
     bl_idname = "node.sv_ifc_create_shape_refresh"
     bl_label = "IFC Create Shape Refresh"
-    bl_options = {'UNDO'}
+    bl_options = {"UNDO"}
 
-    idtree: StringProperty(default='')
-    idname: StringProperty(default='')
+    idtree: StringProperty(default="")
+    idname: StringProperty(default="")
 
     def execute(self, context):
         node = bpy.data.node_groups[self.idtree].nodes[self.idname]
         node.process()
-        return {'FINISHED'}
+        return {"FINISHED"}
 
 
 class SvIfcCreateShape(bpy.types.Node, SverchCustomTreeNode, ifcsverchok.helper.SvIfcCore):
-    bl_idname = 'SvIfcCreateShape'
-    bl_label = 'IFC Create Shape'
-    file: StringProperty(name='file', update=updateNode)
-    entity: StringProperty(name='entity', update=updateNode)
+    bl_idname = "SvIfcCreateShape"
+    bl_label = "IFC Create Shape"
+    file: StringProperty(name="file", update=updateNode)
+    entity: StringProperty(name="entity", update=updateNode)
 
     def sv_init(self, context):
-        self.inputs.new('SvStringsSocket', 'file').prop_name = 'file'
-        self.inputs.new('SvStringsSocket', 'entity').prop_name = 'entity'
+        self.inputs.new("SvStringsSocket", "file").prop_name = "file"
+        self.inputs.new("SvStringsSocket", "entity").prop_name = "entity"
 
     def draw_buttons(self, context, layout):
-        self.wrapper_tracked_ui_draw_op(layout, 'node.sv_ifc_create_shape_refresh', icon='FILE_REFRESH', text='Refresh')
+        self.wrapper_tracked_ui_draw_op(layout, "node.sv_ifc_create_shape_refresh", icon="FILE_REFRESH", text="Refresh")
 
     def process(self):
-        self.sv_input_names = ['file', 'entity']
+        self.sv_input_names = ["file", "entity"]
         super().process()
 
     def process_ifc(self, file, entity):
         try:
-            if not entity.is_a('IfcProduct'):
+            if not entity.is_a("IfcProduct"):
                 return
-            logger = logging.getLogger('ImportIFC')
-            self.ifc_import_settings = blenderbim.bim.import_ifc.IfcImportSettings.factory(bpy.context, '', logger)
+            logger = logging.getLogger("ImportIFC")
+            self.ifc_import_settings = blenderbim.bim.import_ifc.IfcImportSettings.factory(bpy.context, "", logger)
             settings = ifcopenshell.geom.settings()
             shape = ifcopenshell.geom.create_shape(settings, entity)
             ifc_importer = blenderbim.bim.import_ifc.IfcImporter(self.ifc_import_settings)
             ifc_importer.file = file
             mesh = ifc_importer.create_mesh(entity, shape)
-            obj = bpy.data.objects.new('IFC Element', mesh)
+            obj = bpy.data.objects.new("IFC Element", mesh)
             bpy.context.scene.collection.objects.link(obj)
         except:
-            print('Entity could not be converted into a shape', entity)
+            print("Entity could not be converted into a shape", entity)
 
 
 def register():
     bpy.utils.register_class(SvIfcCreateShapeRefresh)
     bpy.utils.register_class(SvIfcCreateShape)
+
 
 def unregister():
     bpy.utils.unregister_class(SvIfcCreateShape)
--- src/ifcsverchok/nodes/ifc/get_property.py
+++ src/ifcsverchok/nodes/ifc/get_property.py
@@ -8,23 +8,23 @@ from sverchok.data_structure import updateNode
 
 
 class SvIfcGetProperty(bpy.types.Node, SverchCustomTreeNode, ifcsverchok.helper.SvIfcCore):
-    bl_idname = 'SvIfcGetProperty'
-    bl_label = 'IFC Get Property'
-    entity: StringProperty(name='entity', update=updateNode)
-    pset_name: StringProperty(name='pset_name', update=updateNode)
-    prop_name: StringProperty(name='prop_name', update=updateNode)
+    bl_idname = "SvIfcGetProperty"
+    bl_label = "IFC Get Property"
+    entity: StringProperty(name="entity", update=updateNode)
+    pset_name: StringProperty(name="pset_name", update=updateNode)
+    prop_name: StringProperty(name="prop_name", update=updateNode)
 
     def sv_init(self, context):
-        self.inputs.new('SvStringsSocket', 'entity').prop_name = 'entity'
-        self.inputs.new('SvStringsSocket', 'pset_name').prop_name = 'pset_name'
-        self.inputs.new('SvStringsSocket', 'prop_name').prop_name = 'prop_name'
-        self.outputs.new('SvStringsSocket', 'value')
+        self.inputs.new("SvStringsSocket", "entity").prop_name = "entity"
+        self.inputs.new("SvStringsSocket", "pset_name").prop_name = "pset_name"
+        self.inputs.new("SvStringsSocket", "prop_name").prop_name = "prop_name"
+        self.outputs.new("SvStringsSocket", "value")
 
     def process(self):
-        self.sv_input_names = ['entity', 'pset_name', 'prop_name']
+        self.sv_input_names = ["entity", "pset_name", "prop_name"]
         self.value_out = []
         super().process()
-        self.outputs['value'].sv_set(self.value_out)
+        self.outputs["value"].sv_set(self.value_out)
 
     def process_ifc(self, entity, pset_name, prop_name):
         try:
@@ -35,6 +35,7 @@ class SvIfcGetProperty(bpy.types.Node, SverchCustomTreeNode, ifcsverchok.helper.
 
 def register():
     bpy.utils.register_class(SvIfcGetProperty)
+
 
 def unregister():
     bpy.utils.unregister_class(SvIfcGetProperty)
