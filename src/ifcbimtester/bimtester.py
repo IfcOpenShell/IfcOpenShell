85fe2b53237559c495bfafa36dd4463e1b48a122
htlcnn <hoangthanhlong@gmail.com>
2020-11-01 19:22:49 +0700

    black ifcbimtester

--- src/ifcbimtester/bimtester.py
+++ src/ifcbimtester/bimtester.py
@@ -5,7 +5,7 @@
 # $ pyinstaller --onefile --clean --icon=icon.ico --add-data "features;features" bimtester.py`
 
 from behave.__main__ import main as behave_main
-import behave.formatter.pretty # Needed for pyinstaller to package it
+import behave.formatter.pretty  # Needed for pyinstaller to package it
 import ifcopenshell
 import pystache
 import os
@@ -32,103 +32,105 @@ def get_resource_path(relative_path):
 
 def run_tests(args):
     if not get_features(args):
-        print('No features could be found to check.')
+        print("No features could be found to check.")
         return False
-    behave_args = [get_resource_path('features')]
-    if args['advanced_arguments']:
-        behave_args.extend(args['advanced_arguments'].split())
-    elif not args['console']:
-        behave_args.extend(['--format', 'json.pretty', '--outfile', 'report/report.json'])
+    behave_args = [get_resource_path("features")]
+    if args["advanced_arguments"]:
+        behave_args.extend(args["advanced_arguments"].split())
+    elif not args["console"]:
+        behave_args.extend(["--format", "json.pretty", "--outfile", "report/report.json"])
     behave_main(behave_args)
-    print('# All tests are finished.')
+    print("# All tests are finished.")
     return True
 
 
 def get_features(args):
     current_path = os.path.abspath(".")
-    features_dir = get_resource_path('features')
+    features_dir = get_resource_path("features")
     for f in os.listdir(features_dir):
-        if f.endswith('.feature'):
+        if f.endswith(".feature"):
             os.remove(os.path.join(features_dir, f))
-    if args['feature']:
-        shutil.copyfile(args['feature'], os.path.join(
-            get_resource_path('features'),
-            os.path.basename(args['feature'])))
+    if args["feature"]:
+        shutil.copyfile(args["feature"], os.path.join(get_resource_path("features"), os.path.basename(args["feature"])))
         return True
-    if os.path.exists('features'):
-        shutil.copytree('features', get_resource_path('features'))
+    if os.path.exists("features"):
+        shutil.copytree("features", get_resource_path("features"))
         return True
     has_features = False
-    for f in os.listdir('.'):
-        if not f.endswith('.feature'):
+    for f in os.listdir("."):
+        if not f.endswith(".feature"):
             continue
-        if args['feature'] and args['feature'] != f:
+        if args["feature"] and args["feature"] != f:
             continue
         has_features = True
-        shutil.copyfile(f, os.path.join(
-            get_resource_path('features'),
-            os.path.basename(f)))
+        shutil.copyfile(f, os.path.join(get_resource_path("features"), os.path.basename(f)))
     return has_features
 
 
 def generate_report():
-    print('# Generating HTML reports now.')
-    if not os.path.exists('report'):
-        os.mkdir('report')
-    report_path = 'report/report.json'
+    print("# Generating HTML reports now.")
+    if not os.path.exists("report"):
+        os.mkdir("report")
+    report_path = "report/report.json"
     if not os.path.exists(report_path):
-        return print('No report data was found.')
+        return print("No report data was found.")
     report = json.loads(open(report_path).read())
     for feature in report:
-        file_name = os.path.basename(feature['location']).split(':')[0]
+        file_name = os.path.basename(feature["location"]).split(":")[0]
         data = {
-            'file_name': file_name,
-            'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            'name': feature['name'],
-            'description': feature['description'],
-            'is_success': feature['status'] == 'passed',
-            'scenarios': []
-            }
-        for scenario in feature['elements']:
+            "file_name": file_name,
+            "time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "name": feature["name"],
+            "description": feature["description"],
+            "is_success": feature["status"] == "passed",
+            "scenarios": [],
+        }
+        for scenario in feature["elements"]:
             steps = []
             total_duration = 0
-            for step in scenario['steps']:
-                if 'result' in step:
-                    total_duration += step['result']['duration']
-                name = step['name']
-                if 'match' in step and 'arguments' in step['match']:
-                    for a in step['match']['arguments']:
-                        name = name.replace(a['value'], '<b>' + a['value'] + '</b>')
-                if 'result' not in step or step['result']['status'] == 'undefined':
-                    step['result'] = {}
-                    step['result']['status'] = 'undefined'
-                    step['result']['duration'] = 0
-                    step['result']['error_message'] = 'This requirement has not yet been specified.'
-                steps.append({
-                    'name': name,
-                    'time': round(step['result']['duration'], 2),
-                    'is_success': step['result']['status'] == 'passed',
-                    'is_unspecified': 'result' not in step or step['result']['status'] == 'undefined',
-                    'error_message': None if step['result']['status'] == 'passed' else step['result']['error_message']
-                })
-            total_passes = len([s for s in steps if s['is_success'] == True])
+            for step in scenario["steps"]:
+                if "result" in step:
+                    total_duration += step["result"]["duration"]
+                name = step["name"]
+                if "match" in step and "arguments" in step["match"]:
+                    for a in step["match"]["arguments"]:
+                        name = name.replace(a["value"], "<b>" + a["value"] + "</b>")
+                if "result" not in step or step["result"]["status"] == "undefined":
+                    step["result"] = {}
+                    step["result"]["status"] = "undefined"
+                    step["result"]["duration"] = 0
+                    step["result"]["error_message"] = "This requirement has not yet been specified."
+                steps.append(
+                    {
+                        "name": name,
+                        "time": round(step["result"]["duration"], 2),
+                        "is_success": step["result"]["status"] == "passed",
+                        "is_unspecified": "result" not in step or step["result"]["status"] == "undefined",
+                        "error_message": None
+                        if step["result"]["status"] == "passed"
+                        else step["result"]["error_message"],
+                    }
+                )
+            total_passes = len([s for s in steps if s["is_success"] == True])
             total_steps = len(steps)
             pass_rate = round((total_passes / total_steps) * 100)
-            data['scenarios'].append({
-                'name': scenario['name'],
-                'is_success': scenario['status'] == 'passed',
-                'time': round(total_duration, 2),
-                'steps': steps,
-                'total_passes': total_passes,
-                'total_steps': total_steps,
-                'pass_rate': pass_rate
-                })
-        data['total_passes'] = sum([s['total_passes'] for s in data['scenarios']])
-        data['total_steps'] = sum([s['total_steps'] for s in data['scenarios']])
-        data['pass_rate'] = round((data['total_passes'] / data['total_steps']) * 100)
+            data["scenarios"].append(
+                {
+                    "name": scenario["name"],
+                    "is_success": scenario["status"] == "passed",
+                    "time": round(total_duration, 2),
+                    "steps": steps,
+                    "total_passes": total_passes,
+                    "total_steps": total_steps,
+                    "pass_rate": pass_rate,
+                }
+            )
+        data["total_passes"] = sum([s["total_passes"] for s in data["scenarios"]])
+        data["total_steps"] = sum([s["total_steps"] for s in data["scenarios"]])
+        data["pass_rate"] = round((data["total_passes"] / data["total_steps"]) * 100)
 
-        with open('report/{}.html'.format(file_name), 'w') as out:
-            with open(get_resource_path('features/template.html')) as template:
+        with open("report/{}.html".format(file_name), "w") as out:
+            with open(get_resource_path("features/template.html")) as template:
                 out.write(pystache.render(template.read(), data))
 
 
@@ -138,35 +140,35 @@ class TestPurger:
 
     def purge(self):
         filenames = []
-        if os.path.exists('features'):
-            for filename in Path('features/').glob('*.feature'):
+        if os.path.exists("features"):
+            for filename in Path("features/").glob("*.feature"):
                 filenames.append(filename)
-        for f in os.listdir('.'):
-            if f.endswith('.feature'):
+        for f in os.listdir("."):
+            if f.endswith(".feature"):
                 filenames.append(f)
 
         for filename in filenames:
-            with open(filename, 'r') as feature_file:
+            with open(filename, "r") as feature_file:
                 old_file = feature_file.readlines()
-            with open(filename, 'w') as new_file:
+            with open(filename, "w") as new_file:
                 for line in old_file:
                     is_purged = False
                     if 'The IFC file "' in line and '" must be provided' in line:
                         filename = line.split('"')[1]
-                        print('Loading file {} ...'.format(filename))
+                        print("Loading file {} ...".format(filename))
                         self.file = ifcopenshell.open(filename)
-                    if line.strip()[0:2] == '* ':
+                    if line.strip()[0:2] == "* ":
                         words = line.strip().split()
                         for word in words:
                             if self.is_a_global_id(word):
                                 if not self.does_global_id_exist(word):
-                                    print('Test for {} purged ...'.format(word))
+                                    print("Test for {} purged ...".format(word))
                                     is_purged = True
                     if not is_purged:
                         new_file.write(line)
 
     def is_a_global_id(self, word):
-        return word[0] in ['0', '1', '2', '3'] and len(word) == 22
+        return word[0] in ["0", "1", "2", "3"] and len(word) == 22
 
     def does_global_id_exist(self, global_id):
         try:
@@ -176,42 +178,21 @@ class TestPurger:
             return False
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Runs unit tests for BIM data')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Runs unit tests for BIM data")
+    parser.add_argument("-p", "--purge", action="store_true", help="Purge tests of deleted elements")
+    parser.add_argument("-r", "--report", action="store_true", help="Generate a HTML report")
+    parser.add_argument("-c", "--console", action="store_true", help="Show results in the console")
+    parser.add_argument("-f", "--feature", type=str, help="Specify a feature file to test", default="")
     parser.add_argument(
-        '-p',
-        '--purge',
-        action='store_true',
-        help='Purge tests of deleted elements')
-    parser.add_argument(
-        '-r',
-        '--report',
-        action='store_true',
-        help='Generate a HTML report')
-    parser.add_argument(
-        '-c',
-        '--console',
-        action='store_true',
-        help='Show results in the console')
-    parser.add_argument(
-        '-f',
-        '--feature',
-        type=str,
-        help='Specify a feature file to test',
-        default='')
-    parser.add_argument(
-        '-a',
-        '--advanced-arguments',
-        type=str,
-        help='Specify your own arguments to Python\'s Behave',
-        default='')
+        "-a", "--advanced-arguments", type=str, help="Specify your own arguments to Python's Behave", default=""
+    )
     args = vars(parser.parse_args())
 
-    if args['purge']:
+    if args["purge"]:
         TestPurger().purge()
-    elif args['report']:
+    elif args["report"]:
         generate_report()
     else:
         run_tests(args)
-    print('# All tasks are complete :-)')
+    print("# All tasks are complete :-)")
--- src/ifcbimtester/features/steps/geometric_detail.py
+++ src/ifcbimtester/features/steps/geometric_detail.py
@@ -2,26 +2,27 @@ from behave import step
 from utils import IfcFile
 from utils import IfcFile, assert_attribute
 
-@step('All elements must be under {number} polygons')
+
+@step("All elements must be under {number} polygons")
 def step_impl(context, number):
     number = int(number)
     errors = []
-    for element in IfcFile.get().by_type('IfcElement'):
+    for element in IfcFile.get().by_type("IfcElement"):
         if not element.Representation:
             continue
         total_polygons = 0
         tree = IfcFile.get().traverse(element.Representation)
         for e in tree:
-            if e.is_a('IfcFace'):
+            if e.is_a("IfcFace"):
                 total_polygons += 1
-            elif e.is_a('IfcPolygonalFaceSet'):
+            elif e.is_a("IfcPolygonalFaceSet"):
                 total_polygons += len(e.Faces)
-            elif e.is_a('IfcTriangulatedFaceSet'):
+            elif e.is_a("IfcTriangulatedFaceSet"):
                 total_polygons += len(e.CoordIndex)
         if total_polygons > number:
             errors.append((total_polygons, element))
     if errors:
-        message = 'The following {} elements are over 500 polygons:\n'.format(len(errors))
+        message = "The following {} elements are over 500 polygons:\n".format(len(errors))
         for error in errors:
-            message += 'Polygons: {} - {}\n'.format(error[0], error[1])
-        assert False,  message
+            message += "Polygons: {} - {}\n".format(error[0], error[1])
+        assert False, message
