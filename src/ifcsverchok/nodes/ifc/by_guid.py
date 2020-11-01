66ba34a53689cb5eee7291b9349641d52b8f3b07
htlcnn <hoangthanhlong@gmail.com>
2020-11-01 19:24:01 +0700

    black ifcsverchok

--- src/ifcsverchok/nodes/ifc/by_guid.py
+++ src/ifcsverchok/nodes/ifc/by_guid.py
@@ -7,25 +7,27 @@ from sverchok.data_structure import updateNode
 
 
 class SvIfcByGuid(bpy.types.Node, SverchCustomTreeNode, ifcsverchok.helper.SvIfcCore):
-    bl_idname = 'SvIfcByGuid'
-    bl_label = 'IFC By Guid'
-    file: StringProperty(name='file', update=updateNode)
-    guid: StringProperty(name='guid', update=updateNode)
+    bl_idname = "SvIfcByGuid"
+    bl_label = "IFC By Guid"
+    file: StringProperty(name="file", update=updateNode)
+    guid: StringProperty(name="guid", update=updateNode)
 
     def sv_init(self, context):
-        self.inputs.new('SvStringsSocket', 'file').prop_name = 'file'
-        self.inputs.new('SvStringsSocket', 'guid').prop_name = 'guid'
-        self.outputs.new('SvStringsSocket', 'entity')
+        self.inputs.new("SvStringsSocket", "file").prop_name = "file"
+        self.inputs.new("SvStringsSocket", "guid").prop_name = "guid"
+        self.outputs.new("SvStringsSocket", "entity")
 
     def process(self):
-        self.sv_input_names = ['file', 'guid']
+        self.sv_input_names = ["file", "guid"]
         super().process()
 
     def process_ifc(self, file, guid):
-        self.outputs['entity'].sv_set([[file.by_guid(guid)]])
+        self.outputs["entity"].sv_set([[file.by_guid(guid)]])
+
 
 def register():
     bpy.utils.register_class(SvIfcByGuid)
+
 
 def unregister():
     bpy.utils.unregister_class(SvIfcByGuid)
