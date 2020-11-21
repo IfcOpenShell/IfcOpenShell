IfcOpenShell/IfcOpenShell

commit b16298630840cafd1f35a1867921bebaa162e3c4

Fixes #1038

src/ifcgeom/IfcGeomShapes.cpp
@@ -1697,7 +1697,7 @@ namespace {
 		}
 		k.remove_duplicate_points_from_loop(polygon, true);
 
-		if (polygon.Size() < 3) {
+		if (polygon.Length() < 3) {
 			return false;
 		}
 
@@ -1816,7 +1816,7 @@ bool IfcGeom::Kernel::convert(const IfcSchema::IfcPolygonalFaceSet* pfs, TopoDS_
         }
     }
 
-    if (faces.Size() == 0) return false;
+    if (faces.IsEmpty() == 0) return false;
 
     return create_solid_from_faces(faces, shape);
 }

commit 054ca5a095e0cfeaae6e08cafc00905153646524

Fix for #1119 (regression from b162986)

src/ifcgeom/IfcGeomShapes.cpp
@@ -1816,7 +1816,7 @@ bool IfcGeom::Kernel::convert(const IfcSchema::IfcPolygonalFaceSet* pfs, TopoDS_
         }
     }
 
-    if (faces.IsEmpty() == 0) return false;
+    if (faces.IsEmpty()) return false;
 
     return create_solid_from_faces(faces, shape);
 }