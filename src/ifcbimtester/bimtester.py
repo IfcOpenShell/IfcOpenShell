15199152ac2fe3ad1aef44d3338c843888683f02
Bernd Hahnebach <bernd@bimstatik.org>
2020-11-26 07:37:29 +0100

    bimtester: code formating and comments

--- src/ifcbimtester/bimtester.py
+++ src/ifcbimtester/bimtester.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 
-# Unix:
-# $ pyinstaller --onefile --clean --icon=icon.ico --add-data "features:features" bimtester.py`
-# Windows:
-# $ pyinstaller --onefile --clean --icon=icon.ico --add-data "features;features" bimtester.py`
-
 import argparse
 
 from bimtester import clean
@@ -13,13 +8,41 @@ from bimtester import run
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Runs unit tests for BIM data")
-    parser.add_argument("-p", "--purge", action="store_true", help="Purge tests of deleted elements")
-    parser.add_argument("-r", "--report", action="store_true", help="Generate a HTML report")
-    parser.add_argument("-c", "--console", action="store_true", help="Show results in the console")
-    parser.add_argument("-f", "--feature", type=str, help="Specify a feature file to test", default="")
+
+    parser = argparse.ArgumentParser(
+        description="Runs unit tests for BIM data"
+    )
     parser.add_argument(
-        "-a", "--advanced-arguments", type=str, help="Specify your own arguments to Python's Behave", default=""
+        "-p",
+        "--purge",
+        action="store_true",
+        help="Purge tests of deleted elements"
+    )
+    parser.add_argument(
+        "-r",
+        "--report",
+        action="store_true",
+        help="Generate a HTML report"
+    )
+    parser.add_argument(
+        "-c",
+        "--console",
+        action="store_true",
+        help="Show results in the console"
+    )
+    parser.add_argument(
+        "-f",
+        "--feature",
+        type=str,
+        help="Specify a feature file to test",
+        default=""
+    )
+    parser.add_argument(
+        "-a",
+        "--advanced-arguments",
+        type=str,
+        help="Specify your own arguments to Python's Behave",
+        default=""
     )
     args = vars(parser.parse_args())
 
