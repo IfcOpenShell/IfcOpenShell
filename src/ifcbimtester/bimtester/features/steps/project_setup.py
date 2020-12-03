cb03388105bfc5f8e5851c37a57d90da49c7c9fc
Bernd Hahnebach <bernd@bimstatik.org>
2020-12-03 18:15:10 +0100

    bimtester: move ifc steps in separte module

--- src/ifcbimtester/bimtester/features/steps/project_setup.py
+++ src/ifcbimtester/bimtester/features/steps/project_setup.py
@@ -4,31 +4,6 @@ from utils import assert_attribute
 from utils import IfcFile
 
 
-@step('The IFC file "{file}" must be provided')
-def step_impl(context, file):
-    try:
-        IfcFile.load(file)
-    except:
-        assert False, f"The file {file} could not be loaded"
-
-
-@step("IFC data must use the {schema} schema")
-def step_impl(context, schema):
-    assert IfcFile.get().schema == schema, "We expected a schema of {} but instead got {}".format(
-        schema, IfcFile.get().schema
-    )
-
-
-@step('The IFC file "{file}" is exempt from being provided')
-def step_impl(context, file):
-    pass
-
-
-@step("No further requirements are specified because {reason}")
-def step_impl(context, reason):
-    pass
-
-
 @step("The project must have an identifier of {guid}")
 def step_impl(context, guid):
     assert_attribute(IfcFile.get().by_type("IfcProject")[0], "GlobalId", guid)
