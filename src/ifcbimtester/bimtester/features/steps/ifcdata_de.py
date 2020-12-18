d4ed20f1d037b8087a02aec52f6f21d0f65560e7
Bernd Hahnebach <bernd@bimstatik.org>
2020-12-18 07:14:42 +0100

    bimtester: se gettext for message translation

--- src/ifcbimtester/bimtester/features/steps/ifcdata_de.py
+++ src/ifcbimtester/bimtester/features/steps/ifcdata_de.py
@@ -1,19 +1,9 @@
 from behave import step
 
-from utils import assert_schema
-
-
-# https://behave.readthedocs.io/en/latest/api.html#step-macro-calling-steps-from-other-steps
-# https://stackoverflow.com/questions/46735425/how-to-only-show-failing-tests-from-behave
-# https://community.osarch.org/discussion/comment/4533/#Comment_4533
+from ifcdata_methods import assert_schema
+from utils import switch_locale
 
 @step("Die IFC Daten müssen das {schema} Schema benutzen")
 def step_impl(context, schema):
-    try:
-        context.execute_steps(
-            "* IFC data must use the {schema} schema".format(schema=schema)
-        )
-    except Exception:
-        assert_schema(schema, context.schema)
-        # TODO translate error
-        # "Wir haben ein Schema {} erwartet, aber die Daten sind Schema (}"
+    switch_locale(context.localedir, "de")
+    assert_schema(context, schema)
