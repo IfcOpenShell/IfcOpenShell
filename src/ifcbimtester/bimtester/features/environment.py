5572faf8b48e64382660192f3d8ebef03c8551d1
Roch Bertucat <roch.bertucat@engisis.com>
2020-12-18 16:12:11 +0100

    Fix an issue with localedir

--- src/ifcbimtester/bimtester/features/environment.py
+++ src/ifcbimtester/bimtester/features/environment.py
@@ -3,5 +3,6 @@ from behave.model import Scenario
 
 def before_all(context):
     userdata = context.config.userdata
+    context.localedir = userdata.get("localedir")
     continue_after_failed = userdata.getbool("runner.continue_after_failed_step", True)
     Scenario.continue_after_failed_step = continue_after_failed
