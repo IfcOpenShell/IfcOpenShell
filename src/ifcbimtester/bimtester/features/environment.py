754620345551bf76e468e828cb7c62a4059b44fa
Bernd Hahnebach <bernd@bimstatik.org>
2020-12-19 09:10:24 +0100

    bimtester: fix translation directory

--- src/ifcbimtester/bimtester/features/environment.py
+++ src/ifcbimtester/bimtester/features/environment.py
@@ -3,5 +3,7 @@ from behave.model import Scenario
 
 def before_all(context):
     userdata = context.config.userdata
+    context.localedir = userdata["localedir"]
+
     continue_after_failed = True
     Scenario.continue_after_failed_step = continue_after_failed
