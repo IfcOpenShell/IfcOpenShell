7c4206430a5dfc349dfa62f2eb5c751eb8e97c32
Bernd Hahnebach <bernd@bimstatik.org>
2020-12-21 10:28:03 +0100

    bimtester: small code improvements

--- src/ifcbimtester/bimtester/features/environment.py
+++ src/ifcbimtester/bimtester/features/environment.py
@@ -3,6 +3,10 @@ from behave.model import Scenario
 
 def before_all(context):
     userdata = context.config.userdata
+    context.ifcbasename = userdata["ifcbasename"]
     context.localedir = userdata.get("localedir")
+
+    # do not break after a failed scenario
+    # https://community.osarch.org/discussion/comment/3328/#Comment_3328
     continue_after_failed = userdata.getbool("runner.continue_after_failed_step", True)
     Scenario.continue_after_failed_step = continue_after_failed
