f639ce565ee99daee14977abd3da0691ce17ed21
Bernd Hahnebach <bernd@bimstatik.org>
2020-12-21 11:28:51 +0100

    bimtester: fix error in step code

--- src/ifcbimtester/bimtester/features/steps/geometric_detail_de.py
+++ src/ifcbimtester/bimtester/features/steps/geometric_detail_de.py
@@ -4,7 +4,7 @@ from geometric_detail_methods import class_geometric_representation
 from utils import switch_locale
 
 
-step("Alle {ifc_class} Bauteile müssen eine geometrische Repräsentation der Klasse {representation_class} verwenden")
+@step("Alle {ifc_class} Bauteile müssen eine geometrische Repräsentation der Klasse {representation_class} verwenden")
 def step_impl(context, ifc_class, representation_class):
     switch_locale(context.localedir, "de")
     class_geometric_representation(context, ifc_class, representation_class)
