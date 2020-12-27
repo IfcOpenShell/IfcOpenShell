b9583c562d4067c0d30206f72f31795552b25c21
Dion Moult <dion@thinkmoult.com>
2020-12-27 16:53:40 +1100

    Implement support for deleting reference links

--- src/ifcblenderexport/blenderbim/bim/__init__.py
+++ src/ifcblenderexport/blenderbim/bim/__init__.py
@@ -38,6 +38,7 @@ if bpy is not None:
         operator.AddBcfHeaderFile,
         operator.ViewBcfTopic,
         operator.RemoveBcfComment,
+        operator.RemoveBcfReferenceLink,
         operator.EditBcfComment,
         operator.AddBcfComment,
         operator.ActivateBcfViewpoint,
