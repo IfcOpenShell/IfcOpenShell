46745eb734c6723a7071da7dc95b1a74912a7560
Dion Moult <dion@thinkmoult.com>
2021-01-04 11:08:15 +1100

    WIP assigning a class now ported over to partial editing and creates an IFC element. See #1222.

---
+++ src/ifcblenderexport/blenderbim/bim/module/root/__init__.py
@@ -0,0 +1,16 @@
+import bpy
+from . import operator
+
+classes = (
+    operator.ReassignClass,
+    operator.AssignClass,
+    operator.UnassignClass,
+)
+
+
+def register():
+    pass
+
+
+def unregister():
+    pass
