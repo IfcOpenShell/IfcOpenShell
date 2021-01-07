0fbd0749916ae81694b4270069e8babbd7d854e8
Bernd Hahnebach <bernd@bimstatik.org>
2021-01-07 18:02:09 +0100

    bimtester: improve step translation

--- src/ifcbimtester/bimtester/features/steps/ifcdata_fr.py
+++ src/ifcbimtester/bimtester/features/steps/ifcdata_fr.py
@@ -3,7 +3,11 @@ from behave import step
 from ifcdata_methods import assert_schema
 from utils import switch_locale
 
+
+the_lang = "fr"
+
+
 @step("Les données IFC doivent utiliser le schéma {schema}")
 def step_impl(context, schema):
-    switch_locale(context.localedir, "fr")
+    switch_locale(context.localedir, the_lang)
     assert_schema(context, schema)
