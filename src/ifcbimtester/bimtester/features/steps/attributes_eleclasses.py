8acbdee052dfb4145e3754992c5c44d11c1223f1
Bernd Hahnebach <bernd@bimstatik.org>
2021-01-07 18:02:09 +0100

    bimtester: improve step translation

--- src/ifcbimtester/bimtester/features/steps/attributes_eleclasses.py
+++ src/ifcbimtester/bimtester/features/steps/attributes_eleclasses.py
@@ -6,9 +6,12 @@ from utils import IfcFile
 from utils import switch_locale
 
 
+the_lang = "en"
+
+
 @step("there are no {ifc_class} elements because {reason}")
 def step_impl(context, ifc_class, reason):
-    switch_locale(context.localedir, "en")
+    switch_locale(context.localedir, the_lang)
     aem.no_eleclass_because_reason(
         context,
         ifc_class,
@@ -18,7 +21,7 @@ def step_impl(context, ifc_class, reason):
 
 @step('all {ifc_class} elements class attributes have a value')
 def step_impl(context, ifc_class):
-    switch_locale(context.localedir, "en")
+    switch_locale(context.localedir, the_lang)
     aem.eleclass_have_class_attributes_with_a_value(
         context,
         ifc_class
@@ -27,7 +30,7 @@ def step_impl(context, ifc_class):
 
 @step('all {ifc_class} elements have a name given')
 def step_impl(context, ifc_class):
-    switch_locale(context.localedir, "en")
+    switch_locale(context.localedir, the_lang)
     aem.eleclass_has_name_with_a_value(
         context,
         ifc_class
@@ -36,7 +39,7 @@ def step_impl(context, ifc_class):
 
 @step('all {ifc_class} elements have a description given')
 def step_impl(context, ifc_class):
-    switch_locale(context.localedir, "en")
+    switch_locale(context.localedir, the_lang)
     aem.eleclass_has_description_with_a_value(
         context,
         ifc_class
--- src/ifcbimtester/bimtester/features/steps/attributes_psets.py
+++ src/ifcbimtester/bimtester/features/steps/attributes_psets.py
@@ -6,9 +6,12 @@ from utils import IfcFile
 from utils import switch_locale
 
 
+the_lang = "en"
+
+
 @step("all {ifc_class} elements have an {aproperty} property in the {pset} pset")
 def step_impl(context, ifc_class, aproperty, pset):
-    switch_locale(context.localedir, "en")
+    switch_locale(context.localedir, the_lang)
     apm.eleclass_has_property_in_pset(
         context,
         ifc_class,
