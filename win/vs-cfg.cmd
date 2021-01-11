75147bb34d6a3b83d07425c697bf27abaf526ba5
ahladik <75784288+ahladik@users.noreply.github.com>
2021-01-11 10:00:17 +0100

    Update vs-cfg.cmd

--- win/vs-cfg.cmd
+++ win/vs-cfg.cmd
@@ -51,7 +51,14 @@ set LAST_GENERATOR_IDX=4
 set GEN_SHORTHAND=!GENERATOR:vs=!
 
 if not "!GEN_SHORTHAND!"=="" if !GEN_SHORTHAND!==!GENERATOR! goto :GeneratorShorthandCheckDone
-set VS_PLATFORM=Win32
+
+set "VS_PLATFORM=Win32"
+:: use the command prompt target platform, at least initially
+if %VSCMD_ARG_TGT_ARCH%==x86	set "VS_PLATFORM=Win32"
+if %VSCMD_ARG_TGT_ARCH%==x64	set "VS_PLATFORM=x64"
+if %VSCMD_ARG_TGT_ARCH%==arm	set "VS_PLATFORM=ARM"
+if %VSCMD_ARG_TGT_ARCH%==arm64	set "VS_PLATFORM=ARM64"
+
 :: "echo if" trick from http://stackoverflow.com/a/8758579
 echo(!GEN_SHORTHAND! | findstr /c:"-x86"     >nul && ( set "VS_PLATFORM=Win32" )
 echo(!GEN_SHORTHAND! | findstr /c:"-x64"     >nul && ( set "VS_PLATFORM=x64" )
