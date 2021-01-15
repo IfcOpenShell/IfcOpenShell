07aed585edf34153947260b132fc981f2b70ed4c
Bernd Hahnebach <bernd@bimstatik.org>
2021-01-15 20:20:18 +0100

    bimtester: steps, translate some attributes

--- src/ifcbimtester/bimtester/features/steps/attributes_eleclasses.py
+++ src/ifcbimtester/bimtester/features/steps/attributes_eleclasses.py
@@ -9,17 +9,25 @@ from utils import switch_locale
 the_lang = "en"
 
 
-@step("there are no {ifc_class} elements because {reason}")
-def step_impl(context, ifc_class, reason):
+@step("There are no {ifc_class} elements")
+def step_impl(context, ifc_class):
     switch_locale(context.localedir, the_lang)
-    aem.no_eleclass_because_reason(
+    aem.no_eleclass(
         context,
-        ifc_class,
-        reason
+        ifc_class
     )
 
 
-@step('all {ifc_class} elements class attributes have a value')
+@step("There are no {ifc_class} elements because {reason}")
+def step_impl(context, ifc_class, reason):
+    switch_locale(context.localedir, the_lang)
+    aem.no_eleclass(
+        context,
+        ifc_class
+    )
+
+
+@step("All {ifc_class} elements class attributes have a value")
 def step_impl(context, ifc_class):
     switch_locale(context.localedir, the_lang)
     aem.eleclass_have_class_attributes_with_a_value(
@@ -28,7 +36,7 @@ def step_impl(context, ifc_class):
     )
 
 
-@step('all {ifc_class} elements have a name given')
+@step("All {ifc_class} elements have a name given")
 def step_impl(context, ifc_class):
     switch_locale(context.localedir, the_lang)
     aem.eleclass_has_name_with_a_value(
@@ -37,7 +45,7 @@ def step_impl(context, ifc_class):
     )
 
 
-@step('all {ifc_class} elements have a description given')
+@step("All {ifc_class} elements have a description given")
 def step_impl(context, ifc_class):
     switch_locale(context.localedir, the_lang)
     aem.eleclass_has_description_with_a_value(
---
+++ src/ifcbimtester/bimtester/features/steps/attributes_eleclasses_fr.py
@@ -0,0 +1,59 @@
+from behave import step
+
+import attributes_eleclasses_methods as aem
+from utils import switch_locale
+
+
+the_lang = "fr"
+
+
+"""
+# TODO the next line needs translation
+@step("There are no {ifc_class} elements")
+def step_impl(context, ifc_class):
+    switch_locale(context.localedir, the_lang)
+    aem.no_eleclass(
+        context,
+        ifc_class
+    )
+
+
+# TODO the next line needs translation
+@step("There are no {ifc_class} elements because {reason}")
+def step_impl(context, ifc_class, reason):
+    switch_locale(context.localedir, the_lang)
+    aem.no_eleclass(
+        context,
+        ifc_class
+    )
+
+
+# TODO the next line needs translation
+@step("All {ifc_class} elements class attributes have a value")
+def step_impl(context, ifc_class):
+    switch_locale(context.localedir, the_lang)
+    aem.eleclass_have_class_attributes_with_a_value(
+        context,
+        ifc_class
+    )
+
+
+# TODO the next line needs translation
+@step("All {ifc_class} elements have a name given")
+def step_impl(context, ifc_class):
+    switch_locale(context.localedir, the_lang)
+    aem.eleclass_has_name_with_a_value(
+        context,
+        ifc_class
+    )
+
+
+# TODO the next line needs translation
+@step("All {ifc_class} elements have a description given")
+def step_impl(context, ifc_class):
+    switch_locale(context.localedir, the_lang)
+    aem.eleclass_has_description_with_a_value(
+        context,
+        ifc_class
+    )
+"""
