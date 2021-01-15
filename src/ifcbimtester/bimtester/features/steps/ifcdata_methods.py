736c9eac0803cea9926c5fee873b3b525c18021d
Bernd Hahnebach <bernd@bimstatik.org>
2021-01-15 20:06:11 +0100

    bimtester: steps, translate ifcdata provide ifc file by argument

--- src/ifcbimtester/bimtester/features/steps/ifcdata_methods.py
+++ src/ifcbimtester/bimtester/features/steps/ifcdata_methods.py
@@ -1,6 +1,16 @@
 from utils import IfcFile
 
 
+def provide_ifcfile_by_argument(context):
+    try:
+        IfcFile.load(context.config.userdata.get("ifcfile"))
+    except:
+        assert False, (
+            _("The IFC {} file could not be loaded")
+            .format(context.config.userdata.get('ifcfile'))
+        )
+
+
 def has_ifcdata_specific_schema(context, target_schema):
     real_schema = IfcFile.get().schema
     assert real_schema == target_schema, (
