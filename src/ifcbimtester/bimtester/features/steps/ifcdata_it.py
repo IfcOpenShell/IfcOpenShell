4271587bd728a50c7622d9060d0b670feae54fbc
Bernd Hahnebach <bernd@bimstatik.org>
2021-01-17 07:45:12 +0100

    bimtester: start italian and dutch translation

---
+++ src/ifcbimtester/bimtester/features/steps/ifcdata_it.py
@@ -0,0 +1,20 @@
+from behave import step
+
+import ifcdata_methods as idm
+from utils import switch_locale
+
+
+the_lang = "it"
+
+
+# TODO the next line needs translation
+@given("Il file IFC è stato fornito attraverso un argumento")
+def step_impl(context):
+    switch_locale(context.localedir, the_lang)
+    idm.provide_ifcfile_by_argument(context)
+
+
+@step("I dati IFC devono seguire lo schema {schema}")
+def step_impl(context, schema):
+    switch_locale(context.localedir, the_lang)
+    idm.has_ifcdata_specific_schema(context, schema)
