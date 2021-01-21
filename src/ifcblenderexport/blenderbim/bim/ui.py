404a6c244ce5c77f55c3802edce55cfe00f2ec80
Dion Moult <dion@thinkmoult.com>
2021-01-22 10:20:38 +1100

    WIP deprecate merging/import aggregates setting as they are much more usable now. See #1222.

--- src/ifcblenderexport/blenderbim/bim/ui.py
+++ src/ifcblenderexport/blenderbim/bim/ui.py
@@ -813,10 +813,6 @@ class BIM_PT_mvd(Panel):
         layout.label(text="Simplifications:")
 
         row = layout.row()
-        row.prop(bim_properties, "import_should_import_aggregates")
-        row = layout.row()
-        row.prop(bim_properties, "import_should_merge_aggregates")
-        row = layout.row()
         row.prop(bim_properties, "import_should_merge_by_class")
         row = layout.row()
         row.prop(bim_properties, "import_should_merge_by_material")
