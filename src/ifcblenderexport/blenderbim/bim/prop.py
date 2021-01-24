af3ed303d8b40af3a5328eb97ef8d136c047aa64
Dion Moult <dion@thinkmoult.com>
2021-01-24 19:17:33 +1100

    WIP port pset template module. It probably needs rewriting, but at least no regressions. See #1222.

--- src/ifcblenderexport/blenderbim/bim/prop.py
+++ src/ifcblenderexport/blenderbim/bim/prop.py
@@ -31,9 +31,6 @@ availablematerialpsets_enum = []
 ifcpatchrecipes_enum = []
 titleblocks_enum = []
 materialpsetnames_enum = []
-psetfiles_enum = []
-psettemplatefiles_enum = []
-propertysettemplates_enum = []
 attributes_enum = []
 materialattributes_enum = []
 contexts_enum = []
@@ -261,32 +258,6 @@ def toggleDecorationsOnLoad(*args):
         decoration.DecorationsHandler.uninstall()
 
 
-def getPsetTemplateFiles(self, context):
-    global psettemplatefiles_enum
-    if len(psettemplatefiles_enum) < 1:
-        files = os.listdir(os.path.join(self.data_dir, "pset"))
-        psettemplatefiles_enum.extend([(f.replace(".ifc", ""), f.replace(".ifc", ""), "") for f in files])
-    return psettemplatefiles_enum
-
-
-def refreshPropertySetTemplates(self, context):
-    global propertysettemplates_enum
-    propertysettemplates_enum.clear()
-    getPropertySetTemplates(self, context)
-
-
-def getPropertySetTemplates(self, context):
-    global propertysettemplates_enum
-    if len(propertysettemplates_enum) < 1:
-        ifc.IfcStore.pset_template_path = os.path.join(
-            context.scene.BIMProperties.data_dir, "pset", context.scene.BIMProperties.pset_template_files + ".ifc"
-        )
-        ifc.IfcStore.pset_template_file = ifcopenshell.open(ifc.IfcStore.pset_template_path)
-        templates = ifc.IfcStore.pset_template_file.by_type("IfcPropertySetTemplate")
-        propertysettemplates_enum.extend([(t.GlobalId, t.Name, "") for t in templates])
-    return propertysettemplates_enum
-
-
 def getMaterialPsetNames(self, context):
     global materialpsetnames_enum
     materialpsetnames_enum.clear()
@@ -508,180 +479,6 @@ class SmartClashGroup(PropertyGroup):
     global_ids: CollectionProperty(name="GlobalIDs", type=StrProperty)
 
 
-class PropertySetTemplate(PropertyGroup):
-    global_id: StringProperty(name="Global ID")
-    name: StringProperty(name="Name")
-    description: StringProperty(name="Description")
-    template_type: EnumProperty(
-        items=[
-            (
-                "PSET_TYPEDRIVENONLY",
-                "Pset - IfcTypeObject",
-                "The property sets defined by this IfcPropertySetTemplate can only be assigned to subtypes of IfcTypeObject.",
-            ),
-            (
-                "PSET_TYPEDRIVENOVERRIDE",
-                "Pset - IfcTypeObject - Override",
-                "The property sets defined by this IfcPropertySetTemplate can only be assigned to subtypes of IfcTypeObject.",
-            ),
-            (
-                "PSET_OCCURRENCEDRIVEN",
-                "Pset - IfcObject",
-                "The property sets defined by this IfcPropertySetTemplate can only be assigned to subtypes of IfcObject.",
-            ),
-            (
-                "PSET_PERFORMANCEDRIVEN",
-                "Pset - IfcPerformanceHistory",
-                "The property sets defined by this IfcPropertySetTemplate can only be assigned to IfcPerformanceHistory.",
-            ),
-            (
-                "QTO_TYPEDRIVENONLY",
-                "Qto - IfcTypeObject",
-                "The element quantity defined by this IfcPropertySetTemplate can only be assigned to subtypes of IfcTypeObject.",
-            ),
-            (
-                "QTO_TYPEDRIVENOVERRIDE",
-                "Qto - IfcTypeObject - Override",
-                "The element quantity defined by this IfcPropertySetTemplate can be assigned to subtypes of IfcTypeObject and can be overridden by an element quantity with same name at subtypes of IfcObject.",
-            ),
-            (
-                "QTO_OCCURRENCEDRIVEN",
-                "Qto - IfcObject",
-                "The element quantity defined by this IfcPropertySetTemplate can only be assigned to subtypes of IfcObject.",
-            ),
-            (
-                "NOTDEFINED",
-                "Not defined",
-                "No restriction provided, the property sets defined by this IfcPropertySetTemplate can be assigned to any entity, if not otherwise restricted by the ApplicableEntity attribute.",
-            ),
-        ],
-        name="Template Type",
-    )
-    applicable_entity: StringProperty(name="Applicable Entity")
-
-
-class PropertyTemplate(PropertyGroup):
-    global_id: StringProperty(name="Global ID")
-    name: StringProperty(name="Name")
-    description: StringProperty(name="Description")
-    primary_measure_type: EnumProperty(
-        items=[
-            (x, x, "")
-            for x in [
-                "IfcInteger",
-                "IfcReal",
-                "IfcBoolean",
-                "IfcIdentifier",
-                "IfcText",
-                "IfcLabel",
-                "IfcLogical",
-                "IfcDateTime",
-                "IfcDate",
-                "IfcTime",
-                "IfcDuration",
-                "IfcTimeStamp",
-                "IfcPositiveInteger",
-                "IfcBinary",
-                "IfcVolumeMeasure",
-                "IfcTimeMeasure",
-                "IfcThermodynamicTemperatureMeasure",
-                "IfcSolidAngleMeasure",
-                "IfcPositiveRatioMeasure",
-                "IfcRatioMeasure",
-                "IfcPositivePlaneAngleMeasure",
-                "IfcPlaneAngleMeasure",
-                "IfcParameterValue",
-                "IfcNumericMeasure",
-                "IfcMassMeasure",
-                "IfcPositiveLengthMeasure",
-                "IfcLengthMeasure",
-                "IfcElectricCurrentMeasure",
-                "IfcDescriptiveMeasure",
-                "IfcCountMeasure",
-                "IfcContextDependentMeasure",
-                "IfcAreaMeasure",
-                "IfcAmountOfSubstanceMeasure",
-                "IfcLuminousIntensityMeasure",
-                "IfcNormalisedRatioMeasure",
-                "IfcComplexNumber",
-                "IfcNonNegativeLengthMeasure",
-                "IfcAbsorbedDoseMeasure",
-                "IfcAccelerationMeasure",
-                "IfcAngularVelocityMeasure",
-                "IfcAreaDensityMeasure",
-                "IfcCompoundPlaneAngleMeasure",
-                "IfcCurvatureMeasure",
-                "IfcDoseEquivalentMeasure",
-                "IfcDynamicViscosityMeasure",
-                "IfcElectricCapacitanceMeasure",
-                "IfcElectricChargeMeasure",
-                "IfcElectricConductanceMeasure",
-                "IfcElectricResistanceMeasure",
-                "IfcElectricVoltageMeasure",
-                "IfcEnergyMeasure",
-                "IfcForceMeasure",
-                "IfcFrequencyMeasure",
-                "IfcHeatFluxDensityMeasure",
-                "IfcHeatingValueMeasure",
-                "IfcIlluminanceMeasure",
-                "IfcInductanceMeasure",
-                "IfcIntegerCountRateMeasure",
-                "IfcIonConcentrationMeasure",
-                "IfcIsothermalMoistureCapacityMeasure",
-                "IfcKinematicViscosityMeasure",
-                "IfcLinearForceMeasure",
-                "IfcLinearMomentMeasure",
-                "IfcLinearStiffnessMeasure",
-                "IfcLinearVelocityMeasure",
-                "IfcLuminousFluxMeasure",
-                "IfcLuminousIntensityDistributionMeasure",
-                "IfcMagneticFluxDensityMeasure",
-                "IfcMagneticFluxMeasure",
-                "IfcMassDensityMeasure",
-                "IfcMassFlowRateMeasure",
-                "IfcMassPerLengthMeasure",
-                "IfcModulusOfElasticityMeasure",
-                "IfcModulusOfLinearSubgradeReactionMeasure",
-                "IfcModulusOfRotationalSubgradeReactionMeasure",
-                "IfcModulusOfSubgradeReactionMeasure",
-                "IfcMoistureDiffusivityMeasure",
-                "IfcMolecularWeightMeasure",
-                "IfcMomentOfInertiaMeasure",
-                "IfcMonetaryMeasure",
-                "IfcPHMeasure",
-                "IfcPlanarForceMeasure",
-                "IfcPowerMeasure",
-                "IfcPressureMeasure",
-                "IfcRadioActivityMeasure",
-                "IfcRotationalFrequencyMeasure",
-                "IfcRotationalMassMeasure",
-                "IfcRotationalStiffnessMeasure",
-                "IfcSectionModulusMeasure",
-                "IfcSectionalAreaIntegralMeasure",
-                "IfcShearModulusMeasure",
-                "IfcSoundPowerLevelMeasure",
-                "IfcSoundPowerMeasure",
-                "IfcSoundPressureLevelMeasure",
-                "IfcSoundPressureMeasure",
-                "IfcSpecificHeatCapacityMeasure",
-                "IfcTemperatureGradientMeasure",
-                "IfcTemperatureRateOfChangeMeasure",
-                "IfcThermalAdmittanceMeasure",
-                "IfcThermalConductivityMeasure",
-                "IfcThermalExpansionCoefficientMeasure",
-                "IfcThermalResistanceMeasure",
-                "IfcThermalTransmittanceMeasure",
-                "IfcTorqueMeasure",
-                "IfcVaporPermeabilityMeasure",
-                "IfcVolumetricFlowRateMeasure",
-                "IfcWarpingConstantMeasure",
-                "IfcWarpingMomentMeasure",
-            ]
-        ],
-        name="Primary Measure Type",
-    )
-
-
 class BIMProperties(PropertyGroup):
     schema_dir: StringProperty(default=os.path.join(cwd, "schema") + os.path.sep, name="Schema Directory")
     data_dir: StringProperty(default=os.path.join(cwd, "data") + os.path.sep, name="Data Directory")
@@ -692,12 +489,6 @@ class BIMProperties(PropertyGroup):
     available_contexts: EnumProperty(items=[("Model", "Model", ""), ("Plan", "Plan", "")], name="Available Contexts")
     available_subcontexts: EnumProperty(items=getSubcontexts, name="Available Subcontexts")
     available_target_views: EnumProperty(items=getTargetViews, name="Available Target Views")
-    pset_template_files: EnumProperty(
-        items=getPsetTemplateFiles, name="Pset Template Files", update=refreshPropertySetTemplates
-    )
-    property_set_templates: EnumProperty(items=getPropertySetTemplates, name="Pset Template Files")
-    active_property_set_template: PointerProperty(type=PropertySetTemplate)
-    property_templates: CollectionProperty(name="Property Templates", type=PropertyTemplate)
     should_section_selected_objects: BoolProperty(name="Section Selected Objects", default=False)
     section_plane_colour: FloatVectorProperty(
         name="Temporary Section Cutaway Colour", subtype="COLOR", default=(1, 0, 0), min=0.0, max=1.0
--- src/ifcblenderexport/blenderbim/bim/ui.py
+++ src/ifcblenderexport/blenderbim/bim/ui.py
@@ -37,50 +37,6 @@ class BIM_PT_object_structural(Panel):
         row.prop(props, "structural_member_connection")
 
 
-class BIM_PT_psets(Panel):
-    bl_label = "IFC Property Sets"
-    bl_idname = "BIM_PT_psets"
-    bl_options = {"DEFAULT_CLOSED"}
-    bl_space_type = "PROPERTIES"
-    bl_region_type = "WINDOW"
-    bl_context = "scene"
-
-    def draw(self, context):
-        layout = self.layout
-        props = context.scene.BIMProperties
-
-        row = layout.row(align=True)
-        row.prop(props, "pset_template_files", text="")
-
-        row = layout.row(align=True)
-        row.prop(props, "property_set_templates", text="")
-        row.operator("bim.add_property_set_template", text="", icon="ADD")
-        row.operator("bim.remove_property_set_template", text="", icon="PANEL_CLOSE")
-        row.operator("bim.edit_property_set_template", text="", icon="IMPORT")
-        row.operator("bim.save_property_set_template", text="", icon="EXPORT")
-
-        row = layout.row(align=True)
-        row.prop(props.active_property_set_template, "name")
-        row = layout.row(align=True)
-        row.prop(props.active_property_set_template, "description")
-        row = layout.row(align=True)
-        row.prop(props.active_property_set_template, "template_type")
-        row = layout.row(align=True)
-        row.prop(props.active_property_set_template, "applicable_entity")
-
-        layout.label(text="Property Templates:")
-
-        row = layout.row(align=True)
-        row.operator("bim.add_property_template")
-
-        for index, template in enumerate(props.property_templates):
-            row = layout.row(align=True)
-            row.prop(template, "name", text="")
-            row.prop(template, "description", text="")
-            row.prop(template, "primary_measure_type", text="")
-            row.operator("bim.remove_property_template", icon="X", text="").index = index
-
-
 class BIM_PT_presentation_layer_data(Panel):
     bl_label = "IFC Presentation Layers"
     bl_idname = "BIM_PT_presentation"
