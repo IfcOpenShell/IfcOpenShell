40d12e5e15a8c6e1835b02e18c92bb2130c20100
Dion Moult <dion@thinkmoult.com>
2021-01-24 19:57:22 +1100

    WIP Refactor patch module. See #1222.

--- src/ifcblenderexport/blenderbim/bim/ui.py
+++ src/ifcblenderexport/blenderbim/bim/ui.py
@@ -403,36 +403,6 @@ class BIM_PT_library(Panel):
         layout.row().prop(scene.BIMLibrary, "description")
 
 
-class BIM_PT_patch(Panel):
-    bl_label = "IFC Patch"
-    bl_idname = "BIM_PT_patch"
-    bl_options = {"DEFAULT_CLOSED"}
-    bl_space_type = "PROPERTIES"
-    bl_region_type = "WINDOW"
-    bl_context = "scene"
-
-    def draw(self, context):
-        layout = self.layout
-        layout.use_property_split = True
-
-        scene = context.scene
-        props = scene.BIMProperties
-
-        row = layout.row()
-        row.prop(props, "ifc_patch_recipes")
-        row = layout.row(align=True)
-        row.prop(props, "ifc_patch_input")
-        row.operator("bim.select_ifc_patch_input", icon="FILE_FOLDER", text="")
-        row = layout.row(align=True)
-        row.prop(props, "ifc_patch_output")
-        row.operator("bim.select_ifc_patch_output", icon="FILE_FOLDER", text="")
-        row = layout.row()
-        row.prop(props, "ifc_patch_args")
-
-        row = layout.row()
-        op = row.operator("bim.execute_ifc_patch")
-
-
 class BIM_UL_generic(bpy.types.UIList):
     def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
         if item:
