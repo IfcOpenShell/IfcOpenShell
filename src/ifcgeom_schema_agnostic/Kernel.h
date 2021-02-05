8681610b79265309d8c02d5a1ff2a1e6ed227c7f
Jesse Vander Does <jvanderdoes@gmail.com>
2021-02-05 14:03:13 -0600

    Fixed white space

--- src/ifcgeom_schema_agnostic/Kernel.h
+++ src/ifcgeom_schema_agnostic/Kernel.h
@@ -53,7 +53,7 @@ namespace IfcGeom {
 			GV_PRECISION,
 			// Whether to process shapes of type Face or higher (1) Wire or lower (-1) or all (0)
 			GV_DIMENSIONALITY,
-            GV_LAYERSET_FIRST,
+			GV_LAYERSET_FIRST,
 			GV_DISABLE_BOOLEAN_RESULT,
 			GV_NO_WIRE_INTERSECTION_CHECK
 		};
