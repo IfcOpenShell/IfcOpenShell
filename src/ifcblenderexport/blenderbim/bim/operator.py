d08f4171e34bc19beda95e0eac7c95152b8117e4
Dion Moult <dion@thinkmoult.com>
2021-02-07 19:57:32 +1100

    Type representations are now always imported (better for authoring), and also import super fast compared to before (before, enabling types being imported would significantly slow down import times)

--- src/ifcblenderexport/blenderbim/bim/operator.py
+++ src/ifcblenderexport/blenderbim/bim/operator.py
@@ -111,7 +111,6 @@ class ImportIFC(bpy.types.Operator, ImportHelper):
     filename_ext = ".ifc"
     filter_glob: bpy.props.StringProperty(default="*.ifc;*.ifczip;*.ifcxml", options={"HIDDEN"})
 
-    should_import_type_representations: bpy.props.BoolProperty(name="Import Type Representations", default=False)
     should_import_spaces: bpy.props.BoolProperty(name="Import Spaces", default=False)
     should_auto_set_workarounds: bpy.props.BoolProperty(name="Automatically Set Vendor Workarounds", default=True)
     should_use_cpu_multiprocessing: bpy.props.BoolProperty(name="Import with CPU Multiprocessing", default=True)
@@ -138,7 +137,6 @@ class ImportIFC(bpy.types.Operator, ImportHelper):
         )
 
         settings = import_ifc.IfcImportSettings.factory(context, self.filepath, logger)
-        settings.should_import_type_representations = self.should_import_type_representations
         settings.should_import_spaces = self.should_import_spaces
         settings.should_auto_set_workarounds = self.should_auto_set_workarounds
         settings.should_use_cpu_multiprocessing = self.should_use_cpu_multiprocessing
