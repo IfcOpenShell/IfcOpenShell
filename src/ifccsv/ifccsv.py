e80a5f0587f8c0d38e942b8cc008564594370434
Priit Laes <plaes@plaes.org>
2021-03-14 22:30:13 +0200

    ifccsv: Fix reference to invalid variable

--- src/ifccsv/ifccsv.py
+++ src/ifccsv/ifccsv.py
@@ -21,7 +21,7 @@ class IfcAttributeSetter:
         if "." not in key:
             return element
         if key[0:3] == "Qto":
-            qto, prop = key.split(".", 1)
+            qto_name, prop = key.split(".", 1)
             qto = IfcAttributeSetter.get_element_qto(element, qto_name)
             if qto:
                 IfcAttributeSetter.set_qto_property(qto, prop, value)
