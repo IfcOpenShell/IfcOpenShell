6d641aaac1594c1f7f8e56588fefdd259151f796
Dion Moult <dion@thinkmoult.com>
2021-03-25 16:32:43 +1100

    Still cleaning up related to ownership histories. See #1399.

--- src/ifcopenshell-python/ifcopenshell/api/owner/settings.py
+++ src/ifcopenshell-python/ifcopenshell/api/owner/settings.py
@@ -1,33 +1,13 @@
-# import bpy
-# import addon_utils
+def get_person():
+    pass
+
+
+def get_organisation():
+    pass
+
 
 settings = {
-    "person": None,
-    "organisation": None,
     "ApplicationIdentifier": "",
     "ApplicationFullName": "",
     "Version": "",
-    "ChangeAction": "NOTDEFINED",
 }
-
-
-# settings = {
-#     "person": file.by_id(int(bpy.context.scene.BIMOwnerProperties.user_person)),
-#     "organisation": file.by_id(int(bpy.context.scene.BIMOwnerProperties.user_organisation)),
-#     "ApplicationIdentifier": "BlenderBIM",
-#     "ApplicationFullName": "BlenderBIM Add-on",
-#     "Version": get_application_version(),
-# }
-# 
-# 
-# def get_application_version():
-#     return ".".join(
-#         [
-#             str(x)
-#             for x in [
-#                 addon.bl_info.get("version", (-1, -1, -1))
-#                 for addon in addon_utils.modules()
-#                 if addon.bl_info["name"] == "BlenderBIM"
-#             ][0]
-#         ]
-#     )
