a777a558bd214be08bb74b8052328d2b8966252f
Sigma Dimensions <79010126+myoualid@users.noreply.github.com>
2021-04-07 00:04:57 +0100

    Feature edit work plan (#1418)
    * ifcopenshell.api modules Persons and Organisations account for IFC4 SCHEMA CHANGE
    
    * New Feature to Edit Workplans

--- src/blenderbim/blenderbim/bim/module/sequence/__init__.py
+++ src/blenderbim/blenderbim/bim/module/sequence/__init__.py
@@ -2,13 +2,21 @@ import bpy
 from . import ui, prop, operator
 
 classes = (
+    operator.LoadWorkPlans,
+    operator.DisableWorkPlanEditingUI,
     operator.LoadTasks,
     operator.DisableTaskEditingUI,
     operator.AddWorkPlan,
+    operator.EditWorkPlan,
     operator.RemoveWorkPlan,
+    operator.EnableEditingWorkPlan,
+    operator.DisableEditingWorkPlan,
+    prop.WorkPlan,
+    prop.BIMWorkPlanProperties,
     prop.Task,
     prop.BIMTaskProperties,
     ui.BIM_PT_work_plans,
+    ui.BIM_UL_work_plans,
     ui.BIM_PT_tasks,
     ui.BIM_UL_tasks,
 )
@@ -16,7 +24,9 @@ classes = (
 
 def register():
     bpy.types.Scene.BIMTaskProperties = bpy.props.PointerProperty(type=prop.BIMTaskProperties)
+    bpy.types.Scene.BIMWorkPlanProperties = bpy.props.PointerProperty(type=prop.BIMWorkPlanProperties)
 
 
 def unregister():
     del bpy.types.Scene.BIMTaskProperties
+    del bpy.types.Scene.BIMWorkPlanProperties
