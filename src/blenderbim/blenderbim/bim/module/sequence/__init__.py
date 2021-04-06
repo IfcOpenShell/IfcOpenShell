569ff43d5635b15ab265ca7c2b2d8c71cb4999d8
bosonprojets <bosonprojets@gmail.com>
2021-04-07 00:28:47 +0100

    New Feature to manpiulate Work Plans

--- src/blenderbim/blenderbim/bim/module/sequence/__init__.py
+++ src/blenderbim/blenderbim/bim/module/sequence/__init__.py
@@ -11,12 +11,23 @@ classes = (
     operator.RemoveWorkPlan,
     operator.EnableEditingWorkPlan,
     operator.DisableEditingWorkPlan,
+    operator.LoadWorkSchedules,
+    operator.DisableWorkScheduleEditingUI,
+    operator.AddWorkSchedule,
+    operator.EditWorkSchedule,
+    operator.RemoveWorkSchedule,
+    operator.EnableEditingWorkSchedule,
+    operator.DisableEditingWorkSchedule,
     prop.WorkPlan,
     prop.BIMWorkPlanProperties,
+    prop.WorkSchedule,
+    prop.BIMWorkScheduleProperties,
     prop.Task,
     prop.BIMTaskProperties,
     ui.BIM_PT_work_plans,
     ui.BIM_UL_work_plans,
+    ui.BIM_PT_work_schedules,
+    ui.BIM_UL_work_schedules,
     ui.BIM_PT_tasks,
     ui.BIM_UL_tasks,
 )
@@ -25,8 +36,10 @@ classes = (
 def register():
     bpy.types.Scene.BIMTaskProperties = bpy.props.PointerProperty(type=prop.BIMTaskProperties)
     bpy.types.Scene.BIMWorkPlanProperties = bpy.props.PointerProperty(type=prop.BIMWorkPlanProperties)
+    bpy.types.Scene.BIMWorkScheduleProperties = bpy.props.PointerProperty(type=prop.BIMWorkScheduleProperties)
 
 
 def unregister():
     del bpy.types.Scene.BIMTaskProperties
     del bpy.types.Scene.BIMWorkPlanProperties
+    del bpy.types.Scene.BIMWorkScheduleProperties
