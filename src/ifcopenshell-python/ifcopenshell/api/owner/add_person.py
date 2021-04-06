1c72d82309414f84698f690e71492473f1c43163
Sigma Dimensions <79010126+myoualid@users.noreply.github.com>
2021-04-06 23:28:02 +0100

    ifcopenshell.api modules Persons and Organisations account for IFC4 SCHEMA CHANGE (#1417)

--- src/ifcopenshell-python/ifcopenshell/api/owner/add_person.py
+++ src/ifcopenshell-python/ifcopenshell/api/owner/add_person.py
@@ -10,4 +10,7 @@ class Usecase:
             self.settings[key] = value
 
     def execute(self):
+        if self.file.schema == "IFC2X3": 
+            self.settings["Id"] = self.settings["Identification"] 
+            del self.settings["Identification"]
         return self.file.create_entity("IfcPerson", **self.settings)
