cd7062c905e290617d127da2f299fc9975d24f4a
bosonprojets <bosonprojets@gmail.com>
2021-04-24 05:59:17 +0000

    UX layout for IfcResources in blenderBIM

--- src/blenderbim/blenderbim/bim/module/resource/__init__.py
+++ src/blenderbim/blenderbim/bim/module/resource/__init__.py
@@ -8,17 +8,27 @@ classes = (
     operator.LoadResources,
     operator.AddCrewResource,
     operator.AddSubcontractResource,
+    operator.AddEquipementResource,
+    operator.AddLaborResource,
+    operator.AddProductResource,
+    operator.AddMaterialResource,
     operator.EditResource,
     operator.RemoveResource,
+    operator.EnableEditingNestedResource,
+    operator.LoadNestedResourceProperties,
+    operator.DisableNestedResourceEditingUI,
     prop.Resource,
     prop.BIMResourceProperties,
+    prop.BIMResourceTreeProperties,
     ui.BIM_PT_resources,
-    ui.BIM_UL_resources,
+    ui.BIM_UL_nested_resources,
 )
 
 
 def register():
     bpy.types.Scene.BIMResourceProperties = bpy.props.PointerProperty(type=prop.BIMResourceProperties)
+    bpy.types.Scene.BIMResourceTreeProperties = bpy.props.PointerProperty(type=prop.BIMResourceTreeProperties)
 
 def unregister():
     del bpy.types.Scene.BIMResourceProperties
+    del bpy.types.Scene.BIMResourceTreeProperties
