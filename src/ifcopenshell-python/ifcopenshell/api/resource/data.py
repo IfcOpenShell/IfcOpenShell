f88ac5de62cd76fe4cad7f29a19d55bcf5f4d9e8
Dion Moult <dion@thinkmoult.com>
2021-04-26 09:38:45 +1000

    Minor code review to assign / unassign resource products

--- src/ifcopenshell-python/ifcopenshell/api/resource/data.py
+++ src/ifcopenshell-python/ifcopenshell/api/resource/data.py
@@ -12,9 +12,12 @@ class Data:
         for resource in file.by_type("IfcResource"):
             data = resource.get_info()
             del data["OwnerHistory"]
-            data["RelatedObjects"] = []
+            data["IsNestedBy"] = []
             for rel in resource.IsNestedBy:
-                [data["RelatedObjects"].append(o.id()) for o in rel.RelatedObjects]
+                [data["IsNestedBy"].append(o.id()) for o in rel.RelatedObjects]
+            data["ResourceOf"] = []
+            for rel in resource.ResourceOf:
+                [data["ResourceOf"].append(o.id()) for o in rel.RelatedObjects]
             data["HasContext"] = resource.HasContext[0].RelatingContext.id() if resource.HasContext else None
             cls.resources[resource.id()] = data
         cls.is_loaded=True
