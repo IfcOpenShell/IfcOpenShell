fd4381035fb92470d5770103117206628f7cf992
bosonprojets <bosonprojets@gmail.com>
2021-04-25 22:59:01 +0000

    Unassign Resources Usecase added to the ifcopenshel.api

---
+++ src/ifcopenshell-python/ifcopenshell/api/resource/unassign_resource.py
@@ -0,0 +1,25 @@
+import ifcopenshell
+import ifcopenshell.api
+
+
+class Usecase:
+    def __init__(self, file, **settings):
+        self.file = file
+        self.settings = {
+            "relating_resource": None,
+            "related_object": None,
+        }
+        for key, value in settings.items():
+            self.settings[key] = value
+
+    def execute(self):
+        for rel in self.settings["related_object"].HasAssignments or []:
+            if not rel.is_a("IfcRelAssignsToResource") or rel.RelatingResource != self.settings["relating_resource"]:
+                continue
+            if len(rel.RelatedObjects) == 1:
+                return self.file.remove(rel)
+            related_objects = list(rel.RelatedObjects)
+            related_objects.remove(self.settings["related_object"])
+            rel.RelatedObjects = related_objects
+            ifcopenshell.api.run("owner.update_owner_history", self.file, **{"element": rel})
+            return rel
