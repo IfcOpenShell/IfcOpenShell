f8eb1c178a730cc8f2c843bf94af972dd70b428f
bosonprojets <bosonprojets@gmail.com>
2021-04-29 02:43:14 +0000

    Usecases to edit WorkCalendar Attributes in the ifcopenshell.api

--- src/ifcopenshell-python/ifcopenshell/api/sequence/add_work_time.py
+++ src/ifcopenshell-python/ifcopenshell/api/sequence/add_work_time.py
@@ -1,12 +1,12 @@
 class Usecase:
     def __init__(self, file, **settings):
         self.file = file
-        self.settings = {"work_calendar": None, "type": "WorkingTimes", "name": None}
+        self.settings = {"work_calendar": None, "time_type": None}
         for key, value in settings.items():
             self.settings[key] = value
 
     def execute(self):
-        work_time = self.file.create_entity("IfcWorkTime", **{"Name": self.settings["name"]})
+        work_time = self.file.create_entity("IfcWorkTime")
         if self.settings["type"] == "WorkingTimes":
             working_times = list(self.settings["work_calendar"].WorkingTimes or [])
             working_times.append(work_time)
