d3bb895c9d2ef472f7cbc101208171e77246e47e
Dion Moult <dion@thinkmoult.com>
2021-05-17 15:15:25 +1000

    No new functionality. Just throwing all drawing code in the drawing module. See #1153.

--- src/blenderbim/blenderbim/bim/module/drawing/__init__.py
+++ src/blenderbim/blenderbim/bim/module/drawing/__init__.py
@@ -1,10 +1,42 @@
 import bpy
-from . import ui, prop, operator, handler
+from . import ui, prop, operator, handler, gizmos
 
 classes = (
     operator.AddDrawing,
     operator.CreateDrawing,
     operator.AddAnnotation,
+    operator.CutSection,  # TODO: To deprecate
+    operator.AddSheet,
+    operator.OpenSheet,
+    operator.AddDrawingToSheet,
+    operator.CreateSheets,
+    operator.OpenView,
+    operator.OpenViewCamera,
+    operator.ActivateView,
+    operator.SelectDocIfcFile,
+    operator.GenerateReferences,
+    operator.ResizeText,
+    operator.AddVariable,
+    operator.RemoveVariable,
+    operator.PropagateTextData,
+    operator.RemoveDrawing,
+    operator.AddDrawingStyle,
+    operator.RemoveDrawingStyle,
+    operator.SaveDrawingStyle,
+    operator.ActivateDrawingStyle,
+    operator.EditVectorStyle,
+    operator.RemoveSheet,
+    operator.AddSchedule,
+    operator.RemoveSchedule,
+    operator.SelectScheduleFile,
+    operator.BuildSchedule,
+    operator.AddScheduleToSheet,
+    operator.AddDrawingStyleAttribute,
+    operator.RemoveDrawingStyleAttribute,
+    operator.RefreshDrawingList,
+    operator.CleanWireframes,
+    operator.CopyGrid,
+    operator.AddSectionsAnnotations,
     prop.Variable,
     prop.Drawing,
     prop.Schedule,
@@ -14,6 +46,17 @@ classes = (
     prop.BIMCameraProperties,
     prop.BIMTextProperties,
     ui.BIM_PT_camera,
+    ui.BIM_PT_drawings,
+    ui.BIM_PT_schedules,
+    ui.BIM_PT_sheets,
+    ui.BIM_PT_text,
+    ui.BIM_PT_annotation_utilities,
+    ui.BIM_UL_drawinglist,
+    gizmos.UglyDotGizmo,
+    gizmos.DotGizmo,
+    gizmos.DimensionLabelGizmo,
+    gizmos.ExtrusionGuidesGizmo,
+    gizmos.ExtrusionWidget,
 )
 
 
@@ -22,6 +65,7 @@ def register():
     bpy.types.Camera.BIMCameraProperties = bpy.props.PointerProperty(type=prop.BIMCameraProperties)
     bpy.types.TextCurve.BIMTextProperties = bpy.props.PointerProperty(type=prop.BIMTextProperties)
     bpy.app.handlers.load_post.append(handler.toggleDecorationsOnLoad)
+    bpy.app.handlers.depsgraph_update_pre.append(handler.depsgraph_update_pre_handler)
 
 
 def unregister():
@@ -29,3 +73,4 @@ def unregister():
     del bpy.types.Camera.BIMCameraProperties
     del bpy.types.TextCurve.BIMTextProperties
     bpy.app.handlers.load_post.remove(handler.toggleDecorationsOnLoad)
+    bpy.app.handlers.depsgraph_update_pre.remove(handler.depsgraph_update_pre_handler)
