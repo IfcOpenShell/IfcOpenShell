82692bb88de543dc1cb7d958a7cfb1fa09f7ff64
Dion Moult <dion@thinkmoult.com>
2021-05-19 17:59:13 +1000

    Adding a new type instance (like furniture, walls) now snaps it to the bottom of the storey you're working on. Nice.

--- src/blenderbim/blenderbim/bim/module/model/operator.py
+++ src/blenderbim/blenderbim/bim/module/model/operator.py
@@ -37,9 +37,11 @@ class AddTypeInstance(bpy.types.Operator):
         mesh.from_pydata(verts, edges, faces)
         obj = bpy.data.objects.new("Instance", mesh)
         obj.location = context.scene.cursor.location
-        context.view_layer.active_layer_collection.collection.objects.link(obj)
-        self.file = IfcStore.get_file()
-        instance_class = ifcopenshell.util.type.get_applicable_entities(tprops.ifc_class, self.file.schema)[0]
+        collection = bpy.context.view_layer.active_layer_collection.collection
+        collection.objects.link(obj)
+        collection_obj = bpy.data.objects.get(collection.name)
+        if collection_obj:
+            obj.location[2] = collection_obj.location[2] - min(v[2] for v in obj.bound_box)
         bpy.ops.bim.assign_class(obj=obj.name, ifc_class=instance_class)
         bpy.ops.bim.assign_type(relating_type=int(tprops.relating_type), related_object=obj.name)
         return {"FINISHED"}
