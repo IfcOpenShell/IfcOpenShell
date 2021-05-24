bfe9213f67daee1fb235ea385e8173a6ee6bda31
Dion Moult <dion@thinkmoult.com>
2021-05-24 19:28:47 +1000

    Dumb walls now auto calculate length, width, height, and simple volume quantities when being updated

--- src/blenderbim/blenderbim/bim/module/parametric/operator.py
+++ src/blenderbim/blenderbim/bim/module/parametric/operator.py
@@ -1,8 +1,12 @@
 import bpy
+import bmesh
 import ifcopenshell
 import ifcopenshell.api
+import ifcopenshell.util.unit
+import ifcopenshell.util.element
 import ifcopenshell.util.representation
 from blenderbim.bim.ifc import IfcStore
+from ifcopenshell.api.pset.data import Data as PsetData
 
 
 def generate_box(usecase_path, ifc_file, **settings):
@@ -29,10 +33,48 @@ def generate_box(usecase_path, ifc_file, **settings):
         )
 
 
+def calculate_dumb_quantities(usecase_path, ifc_file, **settings):
+    unit_scale = ifcopenshell.util.unit.calculate_unit_scale(ifc_file)
+    obj = settings["blender_object"]
+    product = ifc_file.by_id(obj.BIMObjectProperties.ifc_definition_id)
+    parametric = ifcopenshell.util.element.get_psets(product).get("EPset_Parametric")
+    if not parametric or parametric["Engine"] != "BlenderBIM.DumbWall":
+        return
+    qto = ifcopenshell.api.run(
+        "pset.add_qto", ifc_file, should_run_listeners=False, product=product, Name="Qto_WallBaseQuantities"
+    )
+    length = obj.dimensions[0] / unit_scale
+    width = obj.dimensions[1] / unit_scale
+    height = obj.dimensions[2] / unit_scale
+
+    if product.HasOpenings:
+        # TODO: calculate gross / net
+        gross_volume = 0
+        net_volume = 0
+    else:
+        bm = bmesh.new()
+        bm.from_mesh(obj.data)
+        gross_volume = bm.calc_volume()
+        net_volume = gross_volume
+        bm.free()
+
+    ifcopenshell.api.run("pset.edit_qto", ifc_file, should_run_listeners=False, qto=qto, Properties={
+        "Length": round(length, 2),
+        "Width": round(width, 2),
+        "Height": round(height, 2),
+        "GrossVolume": round(gross_volume, 2),
+        "NetVolume": round(net_volume, 2)
+    })
+    PsetData.load(ifc_file, obj.BIMObjectProperties.ifc_definition_id)
+
+
 class ActivateParametricEngine(bpy.types.Operator):
     bl_idname = "bim.activate_parametric_engine"
     bl_label = "Activate Parametric Engine"
 
     def execute(self, context):
         ifcopenshell.api.add_post_listener("geometry.add_representation", IfcStore.get_file(), generate_box)
+        ifcopenshell.api.add_post_listener(
+            "geometry.add_representation", IfcStore.get_file(), calculate_dumb_quantities
+        )
         return {"FINISHED"}
