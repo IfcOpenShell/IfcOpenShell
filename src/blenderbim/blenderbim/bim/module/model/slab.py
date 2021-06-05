766e71fbf9553c1b6d09099eb85cd30d6879a2f6
Dion Moult <dion@thinkmoult.com>
2021-06-05 19:47:42 +1000

    Minor fixes to slab/wall syncing and stability in changing thicknesses.

--- src/blenderbim/blenderbim/bim/module/model/slab.py
+++ src/blenderbim/blenderbim/bim/module/model/slab.py
@@ -127,9 +127,8 @@ class DumbSlabPlaner:
         self.unit_scale = ifcopenshell.util.unit.calculate_unit_scale(ifc_file)
         layer = settings["layer"]
         thickness = settings["attributes"].get("LayerThickness")
-        if thickness is None or layer.LayerThickness == thickness:
+        if thickness is None:
             return
-        delta_thickness = thickness - layer.LayerThickness
         for layer_set in layer.ToMaterialLayerSet:
             total_thickness = sum([l.LayerThickness for l in layer_set.MaterialLayers])
             if not total_thickness:
@@ -142,27 +141,21 @@ class DumbSlabPlaner:
                         if not rel.is_a("IfcRelAssociatesMaterial"):
                             continue
                         for element in rel.RelatedObjects:
-                            self.change_thickness(element, delta_thickness)
+                            self.change_thickness(element, thickness)
                 else:
                     for rel in inverse.AssociatedTo:
                         for element in rel.RelatedObjects:
-                            self.change_thickness(element, delta_thickness)
+                            self.change_thickness(element, thickness)
 
     def regenerate_from_type(self, usecase_path, ifc_file, settings):
         self.unit_scale = ifcopenshell.util.unit.calculate_unit_scale(ifc_file)
         new_material = ifcopenshell.util.element.get_material(settings["relating_type"])
         if not new_material.is_a("IfcMaterialLayerSet"):
             return
-        obj = IfcStore.get_element(settings["related_object"].id())
-        if not obj:
-            return
-        current_thickness = obj.dimensions.z / self.unit_scale
         new_thickness = sum([l.LayerThickness for l in new_material.MaterialLayers])
-        if current_thickness == new_thickness:
-            return
-        self.change_thickness(settings["related_object"], new_thickness - current_thickness)
+        self.change_thickness(settings["related_object"], new_thickness)
 
-    def change_thickness(self, element, delta_thickness):
+    def change_thickness(self, element, thickness):
         parametric = ifcopenshell.util.element.get_psets(element).get("EPset_Parametric")
         if not parametric or parametric["Engine"] != "BlenderBIM.DumbSlab":
             return
@@ -171,10 +164,14 @@ class DumbSlabPlaner:
         if not obj:
             return
 
+        delta_thickness = (thickness * self.unit_scale) - obj.dimensions.z
+        if round(delta_thickness, 2) == 0:
+            return
+
         modifier = [m for m in obj.modifiers if m.type == "SOLIDIFY"]
         if modifier:
             modifier = modifier[0]
         else:
             pass
-        modifier.thickness += delta_thickness * self.unit_scale
-        obj.location[2] -= delta_thickness * self.unit_scale
+        modifier.thickness += delta_thickness
+        obj.location[2] -= delta_thickness
