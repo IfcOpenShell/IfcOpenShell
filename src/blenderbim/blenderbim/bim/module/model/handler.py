aa3b7030fe55dbe457a7b45b075d8ff364921e5a
Dion Moult <dion@thinkmoult.com>
2021-06-07 15:53:12 +1000

    Dumb slabs now auto calculate new quantities when edited

--- src/blenderbim/blenderbim/bim/module/model/handler.py
+++ src/blenderbim/blenderbim/bim/module/model/handler.py
@@ -36,6 +36,9 @@ def load_post(*args):
     ifcopenshell.api.add_post_listener(
         "geometry.add_representation", "BlenderBIM.DumbSlab.GenerateFootprint", slab.generate_footprint
     )
+    ifcopenshell.api.add_post_listener(
+        "geometry.add_representation", "BlenderBIM.DumbSlab.CalculateQuantities", slab.calculate_quantities
+    )
     ifcopenshell.api.add_pre_listener(
         "material.edit_layer", "BlenderBIM.DumbSlab.RegenerateFromLayer", slab.DumbSlabPlaner().regenerate_from_layer
     )
