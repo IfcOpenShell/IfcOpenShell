42f5f025152fecbcbb666565e63549db89e15799
Dion Moult <dion@thinkmoult.com>
2021-06-29 11:57:47 +1000

    Ensure that files are always passed when entities are wrapped

--- src/ifcopenshell-python/ifcopenshell/entity_instance.py
+++ src/ifcopenshell-python/ifcopenshell/entity_instance.py
@@ -48,22 +48,21 @@ class entity_instance(object):
         >>> #423=IfcProductDefinitionShape($,$,(#409,#421))
     """
 
-    def __init__(self, e, file=None):
+    def __init__(self, e, file):
         if isinstance(e, tuple):
             e = ifcopenshell_wrapper.new_IfcBaseClass(*e)
         super(entity_instance, self).__setattr__("wrapped_data", e)
-        if file is not None:
-            self.wrapped_data.file = file
+        self.wrapped_data.file = file
 
     def __getattr__(self, name):
         INVALID, FORWARD, INVERSE = range(3)
         attr_cat = self.wrapped_data.get_attribute_category(name)
         if attr_cat == FORWARD:
             return entity_instance.wrap_value(
-                self.wrapped_data.get_argument(self.wrapped_data.get_argument_index(name))
+                self.wrapped_data.get_argument(self.wrapped_data.get_argument_index(name)), self.wrapped_data.file
             )
         elif attr_cat == INVERSE:
-            return entity_instance.wrap_value(self.wrapped_data.get_inverse(name))
+            return entity_instance.wrap_value(self.wrapped_data.get_inverse(name), self.wrapped_data.file)
         else:
             raise AttributeError(
                 "entity instance of type '%s' has no attribute '%s'" % (self.wrapped_data.is_a(), name)
@@ -79,9 +78,9 @@ class entity_instance(object):
             return value
 
     @staticmethod
-    def wrap_value(v):
+    def wrap_value(v, file):
         def wrap(e):
-            return entity_instance(e)
+            return entity_instance(e, file)
 
         def is_instance(e):
             return isinstance(e, ifcopenshell_wrapper.entity_instance)
@@ -126,7 +125,7 @@ class entity_instance(object):
     def __getitem__(self, key):
         if key < 0 or key >= len(self):
             raise IndexError("Attribute index {} out of range for instance of type {}".format(key, self.is_a()))
-        return entity_instance.wrap_value(self.wrapped_data.get_argument(key))
+        return entity_instance.wrap_value(self.wrapped_data.get_argument(key), self.wrapped_data.file)
 
     def __setitem__(self, idx, value):
         attr_type = real_attr_type = self.attribute_type(idx).title().replace(" ", "")
@@ -276,7 +275,7 @@ class entity_instance(object):
         return return_type(_())
 
     __dict__ = property(get_info)
-    
+
     def get_info_2(self, include_identifier=True, recursive=False, return_type=dict, ignore=()):
         assert include_identifier
         assert recursive
