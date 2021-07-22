8071e079b37c3ced99768a87a1aeaf83aa93ce3d
Dion Moult <dion@thinkmoult.com>
2021-07-22 17:40:50 +1000

    New system module with UI to browse systems in an IFC.

--- src/ifcopenshell-python/ifcopenshell/api/type/data.py
+++ src/ifcopenshell-python/ifcopenshell/api/type/data.py
@@ -1,18 +1,40 @@
 class Data:
     products = {}
+    types = {}
 
     @classmethod
     def purge(cls):
         cls.products = {}
+        cls.types = {}
 
     @classmethod
     def load(cls, file, product_id):
         if not file:
             return
+        cls.file = file
         product = file.by_id(product_id)
-        if file.schema == "IFC2X3" and not hasattr(product, "IsDefinedBy"):
+        if product.is_a("IfcTypeObject"):
+            cls.load_type(product_id)
+        else:
+            cls.load_product(product_id)
+
+    @classmethod
+    def load_type(cls, product_id):
+        product = cls.file.by_id(product_id)
+        cls.types[product_id] = None
+        if cls.file.schema == "IFC2X3":
+            if hasattr(product, "ObjectTypeOf"):
+                cls.types[product_id] = [o.id() for o in product.ObjectTypeOf[0].RelatedObjects]
+        else:
+            if hasattr(product, "Types"):
+                cls.types[product_id] = [o.id() for o in product.Types[0].RelatedObjects]
+
+    @classmethod
+    def load_product(cls, product_id):
+        product = cls.file.by_id(product_id)
+        if cls.file.schema == "IFC2X3" and not hasattr(product, "IsDefinedBy"):
             cls.products[product_id] = None
-        elif file.schema != "IFC2X3" and not hasattr(product, "IsTypedBy"):
+        elif cls.file.schema != "IFC2X3" and not hasattr(product, "IsTypedBy"):
             cls.products[product_id] = None
         elif hasattr(product, "IsTypedBy") and product.IsTypedBy:
             type = product.IsTypedBy[0].RelatingType
