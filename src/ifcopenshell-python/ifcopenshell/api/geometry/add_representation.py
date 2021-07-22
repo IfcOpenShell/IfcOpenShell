d2a1e0193bcce1a0949a7a824b03614687e45516
Dion Moult <dion@thinkmoult.com>
2021-07-22 20:41:40 +1000

    Fix #1574. Fix #1578. Bug where deprecated functions were used in Python 3.9 for drawing generation.

--- src/ifcopenshell-python/ifcopenshell/api/geometry/add_representation.py
+++ src/ifcopenshell-python/ifcopenshell/api/geometry/add_representation.py
@@ -204,12 +204,12 @@ class Usecase:
     def create_variable_representation(self):
         if isinstance(self.settings["geometry"], bpy.types.Curve):
             return self.create_curve3d_representation()
+        elif isinstance(self.settings["geometry"], bpy.types.Camera):
+            return self.create_camera_block_representation()
         elif not len(self.settings["geometry"].polygons):
             return self.create_curve3d_representation()
         elif self.settings["is_point_cloud"]:
             return self.create_point_cloud_representation()
-        elif isinstance(self.settings["geometry"], bpy.types.Camera):
-            return self.create_camera_block_representation()
         elif self.settings["ifc_representation_class"] == "IfcExtrudedAreaSolid/IfcRectangleProfileDef":
             return self.create_rectangle_extrusion_representation()
         elif self.settings["ifc_representation_class"] == "IfcExtrudedAreaSolid/IfcCircleProfileDef":
