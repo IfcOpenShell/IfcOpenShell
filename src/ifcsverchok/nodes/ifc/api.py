6337d04efca5d692aa18655b5d49d4b13602efa3
Dion Moult <dion@thinkmoult.com>
2021-07-25 18:42:28 +1000

    IfcSverchok API node now actually executes API calls and has descriptive tooltips

--- src/ifcsverchok/nodes/ifc/api.py
+++ src/ifcsverchok/nodes/ifc/api.py
@@ -5,34 +5,52 @@ import ifcsverchok.helper
 from bpy.props import StringProperty, EnumProperty
 from sverchok.node_tree import SverchCustomTreeNode
 from sverchok.data_structure import updateNode
-#from blenderbim.bim.module.root.prop import getIfcClasses, getIfcProducts, refreshClasses, refreshPredefinedTypes
+
+
+def update_usecase(self, context):
+    module_usecase = self.get_module_usecase()
+    if module_usecase:
+        self.generate_node(*module_usecase)
+
+
+class SvIfcTooltip(bpy.types.Operator):
+    bl_idname = "node.sv_ifc_tooltip"
+    bl_label = "IFC Info"
+    tooltip: bpy.props.StringProperty()
+
+    @classmethod
+    def description(cls, context, properties):
+        return properties.tooltip
+
+    def execute(self, context):
+        return {"FINISHED"}
 
 
 class SvIfcApi(bpy.types.Node, SverchCustomTreeNode, ifcsverchok.helper.SvIfcCore):
     bl_idname = "SvIfcApi"
     bl_label = "IFC API"
-    usecase: StringProperty(name="Usecase", update=updateNode)
-    #schema: StringProperty(name="schema", update=updateNode, default="IFC4")
-    #ifc_product: EnumProperty(items=getIfcProducts, name="Products", update=refreshClasses)
-    #ifc_class: EnumProperty(items=getIfcClasses, name="Class", update=refreshPredefinedTypes)
-    #custom_ifc_class: StringProperty(name="Custom Ifc Class", update=updateNode)
+    tooltip: StringProperty(name="Tooltip")
+    usecase: StringProperty(name="Usecase", update=update_usecase)
 
     def sv_init(self, context):
         self.inputs.new("SvStringsSocket", "usecase").prop_name = "usecase"
-        #self.inputs.new("SvStringsSocket", "schema").prop_name = "schema"
-        #self.inputs.new("SvStringsSocket", "ifc_product").prop_name = "ifc_product"
-        #self.inputs.new("SvStringsSocket", "ifc_class").prop_name = "ifc_class"
-        #self.inputs.new("SvStringsSocket", "custom_ifc_class").prop_name = "custom_ifc_class"
         self.outputs.new("SvVerticesSocket", "file")
 
+    def draw_buttons(self, context, layout):
+        op = layout.operator("node.sv_ifc_tooltip", text="", icon="QUESTION", emboss=False)
+        op.tooltip = self.tooltip
+
     def process(self):
-        print('process')
-        #self.sv_input_names = ["file", "ifc_product", "ifc_class", "custom_ifc_class"]
+        print("process")
+        module_usecase = self.get_module_usecase()
+        if module_usecase:
+            self.sv_input_names = [i.name for i in self.inputs]
+            super().process()
+
+    def get_module_usecase(self):
         usecase = self.inputs["usecase"].sv_get()[0][0]
         if usecase:
-            self.generate_node(*usecase.split("."))
-        self.sv_input_names = ["usecase"]
-        super().process()
+            return usecase.split(".")
 
     def generate_node(self, module, usecase):
         try:
@@ -43,23 +61,28 @@ class SvIfcApi(bpy.types.Node, SverchCustomTreeNode, ifcsverchok.helper.SvIfcCor
         while len(self.inputs) > 1:
             self.inputs.remove(self.inputs[-1])
 
+        self.tooltip = ""
         for name, data in node_data["inputs"].items():
-            setattr(SvIfcApi, name, StringProperty(name=name))
+            setattr(SvIfcApi, name, StringProperty(name=name, update=updateNode))
             self.inputs.new("SvStringsSocket", name).prop_name = name
+            if "default" in data:
+                self.tooltip = f"{name} ({data['default']}): {data['description']}\n"
+            else:
+                self.tooltip = f"{name}: {data['description']}\n"
+        self.tooltip = self.tooltip.strip()
 
-    #def process_ifc(self, file, ifc_product, ifc_class, custom_ifc_class):
-    def process_ifc(self, usecase):
-        print('run')
-        #self.outputs["file"].sv_set([ifcopenshell.api.run("project.create_file", version=schema)])
-        #if custom_ifc_class:
-        #    self.outputs["entity"].sv_set([file.by_type(custom_ifc_class)])
-        #else:
-        #    self.outputs["entity"].sv_set([file.by_type(ifc_class)])
+    def process_ifc(self, usecase, *setting_values):
+        if usecase:
+            settings = dict(zip(self.sv_input_names[1:], setting_values))
+            settings = {k: v for k, v in settings.items() if v != ""}
+            self.outputs["file"].sv_set([ifcopenshell.api.run(usecase, **settings)])
 
 
 def register():
+    bpy.utils.register_class(SvIfcTooltip)
     bpy.utils.register_class(SvIfcApi)
 
 
 def unregister():
     bpy.utils.unregister_class(SvIfcApi)
+    bpy.utils.unregister_class(SvIfcTooltip)
