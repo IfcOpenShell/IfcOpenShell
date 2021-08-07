794faee13a9944a20742cfb749544a5c261dec71
Dion Moult <dion@thinkmoult.com>
2021-08-07 21:15:36 +1000

    WIP licenses. See #1082.

--- src/ifc2ca/scriptSalome.py
+++ src/ifc2ca/scriptSalome.py
@@ -1,3 +1,22 @@
+
+# Ifc2CA - IFC Code_Aster utility
+# Copyright (C) 2020, 2021 Ioannis P. Christovasilis <ipc@aethereng.com>
+#
+# This file is part of Ifc2CA.
+#
+# Ifc2CA is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ifc2CA is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Ifc2CA.  If not, see <http://www.gnu.org/licenses/>.
+
 from __future__ import division
 from __future__ import print_function
 import os
--- src/ifcpatch/ifcpatch/recipes/Optimise.py
+++ src/ifcpatch/ifcpatch/recipes/Optimise.py
@@ -1,3 +1,22 @@
+
+# IfcPatch - IFC patching utiliy
+# Copyright (C) 2020, 2021 Dion Moult <dion@thinkmoult.com>
+#
+# This file is part of IfcPatch.
+#
+# IfcPatch is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# IfcPatch is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with IfcPatch.  If not, see <http://www.gnu.org/licenses/>.
+
 import ifcopenshell
 import ifcopenshell.util.element
 from toposort import toposort_flatten as toposort
