7abccbd713be70bd9720d5d5e26bc156fa0c05ff
Dion Moult <dion@thinkmoult.com>
2021-08-08 20:56:31 +1000

    Deleting a material set from a type now also purges usages from type instances

--- src/ifcopenshell-python/ifcopenshell/api/material/unassign_material.py
+++ src/ifcopenshell-python/ifcopenshell/api/material/unassign_material.py
@@ -1,4 +1,5 @@
 import ifcopenshell
+import ifcopenshell.util.element
 
 
 class Usecase:
@@ -9,11 +10,26 @@ class Usecase:
             self.settings[key] = value
 
     def execute(self):
+        if self.settings["product"].is_a("IfcTypeObject"):
+            material = ifcopenshell.util.element.get_material(self.settings["product"])
+            if material.is_a() in ["IfcMaterialLayerSet", "IfcMaterialProfileSet"]:
+                for inverse in self.file.get_inverse(material):
+                    if self.file.schema == "IFC2X3":
+                        if not inverse.is_a("IfcMaterialLayerSetUsage"):
+                            continue
+                        for inverse2 in self.file.get_inverse(inverse):
+                            if inverse2.is_a("IfcRelAssociatesMaterial"):
+                                self.file.remove(inverse2)
+                    else:
+                        if not inverse.is_a("IfcMaterialUsageDefinition"):
+                            continue
+                        for rel in inverse.AssociatedTo:
+                            self.file.remove(rel)
+                    self.file.remove(inverse)
+
         for rel in self.settings["product"].HasAssociations:
             if rel.is_a("IfcRelAssociatesMaterial"):
-                if rel.RelatingMaterial.is_a("IfcMaterialLayerSetUsage") or rel.RelatingMaterial.is_a(
-                    "IfcMaterialProfileSetUsage"
-                ):
+                if rel.RelatingMaterial.is_a() in ["IfcMaterialLayerSetUsage", "IfcMaterialProfileSetUsage"]:
                     self.file.remove(rel.RelatingMaterial)
                 if len(rel.RelatedObjects) == 1:
                     self.file.remove(rel)
