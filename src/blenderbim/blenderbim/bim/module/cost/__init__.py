31ab6393b3de5755a282786f68fb02a08fb9befe
Dion Moult <dion@thinkmoult.com>
2021-08-09 17:12:23 +1000

    You can now import cost schedules as CSVs into IFC in Blender

--- src/blenderbim/blenderbim/bim/module/cost/__init__.py
+++ src/blenderbim/blenderbim/bim/module/cost/__init__.py
@@ -33,6 +33,7 @@ classes = (
     operator.CopyCostItemValues,
     operator.SelectCostItemProducts,
     operator.SelectCostScheduleProducts,
+    operator.ImportCostScheduleCsv,
     prop.CostItem,
     prop.BIMCostProperties,
     ui.BIM_PT_cost_schedules,
@@ -40,9 +41,15 @@ classes = (
 )
 
 
+def menu_func_import(self, context):
+    self.layout.operator(operator.ImportCostScheduleCsv.bl_idname, text="Cost Schedule (.csv)")
+
+
 def register():
     bpy.types.Scene.BIMCostProperties = bpy.props.PointerProperty(type=prop.BIMCostProperties)
+    bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
 
 
 def unregister():
     del bpy.types.Scene.BIMCostProperties
+    bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
