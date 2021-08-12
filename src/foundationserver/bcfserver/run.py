7e36d7157824be6470e139412c98935591ea2e6c
Prabhat Singh <59395410+TestPrab@users.noreply.github.com>
2021-08-12 12:40:25 +0530

    Added Routes to BCF (#1644)

--- src/foundationserver/bcfserver/run.py
+++ src/foundationserver/bcfserver/run.py
@@ -10,14 +10,14 @@ login_manager = LoginManager(app)
 bcrypt = Bcrypt(app)
 app.config["SECRET_KEY"] = "f613729206685405cde0e388"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlite.db"
-login_manager.login_view = "website_obj.login_page"
+login_manager.login_view = "foundation_obj.login_page"
 login_manager.login_message_category = "info"
 
-from website.routes import website_obj
+from foundation.routes import foundation_obj
 from bcf.routes import bcf
 
 
-app.register_blueprint(website_obj)
+app.register_blueprint(foundation_obj)
 app.register_blueprint(bcf)
 
 
