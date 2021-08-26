3184f3a9d1f4f58db3c8e73b2839d55358e5e2ce
Gorgious <nathan.hild@gmail.com>
2021-08-26 10:06:33 +0200

    Display project units instead of hardcoded ones

--- src/blenderbim/blenderbim/bim/module/georeference/ui.py
+++ src/blenderbim/blenderbim/bim/module/georeference/ui.py
@@ -56,14 +56,6 @@ class BIM_PT_gis(Panel):
 
         draw_attributes(props.projected_crs, self.layout)
 
-        row = self.layout.row(align=True)
-        row.prop(props, "map_unit_type", text="MapUnit")
-        if props.map_unit_type == "IfcSIUnit":
-            row.prop(props, "map_unit_si", text="")
-        elif props.map_unit_type == "IfcConversionBasedUnit":
-            row.prop(props, "map_unit_imperial", text="")
-        row.prop(props, "is_map_unit_null", icon="RADIOBUT_OFF" if props.is_map_unit_null else "RADIOBUT_ON", text="")
-
         row = self.layout.row()
         row.label(text="Map Conversion", icon="GRID")
 
