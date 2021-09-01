IfcOpenShell/IfcOpenShell

commit 67c055579c31b58281c82f8c14609e3960d3b733

Various tweaks and fixes (#1683)
* Use operator's poll method to ensure correct execution

* Add utilities to get active collection item

* Minor fixes
zombie code
list comprehensions
typos
cleanup
tooltips, descriptions
ensure file extension in folder operator
use custom UI List instead of generic one
ensure active object stays the same after operator execution
use passed context instead of bpy

* Jump through hoops to not display empty dynamic enums

* Group mesh calculation methods in helper module
If not using bmesh, the polygon selection status
is updated only when getting out of Edit mode.
Using bmesh we can get the values in realtime

* Remove unused arg name in dosctring

src/blenderbim/blenderbim/bim/module/drawing/operator.py
@@ -60,6 +60,10 @@ class AddDrawing(bpy.types.Operator):
     bl_label = "Add Drawing"
     bl_options = {"REGISTER", "UNDO"}
 
+    @classmethod
+    def poll(cls, context):
+        return IfcStore.get_file()
+
     def execute(self, context):
         return IfcStore.execute_ifc_operator(self, context)
 
@@ -502,12 +506,14 @@ class AddAnnotation(bpy.types.Operator):
     obj_name: bpy.props.StringProperty()
     data_type: bpy.props.StringProperty()
 
+    @classmethod
+    def poll(cls, context):
+        return IfcStore.get_file() and context.scene.camera
+
     def execute(self, context):
         return IfcStore.execute_ifc_operator(self, context)
 
     def _execute(self, context):
-        if not context.scene.camera:
-            return {"FINISHED"}
         subcontext = ifcopenshell.util.representation.get_context(
             IfcStore.get_file(), "Plan", "Annotation", context.scene.camera.data.BIMCameraProperties.target_view
         )
@@ -563,7 +569,7 @@ class OpenSheet(bpy.types.Operator):
         open_with_user_command(
             context.preferences.addons["blenderbim"].preferences.svg_command,
             os.path.join(
-                context.scene.BIMProperties.data_dir, "sheets", props.sheets[props.active_sheet_index].name + ".svg"
+                context.scene.BIMProperties.data_dir, "sheets", props.active_sheet.name + ".svg"
             ),
         )
         return {"FINISHED"}
@@ -581,9 +587,9 @@ class AddDrawingToSheet(bpy.types.Operator):
         sheet_builder.data_dir = context.scene.BIMProperties.data_dir
         try:
             sheet_builder.add_drawing(
-                props.drawings[props.active_drawing_index].name, props.sheets[props.active_sheet_index].name
+                props.drawings.active_drawing.name, props.active_sheet.name
             )
-        except:
+        except FileNotFoundError:
             self.report({"ERROR"}, "Drawings need to be created before being added to a sheet")
         return {"FINISHED"}
 
@@ -596,7 +602,7 @@ class CreateSheets(bpy.types.Operator):
     def execute(self, context):
         scene = context.scene
         props = scene.DocProperties
-        name = props.sheets[props.active_sheet_index].name
+        name = props.active_sheet.name
         sheet_builder = sheeter.SheetBuilder()
         sheet_builder.data_dir = scene.BIMProperties.data_dir
         sheet_builder.build(name)
@@ -650,18 +656,23 @@ class OpenView(bpy.types.Operator):
 
 
 class OpenViewCamera(bpy.types.Operator):
+    """Select this drawing's camera object and expand its drawing properties"""
     bl_idname = "bim.open_view_camera"
     bl_label = "Open View Camera"
     bl_options = {"REGISTER", "UNDO"}
     view_name: bpy.props.StringProperty()
 
+    @classmethod
+    def poll(cls, context):
+        return bpy.context.object.mode == "OBJECT"
+
     def execute(self, context):
-        new_drawing_index = context.scene.DocProperties.drawings.find(self.view_name)
-        context.scene.DocProperties.active_drawing_index = new_drawing_index
-        drawing = context.scene.DocProperties.drawings[new_drawing_index]
-        context.view_layer.objects.active = drawing.camera
+        doc_props = context.scene.DocProperties
+        doc_props.active_drawing_index = doc_props.drawings.find(self.view_name)
+        drawing = doc_props.active_drawing
         bpy.ops.object.select_all(action="DESELECT")
         drawing.camera.select_set(True)
+        context.view_layer.objects.active = drawing.camera
         for area in context.screen.areas:
             if area.ui_type == "PROPERTIES":
                 for space in area.spaces:
@@ -712,6 +723,7 @@ class SelectDocIfcFile(bpy.types.Operator):
     bl_idname = "bim.select_doc_ifc_file"
     bl_label = "Select Documentation IFC File"
     bl_options = {"REGISTER", "UNDO"}
+    filter_glob: bpy.props.StringProperty(default="*.ifc;*.ifczip;*.ifcxml", options={"HIDDEN"})
     filepath: bpy.props.StringProperty(subtype="FILE_PATH")
     index: bpy.props.IntProperty()
 
@@ -1059,7 +1071,7 @@ class SelectScheduleFile(bpy.types.Operator):
 
     def execute(self, context):
         props = context.scene.DocProperties
-        props.schedules[props.active_schedule_index].file = self.filepath
+        props.active_schedule.file = self.filepath
         return {"FINISHED"}
 
     def invoke(self, context, event):
@@ -1071,9 +1083,13 @@ class BuildSchedule(bpy.types.Operator):
     bl_idname = "bim.build_schedule"
     bl_label = "Build Schedule"
 
+    @classmethod
+    def poll(cls, context):
+        return context.scene.DocProperties.active_schedule.file
+
     def execute(self, context):
         props = context.scene.DocProperties
-        schedule = props.schedules[props.active_schedule_index]
+        schedule = props.active_schedule
         schedule_creator = scheduler.Scheduler()
         outfile = os.path.join(context.scene.BIMProperties.data_dir, "schedules", schedule.name + ".svg")
         schedule_creator.schedule(schedule.file, outfile)
@@ -1092,7 +1108,7 @@ class AddScheduleToSheet(bpy.types.Operator):
         sheet_builder = sheeter.SheetBuilder()
         sheet_builder.data_dir = context.scene.BIMProperties.data_dir
         sheet_builder.add_schedule(
-            props.schedules[props.active_schedule_index].name, props.sheets[props.active_sheet_index].name
+            props.active_schedule.name, props.active_sheet.name
         )
         return {"FINISHED"}
 
@@ -1143,13 +1159,12 @@ class CleanWireframes(bpy.types.Operator):
     bl_options = {"REGISTER", "UNDO"}
 
     def execute(self, context):
-        objects = context.scene.objects
         if context.selected_objects:
             objects = context.selected_objects
-        for obj in objects:
-            if not isinstance(obj.data, bpy.types.Mesh):
-                continue
-            if "EDGE_SPLIT" not in [m.type for m in obj.modifiers]:
+        else:
+            objects = context.scene.objects
+        for obj in (o for o in objects if o.type == "MESH"):
+            if "EDGE_SPLIT" not in (m.type for m in obj.modifiers):
                 obj.modifiers.new("EdgeSplit", "EDGE_SPLIT")
         return {"FINISHED"}
 
@@ -1159,11 +1174,13 @@ class CopyGrid(bpy.types.Operator):
     bl_label = "Add Grid"
     bl_options = {"REGISTER", "UNDO"}
 
+    @classmethod
+    def poll(cls, context):
+        return helper.get_active_drawing(context.scene)[0] is not None
+
     def execute(self, context):
         proj_coll = helper.get_project_collection(context.scene)
         view_coll, camera = helper.get_active_drawing(context.scene)
-        if view_coll is None:
-            return {"CANCELLED"}
         is_ortho = camera.data.type == "ORTHO"
         bounds = helper.ortho_view_frame(camera.data) if is_ortho else None
         clipping = is_ortho and camera.data.BIMCameraProperties.target_view in ("PLAN_VIEW", "REFLECTED_PLAN_VIEW")
@@ -1240,13 +1257,15 @@ class AddSectionsAnnotations(bpy.types.Operator):
     bl_label = "Add Sections"
     bl_options = {"REGISTER", "UNDO"}
 
+    @classmethod
+    def poll(cls, context):        
+        camera = helper.get_active_drawing(context.scene)[1]
+        return camera and camera.data.type == "ORTHO"
+
     def execute(self, context):
         scene = context.scene
         view_coll, camera = helper.get_active_drawing(scene)
-        is_ortho = camera.data.type == "ORTHO"
-        if not is_ortho:
-            return {"CANCELLED"}
-        bounds = helper.ortho_view_frame(camera.data) if is_ortho else None
+        bounds = helper.ortho_view_frame(camera.data)
 
         drawings = [
             d

src/blenderbim/blenderbim/bim/module/bimtester/operator.py
@@ -39,6 +39,11 @@ class ExecuteBIMTester(bpy.types.Operator):
     bl_idname = "bim.execute_bim_tester"
     bl_label = "Execute BIMTester"
 
+    @classmethod
+    def poll(cls, context):
+        props = context.scene.BimTesterProperties
+        return props.ifc_file and props.feature
+
     def execute(self, context):
         props = context.scene.BimTesterProperties
 

src/blenderbim/blenderbim/bim/module/clash/operator.py
@@ -25,6 +25,7 @@ import logging
 import numpy as np
 from mathutils import Matrix
 from math import radians
+from blenderbim.bim.ifc import IfcStore
 
 
 class ExportClashSets(bpy.types.Operator):
@@ -124,7 +125,7 @@ class AddClashSource(bpy.types.Operator):
     group: bpy.props.StringProperty()
 
     def execute(self, context):
-        clash_set = context.scene.BIMClashProperties.clash_sets[context.scene.BIMClashProperties.active_clash_set_index]
+        clash_set = context.scene.BIMClashProperties.active_clash_set
         source = getattr(clash_set, self.group).add()
         return {"FINISHED"}
 
@@ -137,7 +138,7 @@ class RemoveClashSource(bpy.types.Operator):
     group: bpy.props.StringProperty()
 
     def execute(self, context):
-        clash_set = context.scene.BIMClashProperties.clash_sets[context.scene.BIMClashProperties.active_clash_set_index]
+        clash_set = context.scene.BIMClashProperties.active_clash_set
         getattr(clash_set, self.group).remove(self.index)
         return {"FINISHED"}
 
@@ -152,7 +153,7 @@ class SelectClashSource(bpy.types.Operator):
     group: bpy.props.StringProperty()
 
     def execute(self, context):
-        clash_set = context.scene.BIMClashProperties.clash_sets[context.scene.BIMClashProperties.active_clash_set_index]
+        clash_set = context.scene.BIMClashProperties.active_clash_set
         getattr(clash_set, self.group)[self.index].name = self.filepath
         return {"FINISHED"}
 
@@ -276,9 +277,7 @@ class SelectIfcClashResults(bpy.types.Operator):
         self.filepath = bpy.path.ensure_ext(self.filepath, ".json")
         with open(self.filepath) as f:
             clash_sets = json.load(f)
-        clash_set_name = context.scene.BIMClashProperties.clash_sets[
-            context.scene.BIMClashProperties.active_clash_set_index
-        ].name
+        clash_set_name = context.scene.BIMClashProperties.active_clash_set.name
         global_ids = []
         for clash_set in clash_sets:
             if clash_set["name"] != clash_set_name:
@@ -303,6 +302,10 @@ class SmartClashGroup(bpy.types.Operator):
     bl_options = {"REGISTER", "UNDO"}
     filepath: bpy.props.StringProperty(subtype="FILE_PATH")
 
+    @classmethod
+    def poll(cls, context):
+        return context.scene.BIMClashProperties.clash_results_path
+
     def execute(self, context):
         import ifcclash
 
@@ -326,9 +329,7 @@ class SmartClashGroup(bpy.types.Operator):
         with open(save_path, "w") as f:
             f.write(json.dumps(smart_grouped_clashes))
 
-        clash_set_name = context.scene.BIMClashProperties.clash_sets[
-            context.scene.BIMClashProperties.active_clash_set_index
-        ].name
+        clash_set_name = context.scene.BIMClashProperties.active_clash_set.name
 
         # Reset the list of smart_clash_groups for the UI
         context.scene.BIMClashProperties.smart_clash_groups.clear()
@@ -355,12 +356,14 @@ class LoadSmartGroupsForActiveClashSet(bpy.types.Operator):
     bl_label = "Load Smart Groups for Active Clash Set"
     bl_options = {"REGISTER", "UNDO"}
 
+    @classmethod
+    def poll(cls, context):
+        return context.scene.BIMClashProperties.active_clash_set
+
     def execute(self, context):
         smart_groups_path = bpy.path.ensure_ext(context.scene.BIMClashProperties.smart_grouped_clashes_path, ".json")
 
-        clash_set_name = context.scene.BIMClashProperties.clash_sets[
-            context.scene.BIMClashProperties.active_clash_set_index
-        ].name
+        clash_set_name = context.scene.BIMClashProperties.active_clash_set.name
 
         with open(smart_groups_path) as f:
             smart_grouped_clashes = json.load(f)
@@ -389,11 +392,14 @@ class SelectSmartGroup(bpy.types.Operator):
     bl_label = "Select Smart Group"
     bl_options = {"REGISTER", "UNDO"}
 
+    @classmethod
+    def poll(cls, context):
+        return IfcStore.get_file() and context.visible_objects and context.scene.BIMClashProperties.active_smart_group
+
     def execute(self, context):
+        self.file = IfcStore.get_file()
         # Select smart group in view
-        selected_smart_group = context.scene.BIMClashProperties.smart_clash_groups[
-            context.scene.BIMCLashProperties.active_smart_group_index
-        ]
+        selected_smart_group = context.scene.BIMClashProperties.active_smart_group
         # print(selected_smart_group.number)
 
         for obj in context.visible_objects:

src/blenderbim/blenderbim/bim/module/debug/operator.py
@@ -165,7 +165,7 @@ class SelectHighPolygonMeshes(bpy.types.Operator):
 
     def execute(self, context):
         [o.select_set(True) for o in context.view_layer.objects
-            if o.type == 'MESH'
+            if o.type == "MESH"
             and len(o.data.polygons) > context.scene.BIMDebugProperties.number_of_polygons]
         return {"FINISHED"}
 

src/blenderbim/blenderbim/bim/module/georeference/operator.py
@@ -237,6 +237,12 @@ class ConvertLocalToGlobal(bpy.types.Operator):
     bl_label = "Convert Local To Global"
     bl_options = {"REGISTER", "UNDO"}
 
+    @classmethod
+    def poll(cls, context):
+        file = IfcStore.get_file()
+        props = context.scene.BIMGeoreferenceProperties
+        return file and props.coordinate_input.count(",") == 2
+
     def execute(self, context):
         if not Data.is_loaded:
             Data.load(IfcStore.get_file())
@@ -284,6 +290,13 @@ class ConvertGlobalToLocal(bpy.types.Operator):
     bl_label = "Convert Global To Local"
     bl_options = {"REGISTER", "UNDO"}
 
+    @classmethod
+    def poll(cls, context):
+        file = IfcStore.get_file()
+        props = context.scene.BIMGeoreferenceProperties
+        return file and file.by_type("IfcUnitAssignment") \
+            and props.coordinate_input.count(",") == 2
+
     def execute(self, context):
         if not Data.is_loaded:
             Data.load(IfcStore.get_file())
@@ -330,6 +343,11 @@ class GetCursorLocation(bpy.types.Operator):
     bl_label = "Get Cursor Location"
     bl_options = {"REGISTER", "UNDO"}
 
+    @classmethod
+    def poll(cls, context):
+        file = IfcStore.get_file()
+        return file and file.by_type("IfcUnitAssignment")
+
     def execute(self, context):
         props = context.scene.BIMGeoreferenceProperties
         scale = ifcopenshell.util.unit.calculate_unit_scale(IfcStore.get_file())
@@ -342,6 +360,12 @@ class SetCursorLocation(bpy.types.Operator):
     bl_idname = "bim.set_cursor_location"
     bl_label = "Set Cursor Location"
     bl_options = {"REGISTER", "UNDO"}
+
+    @classmethod
+    def poll(cls, context):
+        file = IfcStore.get_file()
+        props = context.scene.BIMGeoreferenceProperties
+        return file and file.by_type("IfcUnitAssignment") and props.coordinate_output.count(",") == 2
 
     def execute(self, context):
         props = context.scene.BIMGeoreferenceProperties

src/blenderbim/blenderbim/bim/module/qto/operator.py
@@ -18,7 +18,6 @@
 # along with BlenderBIM Add-on.  If not, see <http://www.gnu.org/licenses/>.
 
 import bpy
-import bmesh
 import ifcopenshell
 import ifcopenshell.api
 from blenderbim.bim.ifc import IfcStore
@@ -31,14 +30,12 @@ class CalculateEdgeLengths(bpy.types.Operator):
     bl_label = "Calculate Edge Lengths"
     bl_options = {"REGISTER", "UNDO"}
 
+    @classmethod
+    def poll(cls, context):
+        return context.selected_objects and context.active_object
+
     def execute(self, context):
-        result = 0
-        for obj in context.selected_objects:
-            if not obj.data or not obj.data.edges:
-                continue
-            for edge in obj.data.edges:
-                if edge.select:
-                    result += (obj.data.vertices[edge.vertices[1]].co - obj.data.vertices[edge.vertices[0]].co).length
+        result = helper.calculate_edges_lengths([o for o in context.selected_objects if o.type == "MESH"], context)
         context.scene.BIMQtoProperties.qto_result = str(round(result, 3))
         return {"FINISHED"}
 
@@ -48,14 +45,12 @@ class CalculateFaceAreas(bpy.types.Operator):
     bl_label = "Calculate Face Areas"
     bl_options = {"REGISTER", "UNDO"}
 
+    @classmethod
+    def poll(cls, context):
+        return context.selected_objects and context.active_object
+
     def execute(self, context):
-        result = 0
-        for obj in context.selected_objects:
-            if not obj.data or not obj.data.polygons:
-                continue
-            for polygon in obj.data.polygons:
-                if polygon.select:
-                    result += polygon.area
+        result = helper.calculate_faces_areas([o for o in context.selected_objects if o.type == "MESH"], context)
         context.scene.BIMQtoProperties.qto_result = str(round(result, 3))
         return {"FINISHED"}
 
@@ -65,15 +60,12 @@ class CalculateObjectVolumes(bpy.types.Operator):
     bl_label = "Calculate Object Volumes"
     bl_options = {"REGISTER", "UNDO"}
 
+    @classmethod
+    def poll(cls, context):
+        return context.selected_objects and context.active_object
+
     def execute(self, context):
-        result = 0
-        for obj in context.selected_objects:
-            if not obj.data or not isinstance(obj.data, bpy.types.Mesh):
-                continue
-            bm = bmesh.new()
-            bm.from_mesh(obj.data)
-            result += bm.calc_volume()
-            bm.free()
+        result = helper.calculate_volumes([o for o in context.selected_objects if o.type == "MESH"], context)
         context.scene.BIMQtoProperties.qto_result = str(round(result, 3))
         return {"FINISHED"}
 
@@ -83,17 +75,21 @@ class ExecuteQtoMethod(bpy.types.Operator):
     bl_label = "Execute Qto Method"
     bl_options = {"REGISTER", "UNDO"}
 
+    @classmethod
+    def poll(cls, context):
+        return context.selected_objects
+
     def execute(self, context):
+        selected_mesh_objects = [o for o in context.selected_objects if o.type == "MESH"]
         props = context.scene.BIMQtoProperties
         result = 0
         if props.qto_methods == "HEIGHT":
-            for obj in context.selected_objects:
+            for obj in selected_mesh_objects:
                 result += helper.calculate_height(obj)
         elif props.qto_methods == "VOLUME":
-            for obj in context.selected_objects:
-                result += helper.calculate_volume(obj)
+            result = helper.calculate_volumes(selected_mesh_objects, context)
         elif props.qto_methods == "FORMWORK":
-            result = helper.calculate_formwork_area(context.selected_objects, context)
+            result = helper.calculate_formwork_area(selected_mesh_objects, context)
         props.qto_result = str(round(result, 3))
         return {"FINISHED"}
 
@@ -103,20 +99,24 @@ class QuantifyObjects(bpy.types.Operator):
     bl_label = "Quantify Objects"
     bl_options = {"REGISTER", "UNDO"}
 
+    @classmethod
+    def poll(cls, context):
+        return IfcStore.get_file() and context.selected_objects
+
     def execute(self, context):
         return IfcStore.execute_ifc_operator(self, context)
 
     def _execute(self, context):
         props = context.scene.BIMQtoProperties
         self.file = IfcStore.get_file()
-        for obj in context.selected_objects:
+        for obj in (o for o in context.selected_objects if o.type == "MESH"):
             if not obj.BIMObjectProperties.ifc_definition_id:
                 continue
             result = 0
             if props.qto_methods == "HEIGHT":
                 result = helper.calculate_height(obj)
             elif props.qto_methods == "VOLUME":
-                result = helper.calculate_volume(obj)
+                result = helper.calculate_volumes([obj], context)
             elif props.qto_methods == "FORMWORK":
                 result = helper.calculate_formwork_area([obj], context)
             if not result:

src/blenderbim/blenderbim/bim/operator.py
@@ -411,11 +411,13 @@ class RemoveSectionPlane(bpy.types.Operator):
     bl_label = "Remove Temporary Section Cutaway"
     bl_options = {"REGISTER", "UNDO"}
 
+    @classmethod
+    def poll(cls, context):
+        return context.active_object and bpy.data.node_groups.get("Section Override")
+
     def execute(self, context):
         name = context.active_object.name
         section_override = bpy.data.node_groups.get("Section Override")
-        if not section_override:
-            return {"FINISHED"}
         for node in section_override.nodes:
             if node.type != "TEX_COORD" or node.object.name != name:
                 continue
@@ -497,8 +499,11 @@ class SetOverrideColour(bpy.types.Operator):
     bl_label = "Set Override Colour"
     bl_options = {"REGISTER", "UNDO"}
 
+    @classmethod
+    def poll(cls, context):
+        return context.selected_objects
+
     def execute(self, context):
-        result = 0
         for obj in context.selected_objects:
             obj.color = context.scene.BIMProperties.override_colour
         area = next(area for area in context.screen.areas if area.type == "VIEW_3D")
@@ -510,6 +515,10 @@ class SetViewportShadowFromSun(bpy.types.Operator):
     bl_idname = "bim.set_viewport_shadow_from_sun"
     bl_label = "Set Viewport Shadow from Sun"
     bl_options = {"REGISTER", "UNDO"}
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object
 
     def execute(self, context):
         # The vector used for the light direction is a bit funny
@@ -553,17 +562,20 @@ class SnapSpacesTogether(bpy.types.Operator):
     bl_label = "Snap Spaces Together"
     bl_options = {"REGISTER", "UNDO"}
 
+    @classmethod
+    def poll(cls, context):
+        return context.selected_objects
+
     def execute(self, context):
         threshold = 0.5
         processed_polygons = set()
-        for obj in context.selected_objects:
-            if obj.type != "MESH":
-                continue
+        selected_mesh_objects = [o for o in context.selected_objects if o.type == "MESH"]
+        for obj in selected_mesh_objects:
             for polygon in obj.data.polygons:
                 center = obj.matrix_world @ polygon.center
                 distance = None
-                for obj2 in context.selected_objects:
-                    if obj2 == obj or obj.type != "MESH":
+                for obj2 in selected_mesh_objects:
+                    if obj2 == obj:
                         continue
                     result = obj2.ray_cast(obj2.matrix_world.inverted() @ center, polygon.normal, distance=threshold)
                     if not result[0]:

src/blenderbim/blenderbim/bim/module/clash/prop.py
@@ -69,3 +69,15 @@ class BIMClashProperties(PropertyGroup):
     smart_clash_grouping_max_distance: IntProperty(
         name="Smart Clash Grouping Max Distance", default=3, soft_min=1, soft_max=10
     )
+
+    @property
+    def active_clash_set(self):
+        if not self.clash_sets:
+            return None
+        return self.clash_sets[self.active_clash_set_index]
+
+    @property
+    def active_smart_group(self):
+        if not self.smart_clash_groups:
+            return None
+        return self.smart_clash_groups[self.active_smart_group_index]

commit 48d0b456b1d8a71ea922906a00693a69c27de25e

Fix errors introduced in #1683

src/blenderbim/blenderbim/bim/module/drawing/operator.py
@@ -581,16 +581,16 @@ class AddDrawingToSheet(bpy.types.Operator):
     bl_options = {"REGISTER", "UNDO"}
     # TODO: check undo redo
 
+    @classmethod
+    def poll(cls, context):
+        props = context.scene.DocProperties
+        return props.drawings and props.sheets and context.scene.BIMProperties.data_dir
+
     def execute(self, context):
         props = context.scene.DocProperties
         sheet_builder = sheeter.SheetBuilder()
         sheet_builder.data_dir = context.scene.BIMProperties.data_dir
-        try:
-            sheet_builder.add_drawing(
-                props.drawings.active_drawing.name, props.active_sheet.name
-            )
-        except FileNotFoundError:
-            self.report({"ERROR"}, "Drawings need to be created before being added to a sheet")
+        sheet_builder.add_drawing(props.active_drawing.name, props.active_sheet.name)
         return {"FINISHED"}
 
 
@@ -598,6 +598,10 @@ class CreateSheets(bpy.types.Operator):
     bl_idname = "bim.create_sheets"
     bl_label = "Create Sheets"
     # TODO: check undo redo
+
+    @classmethod
+    def poll(cls, context):
+        return context.scene.DocProperties.sheets and context.scene.BIMProperties.data_dir
 
     def execute(self, context):
         scene = context.scene
@@ -664,7 +668,7 @@ class OpenViewCamera(bpy.types.Operator):
 
     @classmethod
     def poll(cls, context):
-        return bpy.context.object.mode == "OBJECT"
+        return context.mode == "OBJECT"
 
     def execute(self, context):
         doc_props = context.scene.DocProperties
@@ -1103,13 +1107,16 @@ class AddScheduleToSheet(bpy.types.Operator):
     bl_options = {"REGISTER", "UNDO"}
     # TODO: check undo redo
 
+    @classmethod
+    def poll(cls, context):
+        props = context.scene.DocProperties
+        return props.schedules and props.sheets and context.scene.BIMProperties.data_dir
+
     def execute(self, context):
         props = context.scene.DocProperties
         sheet_builder = sheeter.SheetBuilder()
         sheet_builder.data_dir = context.scene.BIMProperties.data_dir
-        sheet_builder.add_schedule(
-            props.active_schedule.name, props.active_sheet.name
-        )
+        sheet_builder.add_schedule(props.active_schedule.name, props.active_sheet.name)
         return {"FINISHED"}
 
 