ce92aa6e00c7f8fced49394c4122a115101823f9
Dion Moult <dion@thinkmoult.com>
2021-09-09 13:45:49 +1000

    Fix bug where openings were not unlinked or renamed if they were removed from a non-dynamic object

--- src/blenderbim/blenderbim/bim/module/void/operator.py
+++ src/blenderbim/blenderbim/bim/module/void/operator.py
@@ -91,12 +91,13 @@ class RemoveOpening(bpy.types.Operator):
             if modifier.type != "BOOLEAN":
                 continue
             if modifier.object and modifier.object.BIMObjectProperties.ifc_definition_id == self.opening_id:
-                IfcStore.unlink_element(obj=modifier.object)
-                if "/" in modifier.object.name and modifier.object.name[0:3] == "Ifc":
-                    modifier.object.name = "/".join(modifier.object.name.split("/")[1:])
                 is_modifier_removed = True
                 obj.modifiers.remove(modifier)
                 break
+
+        opening = IfcStore.get_element(self.opening_id)
+        opening.name = "/".join(opening.name.split("/")[1:])
+        IfcStore.unlink_element(obj=opening)
 
         ifcopenshell.api.run("void.remove_opening", self.file, **{"opening": self.file.by_id(self.opening_id)})
 
