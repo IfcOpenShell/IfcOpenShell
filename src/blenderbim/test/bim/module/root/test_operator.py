a29d691944a9cd29fb1a1f43630d8114c849d631
Dion Moult <dion@thinkmoult.com>
2021-09-21 15:19:18 +1000

    Fix #1737. Bug where copying an opening didn't copy relevant boolean modifiers too.

--- src/blenderbim/test/bim/module/root/test_operator.py
+++ src/blenderbim/test/bim/module/root/test_operator.py
@@ -107,3 +107,21 @@ class TestCopyClass(test.bim.bootstrap.NewFile):
         And the object "IfcBuildingStorey/My Storey.001" is in the collection "IfcBuildingStorey/My Storey.001"
         And the collection "IfcBuildingStorey/My Storey.001" is in the collection "IfcBuilding/My Building"
         """
+
+    @test.bim.bootstrap.scenario
+    def test_copying_an_opening(self):
+        return """
+        Given an empty IFC project
+        When the object "Cube" is selected
+        And I set "scene.BIMRootProperties.ifc_class" to "IfcWall"
+        And I press "bim.assign_class"
+        And I add a cube
+        And the object "Cube" is selected
+        And additionally the object "IfcWall/Cube" is selected
+        And I press "bim.add_opening(opening='Cube', obj='IfcWall/Cube')"
+        And the object "IfcOpeningElement/Cube" is selected
+        And I duplicate the selected objects
+        Then the object "IfcOpeningElement/Cube" and "IfcOpeningElement/Cube.001" are different elements
+        And the object "IfcWall/Cube" has a boolean difference by "IfcOpeningElement/Cube"
+        And the object "IfcWall/Cube" has a boolean difference by "IfcOpeningElement/Cube.001"
+        """
