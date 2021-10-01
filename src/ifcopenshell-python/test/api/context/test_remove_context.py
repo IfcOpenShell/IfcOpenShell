8bcec29429842f5b0b6f0c4cc3df60c8d5e0ccd0
Dion Moult <dion@thinkmoult.com>
2021-10-01 10:10:45 +1000

    Application data in IFC now reflects the latest IFC version tag

--- src/ifcopenshell-python/test/api/context/test_remove_context.py
+++ src/ifcopenshell-python/test/api/context/test_remove_context.py
@@ -20,13 +20,16 @@ class TestRemoveContext(test.bootstrap.IFC4):
         subcontext = self.file.createIfcGeometricRepresentationSubcontext()
         subcontext.ParentContext = context
         representation = self.file.createIfcRepresentation(ContextOfItems=subcontext)
+        projected_crs = self.file.createIfcProjectedCRS()
+        map_conversion = self.file.createIfcMapConversion(SourceCRS=subcontext, TargetCRS=projected_crs)
         ifcopenshell.api.run("context.remove_context", self.file, context=subcontext)
         assert len(self.file.by_type("IfcGeometricRepresentationSubcontext")) == 0
         assert representation in self.file.get_inverse(context)
+        assert len(self.file.by_type("IfcMapConversion")) == 0
+        assert len(self.file.by_type("IfcProjectedCRS")) == 0
 
     def test_removing_a_context_with_references(self):
-        subcontext = self.file.createIfcGeometricRepresentationSubContext()
-        representation = self.file.createIfcRepresentation(ContextOfItems=subcontext)
-        map_conversion = self.file.createIfcMapConversion(SourceCRS=subcontext)
-        ifcopenshell.api.run("context.remove_context", self.file, context=subcontext)
+        context = self.file.createIfcGeometricRepresentationContext()
+        representation = self.file.createIfcRepresentation(ContextOfItems=context)
+        ifcopenshell.api.run("context.remove_context", self.file, context=context)
         assert len([e for e in self.file]) == 0
