fe1e30702a02119d17e2b99c6c024fda87cd889c
Dion Moult <dion@thinkmoult.com>
2021-10-01 14:48:44 +1000

    Fix bug where removing addresses and roles could leave other elements in invalid states, "IfcPersonAndOrganization"

--- src/ifcopenshell-python/ifcopenshell/api/owner/remove_role.py
+++ src/ifcopenshell-python/ifcopenshell/api/owner/remove_role.py
@@ -6,4 +6,11 @@ class Usecase:
             self.settings[key] = value
 
     def execute(self):
+        for inverse in self.file.get_inverse(self.settings["role"]):
+            if inverse.is_a() in ("IfcOrganization", "IfcPerson", "IfcPersonAndOrganization"):
+                if inverse.Roles == (self.settings["role"],):
+                    inverse.Roles = None
+            elif inverse.is_a("IfcResourceLevelRelationship") and not inverse.is_a("IfcOrganizationRelationship"):
+                if inverse.RelatedResourceObjects == (self.settings["organisation"],):
+                    self.file.remove(inverse)
         self.file.remove(self.settings["role"])
--- src/ifcopenshell-python/test/api/owner/test_remove_address.py
+++ src/ifcopenshell-python/test/api/owner/test_remove_address.py
@@ -9,3 +9,17 @@ class TestRemoveAddress(test.bootstrap.IFC4):
         ifcopenshell.api.run("owner.remove_address", self.file, address=postal)
         ifcopenshell.api.run("owner.remove_address", self.file, address=telecom)
         assert len(self.file.by_type("IfcAddress")) == 0
+
+    def test_ensuring_organisation_cardinality_is_valid(self):
+        address = self.file.createIfcPostalAddress()
+        organisation = self.file.createIfcOrganization()
+        organisation.Addresses = [address]
+        ifcopenshell.api.run("owner.remove_address", self.file, address=address)
+        assert organisation.Addresses is None
+
+    def test_ensuring_person_cardinality_is_valid(self):
+        address = self.file.createIfcPostalAddress()
+        person = self.file.createIfcPerson()
+        person.Addresses = [address]
+        ifcopenshell.api.run("owner.remove_address", self.file, address=address)
+        assert person.Addresses is None
