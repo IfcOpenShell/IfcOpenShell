92564b10f42077b1e5ce6b98092291dca50638c5
Dion Moult <dion@thinkmoult.com>
2021-10-01 14:48:44 +1000

    Fix bug where removing addresses and roles could leave other elements in invalid states, "IfcPersonAndOrganization"

--- src/ifcopenshell-python/ifcopenshell/api/owner/remove_address.py
+++ src/ifcopenshell-python/ifcopenshell/api/owner/remove_address.py
@@ -6,4 +6,8 @@ class Usecase:
             self.settings[key] = value
 
     def execute(self):
+        for inverse in self.file.get_inverse(self.settings["address"]):
+            if inverse.is_a() in ("IfcOrganization", "IfcPerson"):
+                if inverse.Addresses == (self.settings["address"],):
+                    inverse.Addresses = None
         self.file.remove(self.settings["address"])
