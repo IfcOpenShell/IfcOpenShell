d2a8355da8fd1f462629efece4d8af8a9b8d250d
Dion Moult <dion@thinkmoult.com>
2021-10-01 12:35:46 +1000

    Removing a person no longer has the potential to leave files in an invalid state

---
+++ src/ifcopenshell-python/test/api/owner/test_edit_address.py
@@ -0,0 +1,52 @@
+import test.bootstrap
+import ifcopenshell.api
+
+
+class TestEditAddress(test.bootstrap.IFC4):
+    def test_editing_a_postal_address(self):
+        address = self.file.createIfcPostalAddress()
+        ifcopenshell.api.run("owner.edit_address", self.file, address=address, attributes={
+            "Purpose": "OFFICE",
+            "Description": "Description",
+            "UserDefinedPurpose": "UserDefinedPurpose",
+            "InternalLocation": "InternalLocation",
+            "AddressLines": ["Address", "Lines"],
+            "PostalBox": "PostalBox",
+            "Town": "Town",
+            "Region": "Region",
+            "PostalCode": "PostalCode",
+            "Country": "Country",
+        })
+        assert address.Purpose == "OFFICE"
+        assert address.Description == "Description"
+        assert address.UserDefinedPurpose == "UserDefinedPurpose"
+        assert address.InternalLocation == "InternalLocation"
+        assert address.AddressLines == ("Address", "Lines")
+        assert address.PostalBox == "PostalBox"
+        assert address.Town == "Town"
+        assert address.Region == "Region"
+        assert address.PostalCode == "PostalCode"
+        assert address.Country == "Country"
+
+    def test_editing_a_telecom_address(self):
+        address = self.file.createIfcTelecomAddress()
+        ifcopenshell.api.run("owner.edit_address", self.file, address=address, attributes={
+            "Purpose": "OFFICE",
+            "Description": "Description",
+            "UserDefinedPurpose": "UserDefinedPurpose",
+            "TelephoneNumbers": ["Telephone", "Numbers"],
+            "FacsimileNumbers": ["Facsimile", "Numbers"],
+            "PagerNumber": "PagerNumber",
+            "ElectronicMailAddresses": ["Electronic", "Mail", "Addresses"],
+            "WWWHomePageURL": "WWWHomePageURL",
+            "MessagingIDs": ["Messaging", "IDs"],
+        })
+        assert address.Purpose == "OFFICE"
+        assert address.Description == "Description"
+        assert address.UserDefinedPurpose == "UserDefinedPurpose"
+        assert address.TelephoneNumbers == ("Telephone", "Numbers")
+        assert address.FacsimileNumbers == ("Facsimile", "Numbers")
+        assert address.PagerNumber == "PagerNumber"
+        assert address.ElectronicMailAddresses == ("Electronic", "Mail", "Addresses")
+        assert address.WWWHomePageURL == "WWWHomePageURL"
+        assert address.MessagingIDs == ("Messaging", "IDs")
---
+++ src/ifcopenshell-python/test/api/owner/test_edit_role.py
@@ -0,0 +1,15 @@
+import test.bootstrap
+import ifcopenshell.api
+
+
+class TestEditRole(test.bootstrap.IFC4):
+    def test_editing_a_role(self):
+        role = self.file.createIfcActorRole()
+        ifcopenshell.api.run("owner.edit_role", self.file, role=role, attributes={
+            "Role": "ARCHITECT",
+            "UserDefinedRole": "UserDefinedRole",
+            "Description": "Description"
+        })
+        assert role.Role == "ARCHITECT"
+        assert role.UserDefinedRole == "UserDefinedRole"
+        assert role.Description == "Description"
