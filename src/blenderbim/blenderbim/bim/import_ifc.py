c66471667ab657127bc5d51cfbc6f492ae03e493
Laurens Oostwegel <l.oostwegel@gmail.com>
2021-10-24 15:17:56 +0200

    Updated import_ifc.py based on review PR

--- src/blenderbim/blenderbim/bim/import_ifc.py
+++ src/blenderbim/blenderbim/bim/import_ifc.py
@@ -686,49 +686,64 @@ class IfcImporter:
             obj.matrix_world = self.apply_blender_offset_to_matrix_world(obj, placement_matrix)
             self.link_element(product, obj)
 
+    def get_pointcloud_representation(self, product):
+        if hasattr(product, 'Representation') and hasattr(product.Representation, 'Representations'):
+            representations = product.Representation.Representations
+        elif hasattr(product, "RepresentationMaps") and hasattr(product.RepresentationMaps, 'RepresentationMaps'):
+            representations = product.RepresentationMaps
+        else:
+            return None
+
+        for representation in representations:
+            if representation.RepresentationType == 'PointCloud':
+                return representation
+
+            elif self.file.schema == "IFC2X3" and representation.RepresentationType == 'GeometricSet':
+                for item in representation.Items:
+                    if not (item.is_a("IfcCartesianPointList") or item.is_a("IfcCartesianPoint")):
+                        break
+                else:
+                    return representation
+
+            elif representation.RepresentationType == 'MappedRepresentation':
+                for item in representation.Items:
+                    mapped_representation = self.get_pointcloud_representation(item)
+                    if mapped_representation is not None:
+                        return mapped_representation
+        return None
+
     def create_pointclouds(self, products):
         result = set()
         for product in products:
-            if not self.is_pointcloud(product):
-                continue
-            items = []
-            for representation in product.Representation.Representations:
-                if representation.RepresentationType == 'PointCloud':
-                    items.extend(representation.Items)
-                elif self.file.schema == "IFC2X3" and representation.RepresentationType == 'GeometricSet':
-                    items.extend(representation.Items)
-
-            placement_matrix = ifcopenshell.util.placement.get_local_placement(product.ObjectPlacement)
-            vertex_list = []
-            context = None
-            representation = None
-            for item in items:
-                if item.is_a("IfcCartesianPointList"):
-                    vertices = [mathutils.Vector(list(coordinates)) * self.unit_scale for coordinates in item.CoordList]
-                    vertex_list.extend(vertices)
-                elif item.is_a("IfcCartesianPoint"):
-                    vertex = mathutils.Vector(list(item.Coordinates)) * self.unit_scale
-                    vertex_list.append(vertex)
-
-            for subelement in self.file.traverse(product.Representation):
-                if subelement.is_a("IfcGeometricRepresentationContext"):
-                    context = subelement
-                elif subelement.is_a("IfcRepresentation"):  # not sure what this should be
-                    representation = subelement
-
-            if len(vertex_list) == 0 or not context or not representation:
-                continue
-
-            mesh_name = f"{context.id()}/{representation.id()}"
-            mesh = bpy.data.meshes.new(mesh_name)
-            mesh.from_pydata(vertex_list, [], [])
-
-            obj = bpy.data.objects.new("{}/{}".format(product.is_a(), product.Name), mesh)
-            obj.matrix_world = self.apply_blender_offset_to_matrix_world(obj, placement_matrix)
-            self.link_element(product, obj)
-            result.add(product)
+            representation = self.get_pointcloud_representation(product)
+            if representation is not None:
+                pointcloud = self.create_pointcloud(product, representation)
+                if pointcloud is not None:
+                    result.add(pointcloud)
 
         return result
+
+    def create_pointcloud(self, product, representation):
+        placement_matrix = ifcopenshell.util.placement.get_local_placement(product.ObjectPlacement)
+        vertex_list = []
+        for item in representation.Items:
+            if item.is_a("IfcCartesianPointList"):
+                vertex_list.extend(mathutils.Vector(list(coordinates)) * self.unit_scale
+                                   for coordinates in item.CoordList)
+            elif item.is_a("IfcCartesianPoint"):
+                vertex_list.append(mathutils.Vector(list(item.Coordinates)) * self.unit_scale)
+
+        if len(vertex_list) == 0:
+            return None
+
+        mesh_name = f"{representation.ContextOfItems.id()}/{representation.id()}"
+        mesh = bpy.data.meshes.new(mesh_name)
+        mesh.from_pydata(vertex_list, [], [])
+
+        obj = bpy.data.objects.new("{}/{}".format(product.is_a(), product.Name), mesh)
+        obj.matrix_world = self.apply_blender_offset_to_matrix_world(obj, placement_matrix)
+        self.link_element(product, obj)
+        return product
 
     def is_pointcloud(self, product):
         if not hasattr(product, 'Representation') or not hasattr(product.Representation, 'Representations'):
