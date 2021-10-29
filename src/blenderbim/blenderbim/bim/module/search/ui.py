0af1519a765012ea21208ace0443771350edd65c
chunchk <chun@forecrux.com>
2021-10-29 10:53:03 +0800

    1.Added Selecting/Deselecting All function 2.Suggested an idea for UI improvment 3.Fixed an UX bug

--- src/blenderbim/blenderbim/bim/module/search/ui.py
+++ src/blenderbim/blenderbim/bim/module/search/ui.py
@@ -78,7 +78,8 @@ class BIM_UL_ifctype_filter(bpy.types.UIList):
         split = layout
         split.use_property_split = True
         split.use_property_decorate = False
-        split.prop(item, "is_selected", text="", icon="ADD" if item.is_selected else "REMOVE")
+        #After some tests, using icon ADD & REMOVE may not clearly deliver the message of "Including". TICK & BLANK1 seems to be a better combination but there is no TICK icon so I used CHECKBOX
+        split.prop(item, "is_selected", text="", icon="CHECKBOX_HLT" if item.is_selected else "CHECKBOX_DEHLT")
         split.prop(item, "name", text="", emboss=False, slider=True)
         split = split.column()
         split.scale_x = 0.5
