c61fb54df10b7cd5cbce917c8bf9d4997395f63a
HassanEmam <hassan.emam@hotmail.com>
2021-10-29 11:16:47 +0100

    BlenderBIM: Import Resources for XER files (#1841)
    * P6XML Parser failing on calendar type
    
    * add work plan references a get_person which is not an attribute. modified to get_user to work
    
    * Initial support for Primavera XER files
    
    * IFC4D impl XER calendars exceptions and work time
    
    * merged updats
    
    * refactoring code for the 4D model to avoid duplicate code
    
    * Assign process in the api module was missing an atrribute QuantityIn Process
    
    * Merged changes from master
    
    * refactoring
    
    * resolved conflicts
    
    * refactoring code for IFC4D
    
    * refactoring code for IFC4D
    
    * refactoring code for IFC4D
    
    * Reverted the changes to IfcRelAssignsToProcess usecase
    
    * Changed the class to be more discriptive from Utils to ScheduleIfcGenerator
    
    * reverted changes to add_work_plan and Makefile
    
    * Added resource parsing support for Primavera XER
    
    * added relative path for importing common as it was failing in BlenderBIM
    
    * Initial support for Primavera XER files
    
    * merged updats
    
    * Assign process in the api module was missing an atrribute QuantityIn Process
    
    * Merged changes from master
    
    * refactoring
    
    * resolved conflicts
    
    * refactoring code for IFC4D
    
    * refactoring code for IFC4D
    
    * Reverted the changes to IfcRelAssignsToProcess usecase
    
    * Changed the class to be more discriptive from Utils to ScheduleIfcGenerator
    
    * reverted changes to add_work_plan and Makefile
    
    * Added resource parsing support for Primavera XER
    
    * added relative path for importing common as it was failing in BlenderBIM
    
    * deleted repetitive classes for P6XER import
    
    Co-authored-by: Dion Moult <dion@thinkmoult.com>

--- src/ifc4d/ifc4d/p62ifc.py
+++ src/ifc4d/ifc4d/p62ifc.py
@@ -23,7 +23,7 @@ import ifcopenshell
 import ifcopenshell.api
 import ifcopenshell.util.date
 import xml.etree.ElementTree as ET
-from common import ScheduleIfcGenerator
+from .common import ScheduleIfcGenerator
 
 class P62Ifc:
     def __init__(self):
@@ -49,7 +49,7 @@ class P62Ifc:
     def execute(self):
         self.parse_xml()
         ifcCreator = ScheduleIfcGenerator(self.file, self.work_plan, self.project, self.calendars,
-                           self.wbs, self.root_activites, self.activities, self.relationships)
+                           self.wbs, self.root_activites, self.activities, self.relationships, None)
         ifcCreator.create_ifc()
 
     def parse_xml(self):
