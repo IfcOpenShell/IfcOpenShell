2d62d8181af837f712892262ae3f30a0cdd6098b
Bernd Hahnebach <bernd@bimstatik.org>
2021-11-17 16:08:42 +0100

    bimtester: improve French translation for the provided example

--- src/ifcbimtester/bimtester/features/steps/project_setup/fr.py
+++ src/ifcbimtester/bimtester/features/steps/project_setup/fr.py
@@ -4,3 +4,8 @@ from behave import step
 @step('Les données IFC doivent utiliser le schéma "{schema}"')
 def step_impl(context, schema):
     context.execute_steps(f'* IFC data must use the "{schema}" schema')
+
+
+@step('Nom du projet, son code ou un identifiant sommaire du type "{value}"')
+def step_impl(context, value):
+    context.execute_steps(f'* The project name, code, or short identifier must be "{value}"')
