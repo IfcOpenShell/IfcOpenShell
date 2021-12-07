85d76c084b5b73914bf387fd7ed185a559cfaa24
Bernd Hahnebach <bernd@bimstatik.org>
2021-12-07 12:59:50 +0100

    bimstester: features, move element class attributes tests from examples to implemented steps

--- src/ifcbimtester/examples/steps/attributes_eleclasses.py
+++ src/ifcbimtester/examples/steps/attributes_eleclasses.py
@@ -1,52 +1,6 @@
 from behave import step
 
-import attributes_eleclasses_methods as aem
-from utils import assert_elements
 from utils import IfcFile
-
-
-@step("There are no {ifc_class} elements")
-def step_impl(context, ifc_class):
-    aem.no_eleclass(context, ifc_class)
-
-
-@step("There are no {ifc_class} elements because {reason}")
-def step_impl(context, ifc_class, reason):
-    aem.no_eleclass(context, ifc_class)
-
-
-@step("All {ifc_class} elements class attributes have a value")
-def step_impl(context, ifc_class):
-    aem.eleclass_have_class_attributes_with_a_value(context, ifc_class)
-
-
-@step("All {ifc_class} elements have a name given")
-def step_impl(context, ifc_class):
-    aem.eleclass_has_name_with_a_value(context, ifc_class)
-
-
-@step("All {ifc_class} elements have a description given")
-def step_impl(context, ifc_class):
-    aem.eleclass_has_description_with_a_value(context, ifc_class)
-
-
-@step('all {ifc_class} elements have a name matching the pattern "{pattern}"')
-def step_impl(context, ifc_class, pattern):
-    import re
-
-    elements = IfcFile.get().by_type(ifc_class)
-    for element in elements:
-        if not re.search(pattern, element.Name):
-            assert False
-
-
-@step('there is an {ifc_class} element with a {attribute_name} attribute with a value of "{attribute_value}"')
-def step_impl(context, ifc_class, attribute_name, attribute_value):
-    elements = IfcFile.get().by_type(ifc_class)
-    for element in elements:
-        if hasattr(element, attribute_name) and getattr(element, attribute_name) == attribute_value:
-            return
-    assert False
 
 
 use_step_matcher("re")
