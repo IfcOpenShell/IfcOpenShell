6e7ac53cdbe072fbb91ef616655f539046db0f8b
Bernd Hahnebach <bernd@bimstatik.org>
2021-12-07 15:51:31 +0100

    bimtester: example steps, delete more steps which are implemented already

--- src/ifcbimtester/examples/steps/attributes_eleclasses.py
+++ src/ifcbimtester/examples/steps/attributes_eleclasses.py
@@ -6,25 +6,6 @@ from utils import IfcFile
 use_step_matcher("re")
 
 
-@step("all (?P<ifc_class>.*) elements have an? (?P<attribute>.*) attribute")
-def step_impl(context, ifc_class, attribute):
-    elements = IfcFile.get().by_type(ifc_class)
-    for element in elements:
-        if not getattr(element, attribute):
-            assert False
-
-
-@step('all (?P<ifc_class>.*) elements have an? (?P<attribute>.*) matching the pattern "(?P<pattern>.*)"')
-def step_impl(context, ifc_class, attribute, pattern):
-    import re
-
-    elements = IfcFile.get().by_type(ifc_class)
-    for element in elements:
-        value = getattr(element, attribute)
-        print(f'Checking value "{value}" for {element}')
-        assert re.search(pattern, value)
-
-
 @step('all (?P<ifc_class>.*) elements have an? (?P<attributes>.*) taken from the list in "(?P<list_file>.*)"')
 def step_impl(context, ifc_class, attributes, list_file):
     import csv
