146dd1e74dd07fa3695c854cc43fe85801ac48d4
Bernd Hahnebach <bernd@bimstatik.org>
2021-12-08 08:23:00 +0100

    bimtester: German geometrirc detail step, fix quotation marks

--- src/ifcbimtester/bimtester/features/steps/geometric_detail/de.py
+++ src/ifcbimtester/bimtester/features/steps/geometric_detail/de.py
@@ -1,6 +1,6 @@
 from behave import step
 
 
-@step("Alle {ifc_class} Bauteile müssen eine geometrische Repräsentation der Klasse {representation_class} verwenden")
+@step('Alle "{ifc_class}" Bauteile müssen eine geometrische Repräsentation der Klasse "{representation_class}" verwenden')
 def step_impl(context, ifc_class, representation_class):
     context.execute_steps(f'* All {ifc_class} elements have an "{representation_class}" representation')
