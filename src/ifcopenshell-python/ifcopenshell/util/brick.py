dd77a92e3b3ac6a2f9ac306540ee48d11006000c
Dion Moult <dion@thinkmoult.com>
2022-01-10 17:31:42 +1100

    New utility to get IFC all elements that are relevant in Brickschema

--- src/ifcopenshell-python/ifcopenshell/util/brick.py
+++ src/ifcopenshell-python/ifcopenshell/util/brick.py
@@ -17,4 +17,16 @@ def get_brick_type(element):
         result = ifc4_to_brick_map.get(f"{element.is_a()}.{predefined_type}", None)
     if not result:
         result = ifc4_to_brick_map.get(element.is_a(), None)
-    return result
+    if result:
+        return f"https://brickschema.org/schema/Brick#{result}"
+
+
+def get_brick_elements(ifc_file):
+    classes = {c.split(".")[0] for c in ifc4_to_brick_map.keys()}
+    elements = []
+    for ifc_class in classes:
+        try:
+            elements += ifc_file.by_type(ifc_class)
+        except:
+            pass
+    return elements
--- src/ifcopenshell-python/test/util/test_brick.py
+++ src/ifcopenshell-python/test/util/test_brick.py
@@ -6,6 +6,13 @@ import ifcopenshell.util.brick as subject
 class TestGetBrickTypeIFC4(test.bootstrap.IFC4):
     def test_run(self):
         element = self.file.createIfcAirTerminalBox()
-        assert subject.get_brick_type(element) == "TerminalUnit"
+        assert subject.get_brick_type(element) == "https://brickschema.org/schema/Brick#TerminalUnit"
         element.PredefinedType = "CONSTANTFLOW"
-        assert subject.get_brick_type(element) == "CAV"
+        assert subject.get_brick_type(element) == "https://brickschema.org/schema/Brick#CAV"
+
+
+class TestGetBrickElementsIFC4(test.bootstrap.IFC4):
+    def test_run(self):
+        element = self.file.createIfcAirTerminalBox()
+        self.file.createIfcWall()
+        assert subject.get_brick_elements(self.file) == [element]
