e66ba1660c75501bf4532879d013f045082f313d
krande <kristoffer_andersen@outlook.com>
2022-04-11 17:18:50 +0200

    make tests pass

--- conda/run_test.bat
+++ conda/run_test.bat
@@ -1,2 +1,4 @@
+rem Hack while we do not have bcf-client on our conda ifcopenshell channel yet
+pip install bcf-client
 cd test && python tests.py
 cd ../src/ifcopenshell-python && mv ifcopenshell ifcopenshell-local && make test
