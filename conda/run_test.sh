1d086f260fedd5909a73907123fe45699bfb12db
krande <kristoffer_andersen@outlook.com>
2022-04-14 09:43:36 +0200

    try to skip ids tests for common icopenshell builds (caused failure on windows tests probably due to special character in filepath)

--- conda/run_test.sh
+++ conda/run_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Hack while we do not have bcf-client on our conda ifcopenshell channel yet
-pip install bcf-client
+# pip install bcf-client
 cd test && python tests.py
 cd ../src/ifcopenshell-python && mv ifcopenshell ifcopenshell-local && make test
