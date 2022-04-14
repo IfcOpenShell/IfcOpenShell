06288bb18145fedf32b23305df8dc18baa60ef1c
krande <kristoffer_andersen@outlook.com>
2022-04-14 16:49:30 +0200

    fix errors in tests

--- conda/run_test.bat
+++ conda/run_test.bat
@@ -1,4 +1,4 @@
 rem Hack while we do not have bcf-client on our conda ifcopenshell channel yet
 rem pip install bcf-client
 cd test && python tests.py
-cd ../src/ifcopenshell-python && mv ifcopenshell ifcopenshell-local && make test
+cd ../src/ifcopenshell-python/test && pytest -p no:pytest-blender
--- conda/run_test.sh
+++ conda/run_test.sh
@@ -3,4 +3,4 @@
 # Hack while we do not have bcf-client on our conda ifcopenshell channel yet
 # pip install bcf-client
 cd test && python tests.py
-cd ../src/ifcopenshell-python && mv ifcopenshell ifcopenshell-local && make test
+cd ../src/ifcopenshell-python/test && pytest -p no:pytest-blender
