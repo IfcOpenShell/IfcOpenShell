1f94e657e71e1f5bb4d8a8ee47d7626ff244d6f2
krande <kristoffer_andersen@outlook.com>
2022-04-16 17:01:43 +0200

    remove test files and skip tests

--- conda/run_test.bat
+++
@@ -1,4 +0,0 @@
-rem Hack while we do not have bcf-client on our conda ifcopenshell channel yet
-rem pip install bcf-client
-cd test && python tests.py
-cd ../src/ifcopenshell-python/test && pytest -p no:pytest-blender
--- conda/run_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Hack while we do not have bcf-client on our conda ifcopenshell channel yet
-# pip install bcf-client
-cd test && python tests.py
-cd ../src/ifcopenshell-python/test && pytest -p no:pytest-blender
