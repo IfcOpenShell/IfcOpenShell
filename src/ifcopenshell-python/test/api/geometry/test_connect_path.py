e1594735722eea857386b2dcff9fb32e65d5a446
Dion Moult <dion@thinkmoult.com>
2022-09-15 10:31:44 +1000

    Add support for connecting/disconnecting paths in API

---
+++ src/ifcopenshell-python/test/api/geometry/test_connect_path.py
@@ -0,0 +1,55 @@
+# IfcOpenShell - IFC toolkit and geometry engine
+# Copyright (C) 2022 Dion Moult <dion@thinkmoult.com>
+#
+# This file is part of IfcOpenShell.
+#
+# IfcOpenShell is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# IfcOpenShell is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with IfcOpenShell.  If not, see <http://www.gnu.org/licenses/>.
+
+import test.bootstrap
+import ifcopenshell.api
+
+
+class TestConnectPath(test.bootstrap.IFC4):
+    def test_connecting_a_path(self):
+        wall1 = ifcopenshell.api.run("root.create_entity", self.file, ifc_class="IfcWall")
+        wall2 = ifcopenshell.api.run("root.create_entity", self.file, ifc_class="IfcWall")
+        rel = ifcopenshell.api.run(
+            "geometry.connect_path",
+            self.file,
+            relating_element=wall1,
+            related_element=wall2,
+            relating_connection="ATSTART",
+            related_connection="ATEND",
+        )
+        assert rel.is_a("IfcRelConnectsPathElements")
+        assert rel.RelatingElement == wall1
+        assert rel.RelatedElement == wall2
+        assert rel.RelatingConnectionType == "ATSTART"
+        assert rel.RelatedConnectionType == "ATEND"
+
+    def test_doing_nothing_if_the_element_is_already_connected(self):
+        wall1 = ifcopenshell.api.run("root.create_entity", self.file, ifc_class="IfcWall")
+        wall2 = ifcopenshell.api.run("root.create_entity", self.file, ifc_class="IfcWall")
+        ifcopenshell.api.run("geometry.connect_path", self.file, relating_element=wall1, related_element=wall2)
+        total_elements = len([e for e in self.file])
+        ifcopenshell.api.run("geometry.connect_path", self.file, relating_element=wall1, related_element=wall2)
+        assert len([e for e in self.file]) == total_elements
+
+    def test_preventing_cyclical_connections(self):
+        wall1 = ifcopenshell.api.run("root.create_entity", self.file, ifc_class="IfcWall")
+        wall2 = ifcopenshell.api.run("root.create_entity", self.file, ifc_class="IfcWall")
+        ifcopenshell.api.run("geometry.connect_path", self.file, relating_element=wall1, related_element=wall2)
+        total_elements = len([e for e in self.file])
+        ifcopenshell.api.run("geometry.connect_path", self.file, relating_element=wall2, related_element=wall1)
+        assert len([e for e in self.file]) == total_elements
