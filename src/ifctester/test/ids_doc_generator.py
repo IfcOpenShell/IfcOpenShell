82f847f5d453a48dd4fc4e34f7cbd51f87ce39fa
Dion Moult <dion@thinkmoult.com>
2022-10-07 16:00:17 +1100

    Basic adding of filled openings reimplemented with new opening system

--- src/ifctester/test/ids_doc_generator.py
+++ src/ifctester/test/ids_doc_generator.py
@@ -78,7 +78,7 @@ class FacetDocGenerator:
         # Create an IDS with the applicability selecting exactly
         # the entity type passed to us in `inst`.
         specs = ids.Ids(title=name)
-        spec = ids.Specification(name=name)
+        spec = ids.Specification(name=name, minOccurs=1)
         spec.applicability.append(ids.Entity(name=inst.is_a().upper()))
         spec.requirements.append(facet)
         specs.specifications.append(spec)
