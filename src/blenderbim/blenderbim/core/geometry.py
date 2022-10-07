6fedb1106fe8e6aa9f5c577f7aeaac6ea4484a2d
Dion Moult <dion@thinkmoult.com>
2022-10-07 20:46:54 +1100

    Begin purging old dynamic void feature

--- src/blenderbim/blenderbim/core/geometry.py
+++ src/blenderbim/blenderbim/core/geometry.py
@@ -97,7 +97,7 @@ def switch_representation(
     existing_data = geometry.get_representation_data(representation)
 
     if should_reload or not existing_data:
-        data = geometry.import_representation(obj, representation, enable_dynamic_voids=enable_dynamic_voids)
+        data = geometry.import_representation(obj, representation)
         geometry.rename_object(data, geometry.get_representation_name(representation))
         geometry.link(representation, data)
     else:
@@ -109,9 +109,6 @@ def switch_representation(
         geometry.delete_data(existing_data)
 
     geometry.clear_modifiers(obj)
-
-    if enable_dynamic_voids and geometry.is_body_representation(representation):
-        geometry.create_dynamic_voids(obj)
 
 
 def get_representation_ifc_parameters(geometry, obj=None, should_sync_changes_first=False):
