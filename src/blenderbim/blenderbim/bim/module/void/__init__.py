4110b81270cdc463a6be0c393c7ed06417a35ff3
Dion Moult <dion@thinkmoult.com>
2022-10-08 23:42:03 +1100

    Decomposition mode now selects all children (not parenting) to move a host and all children easily

--- src/blenderbim/blenderbim/bim/module/void/__init__.py
+++ src/blenderbim/blenderbim/bim/module/void/__init__.py
@@ -20,11 +20,11 @@ import bpy
 from . import ui, prop, operator
 
 classes = (
-    operator.AddOpening,
-    operator.RemoveOpening,
     operator.AddFilling,
+    operator.AddOpening,
     operator.RemoveFilling,
-    operator.ToggleDecompositionParenting,
+    operator.RemoveOpening,
+    operator.SelectDecomposition,
     prop.VoidProperties,
     ui.BIM_PT_voids,
     ui.BIM_PT_booleans,
--- src/blenderbim/blenderbim/bim/module/void/operator.py
+++ src/blenderbim/blenderbim/bim/module/void/operator.py
@@ -182,55 +182,18 @@ class RemoveFilling(bpy.types.Operator):
         return {"FINISHED"}
 
 
-class ToggleDecompositionParenting(bpy.types.Operator):
-    bl_idname = "bim.toggle_decomposition_parenting"
-    bl_label = "Toggle Decomposition Parenting"
+class SelectDecomposition(bpy.types.Operator):
+    bl_idname = "bim.select_decomposition"
+    bl_label = "Select Decomposition"
     bl_options = {"REGISTER", "UNDO"}
-    obj: bpy.props.StringProperty()
 
     def execute(self, context):
-        obj = bpy.data.objects.get(self.obj) if self.obj else context.active_object
-        self.file = IfcStore.get_file()
-        is_parenting = None
-
-        self.decompositions = {}
-        self.load_decompositions(obj)
-
-        for parent, children in self.decompositions.items():
-            bpy.ops.bim.dynamically_void_product(obj=parent.name)
-            for child in children:
-                bpy.ops.bim.dynamically_void_product(obj=child.name)
-                if is_parenting is None:
-                    is_parenting = not bool(child.parent)
-
-                if is_parenting:
-                    child.parent = parent
-                    child.matrix_parent_inverse = parent.matrix_world.inverted()
-                else:
-                    parent_matrix_world = child.matrix_world.copy()
-                    child.parent = None
-                    child.matrix_world = parent_matrix_world
-
+        for obj in context.selected_objects:
+            element = tool.Ifc.get_entity(obj)
+            if not element:
+                continue
+            for subelement in ifcopenshell.util.element.get_decomposition(element):
+                subobj = tool.Ifc.get_object(subelement)
+                if subobj:
+                    subobj.select_set(True)
         return {"FINISHED"}
-
-    def load_decompositions(self, parent_obj):
-        element = self.file.by_id(parent_obj.BIMObjectProperties.ifc_definition_id)
-        for rel in self.file.get_inverse(element):
-            if rel.is_a("IfcRelDecomposes"):
-                if rel[4] != element:
-                    continue
-                if isinstance(rel[5], tuple):
-                    for related_object in rel[5]:
-                        self.add_decomposition(parent_obj, related_object)
-                else:
-                    self.add_decomposition(parent_obj, rel[5])
-            elif rel.is_a("IfcRelFillsElement"):
-                if rel.RelatingOpeningElement != element:
-                    continue
-                self.add_decomposition(parent_obj, rel.RelatedBuildingElement)
-
-    def add_decomposition(self, parent_obj, child_element):
-        child_obj = IfcStore.get_element(child_element.id())
-        if child_obj:
-            self.decompositions.setdefault(parent_obj, []).append(child_obj)
-            self.load_decompositions(child_obj)
