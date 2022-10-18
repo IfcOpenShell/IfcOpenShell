1f33e78a2a495b558289a71e47af57d1ec474159
Gorgious56 <nathan.hild@gmail.com>
2022-10-18 23:17:31 +0200

    Dynamic Attribute Info (#1990)
    * Add info mode toggle in addon prefs
    
    * Add documentation property to Attribute
    
    * Add webbrowser open operator
    
    * Populate attribute doc on import
    
    * Add attribute doc popup operator
    
    * use path_resolve instead of eval
    
    * Rename operator
    
    * Add description field
    
    * Panel UI is managed by operator
    
    * Revert "Panel UI is managed by operator"
    
    This reverts commit a7bbd06865250fa502506a84cc486fede5ff3bed.
    
    * Revert "Revert "Panel UI is managed by operator""
    
    This reverts commit d62115bf632df8988fafaea4bc1c6af1f9169e8a.
    
    * Add info popup to ifc class panel in object properties
    
    * Implement fetching doc for non-ifc classes from jsonlike format
    
    * Draw doc from non ifc classes
    
    * Add utility function to draw info button on a given layout
    
    * Add info buttons in the create project UI
    
    * Display entity descriptions as tooltips in enum items
    
    * Move predefined types fetching to data class
    
    * Remove leftover prop code
    
    * Use tool to get schema

--- src/blenderbim/blenderbim/bim/operator.py
+++ src/blenderbim/blenderbim/bim/operator.py
@@ -429,6 +429,20 @@ class RemoveIfcFile(bpy.types.Operator):
         return {"FINISHED"}
 
 
+class BIM_OT_open_webbrowser(bpy.types.Operator):
+    bl_idname = "bim.open_webbrowser"
+    bl_description = "Open the URL in your Web Browser"
+    bl_label = "Open URL"
+
+    url: bpy.props.StringProperty()
+
+    def execute(self, context):
+        import webbrowser
+
+        webbrowser.open(self.url)
+        return {"FINISHED"}
+
+
 class SelectExternalMaterialDir(bpy.types.Operator):
     bl_idname = "bim.select_external_material_dir"
     bl_label = "Select Material File"
