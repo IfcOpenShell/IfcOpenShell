e6d7b194c9f065cc8254b51f2c2148e4a1eb0395
Thomas Krijnen <t.krijnen@gmail.com>
2022-12-25 11:19:50 +0100

    Return calculated values for redeclared derived attributes

--- src/ifcopenshell-python/ifcopenshell/entity_instance.py
+++ src/ifcopenshell-python/ifcopenshell/entity_instance.py
@@ -34,7 +34,7 @@ except ImportError as e:
     logging = type("logger", (object,), {"exception": staticmethod(lambda s: print(s))})
 
 
-def set_derived_atribute(*args):
+def set_derived_attribute(*args):
     raise TypeError("Unable to set derived attribute")
 
 
@@ -74,7 +74,7 @@ def register_schema_attributes(schema):
 
             # resolve to actual functions in wrapper
             functions = [
-                set_derived_atribute
+                set_derived_attribute
                 if mname == "setArgumentAsDerived"
                 else getattr(ifcopenshell_wrapper.entity_instance, mname)
                 for mname in fn_names
@@ -114,25 +114,30 @@ class entity_instance(object):
         INVALID, FORWARD, INVERSE = range(3)
         attr_cat = self.wrapped_data.get_attribute_category(name)
         if attr_cat == FORWARD:
-            return entity_instance.wrap_value(
-                self.wrapped_data.get_argument(self.wrapped_data.get_argument_index(name)), self.wrapped_data.file
-            )
+            idx = self.wrapped_data.get_argument_index(name)
+            if _method_dict[self.is_a(True)][idx] != set_derived_attribute:
+                # A bit ugly, but we fall through to derived attribute handling below
+                return entity_instance.wrap_value(
+                    self.wrapped_data.get_argument(idx), self.wrapped_data.file
+                )
         elif attr_cat == INVERSE:
             return entity_instance.wrap_value(self.wrapped_data.get_inverse(name), self.wrapped_data.file)
-        else:
-            schema_name = self.wrapped_data.is_a(True).split('.')[0]
-            rules = importlib.import_module(f"ifcopenshell.express.rules.{schema_name}")
-            def yield_supertypes():
-                decl = ifcopenshell_wrapper.schema_by_name(schema_name).declaration_by_name(self.is_a())
-                while decl:
-                    yield decl.name()
-                    decl = decl.supertype()
+        
+        # derived attribute perhaps?
+        schema_name = self.wrapped_data.is_a(True).split('.')[0]
+        rules = importlib.import_module(f"ifcopenshell.express.rules.{schema_name}")
+        def yield_supertypes():
+            decl = ifcopenshell_wrapper.schema_by_name(schema_name).declaration_by_name(self.is_a())
+            while decl:
+                yield decl.name()
+                decl = decl.supertype()
 
-            for sty in yield_supertypes():
-                fn = getattr(rules, f"calc_{sty}_{name}", None)
-                if fn:
-                    return fn(self)
+        for sty in yield_supertypes():
+            fn = getattr(rules, f"calc_{sty}_{name}", None)
+            if fn:
+                return fn(self)
 
+        if attr_cat != FORWARD:
             raise AttributeError(
                 "entity instance of type '%s' has no attribute '%s'" % (self.wrapped_data.is_a(True), name)
             )
@@ -204,7 +209,7 @@ class entity_instance(object):
         method = self.method_list[idx]
 
         if value is None:
-            if method is not set_derived_atribute:
+            if method is not set_derived_attribute:
                 self.wrapped_data.setArgumentAsNull(idx)
         else:
             self.method_list[idx](self.wrapped_data, idx, entity_instance.unwrap_value(value))
