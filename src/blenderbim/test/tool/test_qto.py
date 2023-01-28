58e3d705b02139ba3d90f788a6e270b629c35aaf
Massimo Fabbro <maxfb87@yahoo.it>
2023-01-28 20:45:32 +0100

    complete test_run qto tests

--- src/blenderbim/test/tool/test_qto.py
+++ src/blenderbim/test/tool/test_qto.py
@@ -22,6 +22,7 @@ import test.bim.bootstrap
 import blenderbim.core.tool
 import blenderbim.tool as tool
 from blenderbim.tool.qto import Qto as subject
+from blenderbim.bim.module.pset.qto_calculator import QtoCalculator
 
 
 class TestImplementsTool(test.bim.bootstrap.NewFile):
@@ -37,6 +38,90 @@ class TestSetQtoResult(test.bim.bootstrap.NewFile):
     def test_run(self):
         subject.set_qto_result(123.4567)
         assert bpy.context.scene.BIMQtoProperties.qto_result == "123.457"
+
+
+class TestGetApplicableQuantityNames(test.bim.bootstrap.NewFile):
+    def test_run(self):
+        ifc = ifcopenshell.file()
+        tool.Ifc.set(ifc)
+        schema = ifc.schema
+        properties_templates = ifcopenshell.util.pset.PsetQto(schema).get_by_name('Qto_WallBaseQuantities').get_info()['HasPropertyTemplates']
+        applicable_quantity_names = [a.Name for a in properties_templates]
+        assert subject.get_applicable_quantity_names('Qto_WallBaseQuantities') == applicable_quantity_names
+
+class TestGetApplicableBaseQuantityName(test.bim.bootstrap.NewFile):
+    def test_run(self):
+        ifc = ifcopenshell.file()
+        tool.Ifc.set(ifc)
+        wall = ifc.createIfcWall()
+        assert subject.get_applicable_base_quantity_name(wall) == "Qto_WallBaseQuantities"
+
+    def test_no_base_quantity(self):
+        ifc = ifcopenshell.file()
+        tool.Ifc.set(ifc)
+        ifcopenshell.api.run("root.create_entity", ifc, ifc_class = "IfcProject")
+        product = ifc.by_type('IfcProject')[0]
+        assert subject.get_applicable_base_quantity_name(product) == None
+
+class TestGetRoundedValue(test.bim.bootstrap.NewFile):
+    def test_run(self):
+        quantity = 1.2345
+        assert subject.get_rounded_value(quantity) == 1.234
+
+class TestGetCalculatedObjectQuantities(test.bim.bootstrap.NewFile):
+    def test_run(self):
+        ifc = ifcopenshell.file()
+        tool.Ifc.set(ifc)
+        project = ifcopenshell.api.run("root.create_entity", ifc, ifc_class="IfcProject", name="My Project")
+        ifcopenshell.api.run("unit.assign_unit", ifc)
+        context = ifcopenshell.api.run("context.add_context", ifc, context_type="Model")
+        bpy.ops.mesh.primitive_cube_add(location=(0.0,0.0,0.0), size = 2)
+        obj = bpy.context.active_object
+        element = blenderbim.core.root.assign_class(tool.Ifc,
+                                                    tool.Collector,
+                                                    tool.Root,
+                                                    obj=obj,
+                                                    ifc_class="IfcWall",
+                                                    predefined_type = "ELEMENTEDWALL",
+                                                    context = context)
+        calculator = QtoCalculator()
+        base_qto = ifcopenshell.api.run("pset.add_qto", ifc, product=element, name="Qto_WallBaseQuantities")
+        quantities = subject.get_calculated_object_quantities(calculator = calculator, qto_name = "Qto_WallBaseQuantities", obj = obj)
+        assert quantities["Length"] == 2
+        assert quantities["Width"] == 2
+        assert quantities["Height"] == 2
+        assert quantities["GrossFootprintArea"] == 4
+        assert quantities["NetFootprintArea"] == 4
+        assert quantities["GrossSideArea"] == 4
+        assert quantities["NetSideArea"] == 4
+        assert quantities["GrossVolume"] == 8
+        assert quantities["NetVolume"] == 8
+
+class TestAddObjectBaseQto(test.bim.bootstrap.NewFile):
+    def test_run(self):
+        ifc = ifcopenshell.file()
+        tool.Ifc.set(ifc)
+        project = ifcopenshell.api.run("root.create_entity", ifc, ifc_class="IfcProject", name="My Project")
+        context = ifcopenshell.api.run("context.add_context", ifc, context_type="Model")
+        bpy.ops.mesh.primitive_cube_add(location=(0.0,0.0,0.0), size = 2)
+        obj = bpy.context.active_object
+        element = blenderbim.core.root.assign_class(tool.Ifc,
+                                                    tool.Collector,
+                                                    tool.Root,
+                                                    obj=obj,
+                                                    ifc_class="IfcWall",
+                                                    predefined_type = "ELEMENTEDWALL",
+                                                    context = context)
+        assert subject.add_object_base_qto(obj).Name == "Qto_WallBaseQuantities"
+
+class TestAddProductBaseQto(test.bim.bootstrap.NewFile):
+    def test_run(self):
+        ifc = ifcopenshell.file()
+        tool.Ifc.set(ifc)
+        wall = ifc.createIfcWall()
+        base_qto = subject.add_product_base_qto(wall)
+        assert base_qto.Name == "Qto_WallBaseQuantities"
+
 
 
 class TestGetBaseQto(test.bim.bootstrap.NewFile):
