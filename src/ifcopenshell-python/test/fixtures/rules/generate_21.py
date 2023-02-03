3f4417c9f205ac6c00c422db04b93db5608647aa
Thomas Krijnen <t.krijnen@gmail.com>
2023-02-03 21:17:48 +0100

    Window lining test case

---
+++ src/ifcopenshell-python/test/fixtures/rules/generate_21.py
@@ -0,0 +1,13 @@
+import itertools
+import ifcopenshell
+
+def EXISTS(v): return v is not None
+def NOT(v): return not v
+
+for LiningDepth, LiningThickness in itertools.product((None, 1.), (None, 1.)):
+    f = ifcopenshell.file(schema="IFC4")
+    valid = NOT(EXISTS(LiningDepth) and NOT(EXISTS(LiningThickness)))
+    f.createIfcWindowType(ifcopenshell.guid.new(), None, 'WindowType', HasPropertySets=[
+        f.createIfcWindowLiningProperties(ifcopenshell.guid.new(), LiningDepth=LiningDepth, LiningThickness=LiningThickness)
+    ])
+    f.write(f"{'pass' if valid else 'fail'}-lining-properties-{LiningDepth}-{LiningThickness}.ifc")
