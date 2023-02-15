IfcOpenShell/IfcOpenShell

commit cf87852d9ce442e59fe3f08dd44553b59afd41a5

Add API documentation for the material module

src/ifcopenshell-python/ifcopenshell/api/material/copy_material.py
@@ -21,9 +21,46 @@ import ifcopenshell.util.element
 
 
 class Usecase:
-    def __init__(self, file, **settings):
+    def __init__(self, file, material=None, element=None):
+        """Copies a material to an element
+
+        This convenience function exists to allow you to easily unassign any
+        existing material an element has, and assign a new material to it.
+        Essentially, it is a form a copy / pasting a material from one element
+        to another.
+
+        It's a bit out of place, and will likely be redesigned or removed in the
+        future.  It's not recommended to use this function, and
+        ifcopenshell.api.material.assign_material should be used instead.
+
+        :param material: The IfcMaterial to assign to the element.
+        :type material: ifcopenshell.entity_instance.entity_instance
+        :param element: The IfcElement or IfcElementType to remove all previous
+            materials from and assign the new material to.
+        :type element: ifcopenshell.entity_instance.entity_instance
+        :return: None
+        :rtype: None
+
+        Example::
+
+            concrete = ifcopenshell.api.run("material.add_material", model, name="CON01", category="concrete")
+            wood = ifcopenshell.api.run("material.add_material", model, name="TIM01", category="wood")
+
+            # Let's imagine a concrete bench made out of concrete.
+            bench = ifcopenshell.api.run("root.create_entity", model, ifc_class="IfcFurniture")
+            ifcopenshell.api.run("material.assign_material", model,
+                product=bench, type="IfcMaterial", material=concrete)
+
+            # And some street furniture made from wood.
+            street_furniture = ifcopenshell.api.run("root.create_entity", model, ifc_class="IfcFurniture")
+            ifcopenshell.api.run("material.assign_material", model,
+                product=street_furniture, type="IfcMaterial", material=wood)
+
+            # Let's copy the concrete over to the street furniture.
+            ifcopenshell.api.run("material.copy_material", model, material=wood, element=street_furniture)
+        """
         self.file = file
-        self.settings = {"material": None, "element": None}
+        self.settings = {"material": material, "element": element}
         for key, value in settings.items():
             self.settings[key] = value
 

src/ifcopenshell-python/ifcopenshell/api/material/reorder_set_item.py
@@ -18,9 +18,43 @@
 
 
 class Usecase:
-    def __init__(self, file, **settings):
+    def __init__(self, file, material_set=None, old_index=0, new_index=0):
+        """Reorders an item in a material set
+
+        In some material sets, the order have meaning, like in a layer set. In
+        other cases, it is purely for human convenience.
+
+        :param material_set: The IfcMaterialSet which you want to reorder an
+            item in.
+        :type material_set: ifcopenshell.entity_instance.entity_instance
+        :param old_index: The index of the item you want to move. This starts
+            counting from 0.
+        :type old_index: int
+        :param new_index: The index of the new position the item will move to.
+            This starts counting from 0.
+        :type new_index: int
+        :return: None
+        :rtype: None
+
+        Example::
+
+            material_set = ifcopenshell.api.run("material.add_material_set", model,
+                name="Window", set_type="IfcMaterialList")
+
+            aluminium = ifcopenshell.api.run("material.add_material", model, name="AL01", category="aluminium")
+            glass = ifcopenshell.api.run("material.add_material", model, name="GLZ01", category="glass")
+
+            # Now let's use those materials as two items in our list.
+            ifcopenshell.api.run("material.add_list_item", model, material_list=material_set, material=aluminium)
+            ifcopenshell.api.run("material.add_list_item", model, material_list=material_set, material=glass)
+
+            # Switch the order around, this has no meaning for a list, so this
+            # is just for fun.
+            ifcopenshell.api.run("material.reorder_set_item", model,
+                material_set=material_set, old_index=0, new_index=1)
+        """
         self.file = file
-        self.settings = {"material_set": None, "old_index": 0, "new_index": 0}
+        self.settings = {"material_set": material_set, "old_index": old_index, "new_index": new_index}
         for key, value in settings.items():
             self.settings[key] = value
 

src/ifcopenshell-python/ifcopenshell/api/material/add_constituent.py
@@ -18,11 +18,68 @@
 
 
 class Usecase:
-    def __init__(self, file, **settings):
+    def __init__(self, file, constituent_set=None, material=None):
+        """Adds a new constituent to a constituent set
+
+        A constituent describes how a portion of an object is made out of a
+        material whereas other portions of the object is made out of other
+        materials. For example, a window might be made out of an aluminium frame
+        and a glass panel. The aluminium used for the frame is one constituent
+        of the material, and glass would be another constituent. Another example
+        might be concrete, where one constituent might be cement, and another
+        constituent might be binder. In the case of the window, the constituent
+        is represented explicitly by the geometry of the window frame and the
+        geometry of the window panel. In the case of a concrete slab, the
+        constituents might be represented in terms of percentages.
+
+        Constituents are not available in IFC2X3.
+
+        :param constituent_set: The IfcMaterialConstituentSet that the
+            constituent is part of. The constituent set represents a group of
+            constituents. See ifcopenshell.api.material.add_material_set for
+            information on how to add a constituent set.
+        :type constituent_set: ifcopenshell.entity_instance.entity_instance
+        :param material: The IfcMaterial that the constituent is made out of.
+        :type material: ifcopenshell.entity_instance.entity_instance
+        :return: The newly created IfcMaterialConstituent
+        :rtype: ifcopenshell.entity_instance.entity_instance
+
+        Example::
+
+            # Let's imagine we have a window type that has an aluminium frame
+            # and a glass glazing panel. Notice we are assigning to the type
+            # only, as all occurrences of that type will automatically inherit
+            # the material.
+            window_type = ifcopenshell.api.run("root.create_entity", model, ifc_class="IfcWindowType")
+
+            # First, let's create a constituent set. This will later be assigned
+            # to our window element.
+            material_set = ifcopenshell.api.run("material.add_material_set", model,
+                name="Window", set_type="IfcMaterialConstituentSet")
+
+            # Let's create a few materials, it's important to also give them
+            # categories. This makes it easy for model recipients to do things
+            # like "show me everything made out of aluminium / concrete / steel
+            # / glass / etc". The IFC specification states a list of categories
+            # you can use.
+            aluminium = ifcopenshell.api.run("material.add_material", model, name="AL01", category="aluminium")
+            glass = ifcopenshell.api.run("material.add_material", model, name="GLZ01", category="glass")
+
+            # Now let's use those materials as two constituents in our set.
+            ifcopenshell.api.run("material.add_constituent", model,
+                constituent_set=material_set, material=aluminium)
+            ifcopenshell.api.run("material.add_constituent", model,
+                constituent_set=material_set, material=glass)
+
+            # Great! Let's assign our material set to our window type.
+            # We're technically not done here, we might want to add geometry to
+            # our window too, but to keep this example simple, geometry is
+            # optional and it is enough to say that this window is made out of
+            # aluminium and glass.
+            ifcopenshell.api.run("material.assign_material", model, product=window_type, material=material_set)
+        """
         self.file = file
-        self.settings = {"constituent_set": None, "material": None}
-        for key, value in settings.items():
-            self.settings[key] = value
+        self.settings = {"constituent_set": constituent_set, "material": material}
 
     def execute(self):
         constituents = list(self.settings["constituent_set"].MaterialConstituents or [])

src/ifcopenshell-python/ifcopenshell/api/material/add_layer.py
@@ -18,11 +18,66 @@
 
 
 class Usecase:
-    def __init__(self, file, **settings):
+    def __init__(self, file, layer_set=None, material=None):
+        """Adds a new layer to a layer set
+
+        A layer represents a portion of material within a layered build up,
+        defined by a thickness. Typical layered construction includes walls and
+        slabs, where a wall might include a layer of finish, a layer of
+        structure, a layer of insulation, and so on. It is recommended to define
+        layered construction this way where it is unnecessary to define the
+        exact geometry of how the wall or slab will be built, and it will
+        instead be determined on site by a trade.
+
+        Layers are defined in a particular order and thickness, so that it is
+        clear which layer comes next.
+
+        :param layer_set: The IfcMaterialLayerSet that the layer is part of. The
+            layer set represents a group of layers. See
+            ifcopenshell.api.material.add_material_set for more information on
+            how to add a layer set.
+        :type layer_set: ifcopenshell.entity_instance.entity_instance
+        :param material: The IfcMaterial that the layer is made out of.
+        :type material: ifcopenshell.entity_instance.entity_instance
+        :return: The newly created IfcMaterialLayer
+        :rtype: ifcopenshell.entity_instance.entity_instance
+
+        Example::
+
+            # Let's imagine we have a wall type that has two layers of
+            # gypsum with steel studs inside. Notice we are assigning to
+            # the type only, as all occurrences of that type will automatically
+            # inherit the material.
+            wall_type = ifcopenshell.api.run("root.create_entity", model, ifc_class="IfcWallType", name="WAL01")
+
+            # First, let's create a material set. This will later be assigned
+            # to our wall type element.
+            material_set = ifcopenshell.api.run("material.add_material_set", model,
+                name="GYP-ST-GYP", set_type="IfcMaterialLayerSet")
+
+            # Let's create a few materials, it's important to also give them
+            # categories. This makes it easy for model recipients to do things
+            # like "show me everything made out of aluminium / concrete / steel
+            # / glass / etc". The IFC specification states a list of categories
+            # you can use.
+            gypsum = ifcopenshell.api.run("material.add_material", model, name="PB01", category="gypsum")
+            steel = ifcopenshell.api.run("material.add_material", model, name="ST01", category="steel")
+
+            # Now let's use those materials as three layers in our set, such
+            # that the steel studs are sandwiched by the gypsum. Let's imagine
+            # we're setting the layer thickness in millimeters.
+            layer = ifcopenshell.api.run("material.add_layer", model, layer_set=material_set, material=gypsum)
+            ifcopenshell.api.run("material.edit_layer", model, layer=layer, attributes={"LayerThickness": 13})
+            layer = ifcopenshell.api.run("material.add_layer", model, layer_set=material_set, material=steel)
+            ifcopenshell.api.run("material.edit_layer", model, layer=layer, attributes={"LayerThickness": 92})
+            layer = ifcopenshell.api.run("material.add_layer", model, layer_set=material_set, material=gypsum)
+            ifcopenshell.api.run("material.edit_layer", model, layer=layer, attributes={"LayerThickness": 13})
+
+            # Great! Let's assign our material set to our wall type.
+            ifcopenshell.api.run("material.assign_material", model, product=wall_type, material=material_set)
+        """
         self.file = file
-        self.settings = {"layer_set": None, "material": None}
-        for key, value in settings.items():
-            self.settings[key] = value
+        self.settings = {"layer_set": layer_set, "material": material}
 
     def execute(self):
         layers = list(self.settings["layer_set"].MaterialLayers or [])

src/ifcopenshell-python/ifcopenshell/api/material/add_list_item.py
@@ -20,11 +20,65 @@ import ifcopenshell
 
 
 class Usecase:
-    def __init__(self, file, **settings):
+    def __init__(self, file, material_list=None, material=None):
+        """Adds a new material in a list of materials
+
+        In IFC2X3, if you wanted an object to have multiple materials (i.e. a
+        composite material) you would assign the object to a material list,
+        which would contain a list of materials. For example, a window might
+        have a list of 2 materials, one being aluminium for the frame, and
+        another being glass for the panel.
+
+        In IFC4 and above, this is deprecated and should not be used. Instead,
+        you should use constituent sets instead, which achieve the same thing
+        but are more powerful as they allow you to define the properties of the
+        constituents too.
+
+        However if you're stuck on IFC2X3, you have my condolences as well as
+        this function.
+
+        :param material_list: The IfcMaterialList the material should be added
+            to.
+        :type material_list: ifcopenshell.entity_instance.entity_instance
+        :param material: The IfcMaterial to add to the list
+        :type material: ifcopenshell.entity_instance.entity_instance
+        :return: None
+        :rtype: None
+
+        Example::
+
+            # Let's imagine we have a window type that has an aluminium frame
+            # and a glass glazing panel. Notice we are assigning to the type
+            # only, as all occurrences of that type will automatically inherit
+            # the material.
+            window_type = ifcopenshell.api.run("root.create_entity", model, ifc_class="IfcWindowType")
+
+            # First, let's create a list. This will later be assigned to our
+            # window element.
+            material_set = ifcopenshell.api.run("material.add_material_set", model,
+                name="Window", set_type="IfcMaterialList")
+
+            # Let's create a few materials, it's important to also give them
+            # categories. This makes it easy for model recipients to do things
+            # like "show me everything made out of aluminium / concrete / steel
+            # / glass / etc". The IFC specification states a list of categories
+            # you can use.
+            aluminium = ifcopenshell.api.run("material.add_material", model, name="AL01", category="aluminium")
+            glass = ifcopenshell.api.run("material.add_material", model, name="GLZ01", category="glass")
+
+            # Now let's use those materials as two items in our list.
+            ifcopenshell.api.run("material.add_list_item", model, material_list=material_set, material=aluminium)
+            ifcopenshell.api.run("material.add_list_item", model, material_list=material_set, material=glass)
+
+            # Great! Let's assign our material set to our window type.
+            # We're technically not done here, we might want to add geometry to
+            # our window too, but to keep this example simple, geometry is
+            # optional and it is enough to say that this window is made out of
+            # aluminium and glass.
+            ifcopenshell.api.run("material.assign_material", model, product=window_type, material=material_set)
+        """
         self.file = file
-        self.settings = {"material_list": None, "material": None}
-        for key, value in settings.items():
-            self.settings[key] = value
+        self.settings = {"material_list": material_list, "material": material}
 
     def execute(self):
         materials = list(self.settings["material_list"].Materials or [])

src/ifcopenshell-python/ifcopenshell/api/material/add_material.py
@@ -18,11 +18,61 @@
 
 
 class Usecase:
-    def __init__(self, file, **settings):
+    def __init__(self, file, name=None, category=None):
+        """Adds a new material
+
+        A material in IFC represents a physical material, such as timber, steel,
+        concrete, aluminium, etc. It may also contain physical properties used
+        for structural or lighting simulation. Note that unlike the computer
+        graphics industry, a material by itself does not define any colour or
+        lighting information. Colours in IFC are known as "styles", and an IFC
+        material may or may not have any style information associated with it.
+        See ifcopenshell.api.style for more information.
+
+        A material is typically given a code name which is used by architects in
+        elevations and details when tagging finishes. Materials are also useful
+        to structural engineers in specifying the exact types of concrete and
+        steel to be used in structural simulations.
+
+        In addition, materials can belong to a category. Specifying this
+        category is critical to allow model recipients to make simple queries
+        like "show me all concrete / steel" elements in the model. Without
+        standardised category naming of all materials, this type of query
+        becomes a bespoke and inefficient task. A list of categories are:
+        'concrete', 'steel', 'aluminium', 'block', 'brick', 'stone', 'wood',
+        'glass', 'gypsum', 'plastic', and 'earth'. The user is allowed to
+        specify their own category instead if none of these categories are
+        appropriate.
+
+        Note that categories are not available in IFC2X3. This shortcoming is
+        one of the big reasons projects should upgrade to IFC4.
+
+        :param name: The name of the material, typically tagged in a finishes
+            drawing or schedule.
+        :type name: str
+        :param category: The category of the material.
+        :type category: str, optional
+        :return: The newly created IfcMaterial
+        :rtype: ifcopenshell.entity_instance.entity_instance
+
+        Example::
+
+            # Let's create two materials with their respective categories
+            concrete = ifcopenshell.api.run("material.add_material", model, name="CON01", category="concrete")
+            steel = ifcopenshell.api.run("material.add_material", model, name="ST01", category="steel")
+
+            # Let's imagine an urban concrete bench which is purely made out of concrete
+            concrete_bench = ifcopenshell.api.run("root.create_entity", model, ifc_class="IfcFurnitureType")
+
+            # Assign the concrete material to that bench. Note that no colour
+            # "Style" has been specified.
+            ifcopenshell.api.run("material.assign_material", model, product=concrete_bench, material=concrete)
+        """
         self.file = file
-        self.settings = {"name": "Unnamed"}
-        for key, value in settings.items():
-            self.settings[key] = value
+        self.settings = {"name": name or "Unnamed", "category": category}
 
     def execute(self):
-        return self.file.create_entity("IfcMaterial", **{"Name": self.settings["name"] or "Unnamed"})
+        material = self.file.create_entity("IfcMaterial", **{"Name": self.settings["name"] or "Unnamed"})
+        if self.settings["category"]:
+            material.Category = self.settings["category"]
+        return material

src/ifcopenshell-python/ifcopenshell/api/material/add_material_set.py
@@ -18,11 +18,90 @@
 
 
 class Usecase:
-    def __init__(self, file, **settings):
+    def __init__(self, file, name="Unnamed", set_type="IfcMaterialConstituentSet"):
+        """Adds a new material set
+
+        IFC allows you to state that objects are made out of multiple materials.
+        These are known generically as material sets, but may also be called
+        layered materials, composite materials, or other names in software.
+
+        There are three types of material sets:
+
+         - A layer set, used for layered construction such as walls, where the
+           element is parametrically made out of extruded layers, each layer
+           having a thickness defined. Even though this is known as a layer
+           "set" it is still recommended to use it for all standared layered
+           construction as it describes the intent of the element to be layered
+           construction and thus can be used for parametric editing.
+         - A profile set, used for profiled construction such as beams or
+           columns, where the element is parametrically made out of one or more
+           extruded profiles, where each profile may be parametric from a
+           standard section (e.g. standardised steel profile) or an arbitrary
+           shape (e.g. cold rolled sections, or skirtings, moldings, etc). Note
+           that even though this is called a profile "set", it should still be
+           used even if there is only a single profile. This is not available in
+           IFC2X3.
+         - A constituent set, used for arbitrary composite construction where
+           the object is made out of multiple materials. The constituents may be
+           explicitly defined via a shape, such as a window where the frame
+           geometry is made from one material and the panel geometry is made
+           from another material. Alternatively, the constituents may be
+           represented in terms of percentages, such as in mixtures like
+           concrete where there might be a percentage constituent of cement and
+           another percentage constituent of binder. This is not available in
+           IFC2X3.
+
+        There is also a fourth material set known as a material list, which is a
+        legacy type of set used by IFC2X3. It should not be used on IFC4 and
+        above, and constituent sets should be used instead.
+
+        :param name: The name of the material set, which may be purely
+            descriptive or annotated in drawings. Defaults to "Unnamed".
+        :type name: str, optional
+        :param set_type: What type of set you want to create, chosen from
+            IfcMaterialLayerSet, IfcMaterialProfileSet,
+            IfcMaterialConstituentSet, or IfcMaterialList. Defaults to
+            IfcMaterialConstituentSet.
+        :type set_type: str, optional
+        :return: The newly created material set element
+        :rtype: ifcopenshell.entity_instance.entity_instance
+
+        Example::
+
+            # Let's imagine we have a wall type that has two layers of
+            # gypsum with steel studs inside. Notice we are assigning to
+            # the type only, as all occurrences of that type will automatically
+            # inherit the material.
+            wall_type = ifcopenshell.api.run("root.create_entity", model, ifc_class="IfcWallType", name="WAL01")
+
+            # First, let's create a material set. This will later be assigned
+            # to our wall type element.
+            material_set = ifcopenshell.api.run("material.add_material_set", model,
+                name="GYP-ST-GYP", set_type="IfcMaterialLayerSet")
+
+            # Let's create a few materials, it's important to also give them
+            # categories. This makes it easy for model recipients to do things
+            # like "show me everything made out of aluminium / concrete / steel
+            # / glass / etc". The IFC specification states a list of categories
+            # you can use.
+            gypsum = ifcopenshell.api.run("material.add_material", model, name="PB01", category="gypsum")
+            steel = ifcopenshell.api.run("material.add_material", model, name="ST01", category="steel")
+
+            # Now let's use those materials as three layers in our set, such
+            # that the steel studs are sandwiched by the gypsum. Let's imagine
+            # we're setting the layer thickness in millimeters.
+            layer = ifcopenshell.api.run("material.add_layer", model, layer_set=material_set, material=gypsum)
+            ifcopenshell.api.run("material.edit_layer", model, layer=layer, attributes={"LayerThickness": 13})
+            layer = ifcopenshell.api.run("material.add_layer", model, layer_set=material_set, material=steel)
+            ifcopenshell.api.run("material.edit_layer", model, layer=layer, attributes={"LayerThickness": 92})
+            layer = ifcopenshell.api.run("material.add_layer", model, layer_set=material_set, material=gypsum)
+            ifcopenshell.api.run("material.edit_layer", model, layer=layer, attributes={"LayerThickness": 13})
+
+            # Great! Let's assign our material set to our wall type.
+            ifcopenshell.api.run("material.assign_material", model, product=wall_type, material=material_set)
+        """
         self.file = file
-        self.settings = {"name": "Unnamed", "set_type": "IfcMaterialConstituentSet"}
-        for key, value in settings.items():
-            self.settings[key] = value
+        self.settings = {"name": name or "Unnamed", "set_type": set_type or "IfcMaterialConstituentSet"}
 
     def execute(self):
         if self.settings["set_type"] == "IfcMaterialLayerSet":

commit a6fa6201f7857ed171b542abafddc6302a01323b

Fix bugs introduced in cf87852

src/ifcopenshell-python/ifcopenshell/api/material/copy_material.py
@@ -63,8 +63,6 @@ class Usecase:
         """
         self.file = file
         self.settings = {"material": material, "element": element}
-        for key, value in settings.items():
-            self.settings[key] = value
 
     def execute(self):
         ifcopenshell.api.run("material.unassign_material", self.file, product=self.settings["element"])

src/ifcopenshell-python/ifcopenshell/api/material/reorder_set_item.py
@@ -57,8 +57,6 @@ class Usecase:
         """
         self.file = file
         self.settings = {"material_set": material_set, "old_index": old_index, "new_index": new_index}
-        for key, value in settings.items():
-            self.settings[key] = value
 
     def execute(self):
         if self.settings["material_set"].is_a("IfcMaterialConstituentSet"):