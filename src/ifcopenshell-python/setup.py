8af60a9d9f8a3400039eff721a428886934ea473
Dion Moult <dion@thinkmoult.com>
2023-02-17 12:03:25 +1100

    See #2762.

--- src/ifcopenshell-python/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-from pathlib import Path
-
-cwd = Path(__file__).parent
-long_description = (cwd / "README.md").read_text()
-
-setup(name="ifcopenshell", long_description=long_description, long_description_content_type="text/markdown")
