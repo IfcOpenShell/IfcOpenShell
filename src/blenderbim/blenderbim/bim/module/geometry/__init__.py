9c8b15fa1a6a8337f991f4839e865fd91393add4
Dion Moult <dion@thinkmoult.com>
2023-03-01 14:30:56 +1100

    Fix #2645. No longer override built-in operators for critical features.

--- src/blenderbim/blenderbim/bim/module/geometry/__init__.py
+++ src/blenderbim/blenderbim/bim/module/geometry/__init__.py
@@ -50,19 +50,25 @@ addon_keymaps = []
 def register():
     bpy.types.Scene.BIMGeometryProperties = bpy.props.PointerProperty(type=prop.BIMGeometryProperties)
     bpy.types.OBJECT_PT_transform.append(ui.BIM_PT_transform)
+    bpy.types.VIEW3D_MT_object.append(ui.object_menu)
     wm = bpy.context.window_manager
     if wm.keyconfigs.addon:
-        km = wm.keyconfigs.addon.keymaps.new(name="3D View", space_type="VIEW_3D")
+        km = wm.keyconfigs.addon.keymaps.new(name="Object Mode", space_type="EMPTY")
+        kmi = km.keymap_items.new("bim.override_object_duplicate_move", "D", "PRESS", shift=True)
+        kmi = km.keymap_items.new("bim.override_object_duplicate_move_linked", "D", "PRESS", alt=True)
         kmi = km.keymap_items.new("bim.override_paste_buffer", "V", "PRESS", ctrl=True)
-        kmi.properties["name"] = "bim.override_paste_buffer"
-        addon_keymaps.append((km, kmi))
+        kmi = km.keymap_items.new("bim.override_object_delete", "X", "PRESS")
+        kmi = km.keymap_items.new("bim.override_object_delete", "DEL", "PRESS")
+        kmi.properties.confirm = False
+
         km = wm.keyconfigs.addon.keymaps.new(name="Outliner", space_type="OUTLINER")
         kmi = km.keymap_items.new("bim.override_paste_buffer", "V", "PRESS", ctrl=True)
-        kmi.properties["name"] = "bim.override_paste_buffer"
-        addon_keymaps.append((km, kmi))
+        kmi = km.keymap_items.new("bim.override_outliner_delete", "X", "PRESS")
+        kmi = km.keymap_items.new("bim.override_outliner_delete", "DEL", "PRESS")
 
 
 def unregister():
+    bpy.types.VIEW3D_MT_object.remove(ui.object_menu)
     bpy.types.OBJECT_PT_transform.remove(ui.BIM_PT_transform)
     del bpy.types.Scene.BIMGeometryProperties
     wm = bpy.context.window_manager
