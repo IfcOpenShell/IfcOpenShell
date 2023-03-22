297303063e3a866d2621c1541a4fe83b1758161d
Andrej730 <azhilenkov@gmail.com>
2023-03-22 16:58:13 +0500

    Small changes in default names for saving ifc projects
    1) changed first ifc saving handler from save_pre to save_post so that in the situation when you just created with .blend file and started ifc project on save dialog would suggest the same name for the .ifc file and for .blend file (previously it was always suggesting ".ifc" and you had to type it in manually).
    
    2) suggested default ifc project named changed from `<blend_filename>.blend.ifc` to `<blend_filename>.ifc` for simplicity.
    
    PS save_pre didn't really work anyway - by the time "save ifc dialog" file was invoked and appeared .blend file was already saved anyway.

--- src/blenderbim/blenderbim/bim/module/project/operator.py
+++ src/blenderbim/blenderbim/bim/module/project/operator.py
@@ -35,6 +35,7 @@ from blenderbim.bim.ifc import IfcStore
 from blenderbim.bim.ui import IFCFileSelector
 from blenderbim.bim import import_ifc
 from blenderbim.bim import export_ifc
+from pathlib import Path
 
 
 class CreateProject(bpy.types.Operator):
@@ -828,7 +829,7 @@ class ExportIFC(bpy.types.Operator):
                 self.filepath = os.path.abspath(os.path.join(bpy.path.abspath("//"), self.filepath))
             return self.execute(context)
         if not self.filepath:
-            self.filepath = bpy.path.ensure_ext(bpy.data.filepath, ".ifc")
+            self.filepath = Path(bpy.data.filepath).with_suffix(".ifc").__str__()
         WindowManager = context.window_manager
         WindowManager.fileselect_add(self)
         return {"RUNNING_MODAL"}
