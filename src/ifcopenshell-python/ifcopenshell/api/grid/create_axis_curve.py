1a117b475254d6cf7bcfa1757928d18c1c2e5b64
Dion Moult <dion@thinkmoult.com>
2023-07-12 11:41:47 +1000

    Fix #3425. Fix invalid IFC data where grid axes should be 2D not 3D.

--- src/ifcopenshell-python/ifcopenshell/api/grid/create_axis_curve.py
+++ src/ifcopenshell-python/ifcopenshell/api/grid/create_axis_curve.py
@@ -77,8 +77,8 @@ class Usecase:
         ]
         self.settings["grid_axis"].AxisCurve = self.file.createIfcPolyline(
             [
-                self.create_cartesian_point(points[0][0], points[0][1], points[0][2]),
-                self.create_cartesian_point(points[1][0], points[1][1], points[1][2]),
+                self.create_cartesian_point(points[0][0], points[0][1]),
+                self.create_cartesian_point(points[1][0], points[1][1]),
             ]
         )
 
