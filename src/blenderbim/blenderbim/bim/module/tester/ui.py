772414451e339fd93fc7b59e0a74d1c9cd922143
c4rlosdias <c4rlos.dias@gmail.com>
2023-07-14 12:04:22 -0300

    update ifctester - 2023_07_14

--- src/blenderbim/blenderbim/bim/module/tester/ui.py
+++ src/blenderbim/blenderbim/bim/module/tester/ui.py
@@ -17,7 +17,7 @@
 # along with BlenderBIM Add-on.  If not, see <http://www.gnu.org/licenses/>.
 
 import blenderbim.tool as tool
-from bpy.types import Panel
+from bpy.types import Panel, UIList
 
 
 class BIM_PT_tester(Panel):
@@ -32,20 +32,53 @@ class BIM_PT_tester(Panel):
     def draw(self, context):
         self.layout.use_property_split = True
 
-        props = context.scene.IfcTesterProperties
+        self.props = context.scene.IfcTesterProperties
 
         if tool.Ifc.get():
             row = self.layout.row()
-            row.prop(props, "should_load_from_memory")
+            row.prop(self.props, "should_load_from_memory")
 
-        if not tool.Ifc.get() or not props.should_load_from_memory:
+        if not tool.Ifc.get() or not self.props.should_load_from_memory:
             row = self.layout.row(align=True)
-            row.prop(props, "ifc_file")
+            row.prop(self.props, "ifc_file")
             row.operator("bim.select_ifctester_ifc_file", icon="FILE_FOLDER", text="")
 
         row = self.layout.row(align=True)
-        row.prop(props, "specs")
+        row.prop(self.props, "specs")
         row.operator("bim.select_specs", icon="FILE_FOLDER", text="")
 
         row = self.layout.row()
-        row.operator("bim.execute_ifc_tester")
+        result = row.operator("bim.execute_ifc_tester")
+
+        if self.props.has_report:
+            self.layout.template_list(
+                "BIM_UL_tester",
+                "",
+                self.props,
+                "specifications",
+                self.props,
+                "active_specification_index",
+            )
+
+            self.draw_editable_ui(context)
+
+    def draw_editable_ui(self, context):
+        props = context.scene.IfcTesterProperties
+        row = self.layout.row()
+        row.label(text="Failed entities: " + str(props.entities))
+        
+
+        
+class BIM_UL_tester(UIList):
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
+        props = context.scene.IfcTesterProperties
+        if item:
+            icon = "MOD_MESHDEFORM"
+            row = layout.row(align=True)
+            row.label(text=item.name or "Unnamed", icon=icon)
+            if item.status:
+                row.label(text="PASS")
+            else:
+                row.label(text="FAIL")
+            op = row.operator("bim.select_specification", text="", icon="VIEWZOOM")
+            op.spec = item.failed_entities
