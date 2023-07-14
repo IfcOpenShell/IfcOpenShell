207a2bb1ef439d2d5ca3d7436709c7a0dcea50be
Andrej730 <azhilenkov@gmail.com>
2023-07-14 14:46:36 +0500

    avoid IfcSpatialElement errors in ifc2x3
    IfcSpatialElement was added only in ifc4

--- src/blenderbim/blenderbim/tool/geometry.py
+++ src/blenderbim/blenderbim/tool/geometry.py
@@ -99,7 +99,7 @@ class Geometry(blenderbim.core.tool.Geometry):
                 if element.VoidsElements:
                     bpy.ops.bim.remove_opening(opening_id=element.id())
         else:
-            is_spatial = element.is_a("IfcSpatialElement")
+            is_spatial = element.is_a() in ("IfcSpatialElement", "IfcSpatialStructureElement")
             if getattr(element, "HasOpenings", None):
                 for rel in element.HasOpenings:
                     bpy.ops.bim.remove_opening(opening_id=rel.RelatedOpeningElement.id())
