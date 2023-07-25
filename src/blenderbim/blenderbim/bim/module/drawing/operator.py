IfcOpenShell/IfcOpenShell

commit c1c49ba9cc826d94c7cf76982945d3c79a2d97c1

See #3421. Fix drawing bug where IfcOpenShell might return unclosed paths even though the shape is closed and therefore break cut polygon fills.

src/blenderbim/blenderbim/bim/module/drawing/operator.py
@@ -892,6 +892,42 @@ class CreateDrawing(bpy.types.Operator):
             classes.append("cut")
             el.set("class", " ".join(classes))
 
+            # An element group will contain a bunch of paths representing the
+            # cut of that element. However IfcOpenShell may not correctly
+            # create closed paths. We post-process all paths with shapely to
+            # ensure things that should be closed (i.e.
+            # shapely.polygonize_full) are, and things which aren't are left
+            # alone (e.g. dangles, cuts, invalids). See #3421.
+            line_strings = []
+            old_paths = []
+            for path in el.findall("{http://www.w3.org/2000/svg}path"):
+                for subpath in path.attrib["d"].split("M")[1:]:
+                    subpath = "M" + subpath.strip()
+                    coords = [[round(float(o), 1) for o in co[1:].split(",")] for co in subpath.split()]
+                    line_strings.append(shapely.LineString(coords))
+                old_paths.append(path)
+            unioned_line_strings = shapely.union_all(shapely.GeometryCollection(line_strings))
+            if hasattr(unioned_line_strings, "geoms"):
+                results = shapely.polygonize_full(unioned_line_strings.geoms)
+            else:
+                results = []
+
+            # If we succeeded in generating new path geometry, remove all the
+            # old paths and add new ones.
+            if results:
+                for path in old_paths:
+                    path.getparent().remove(path)
+            for result in results:
+                for geom in result.geoms:
+                    path = etree.SubElement(el, "path")
+                    if isinstance(geom, shapely.Polygon):
+                        d = "M" + " L".join([",".join([str(o) for o in co]) for co in geom.exterior.coords[0:-1]]) + " Z"
+                        for interior in geom.interiors:
+                            d += " M" + " L".join([",".join([str(o) for o in co]) for co in interior.coords[0:-1]]) + " Z"
+                    elif isinstance(geom, shapely.LineString):
+                        d = "M" + " L".join([",".join([str(o) for o in co]) for co in geom.coords]) + " Z"
+                    path.attrib["d"] = d
+
             # Architectural convention only merges these objects. E.g. pipe segments and fittings shouldn't merge.
             if not element.is_a("IfcWall") and not element.is_a("IfcSlab"):
                 continue

commit 3d7d0d2a436a117cc76cc636e24977857f70c50f

Fix #3479. Regression in c1c49ba when addressing #3421 that led to objects with multiple paths not taking part in the wall / slab joining procedure.

src/blenderbim/blenderbim/bim/module/drawing/operator.py
@@ -919,7 +919,7 @@ class CreateDrawing(bpy.types.Operator):
                     path.getparent().remove(path)
             for result in results:
                 for geom in result.geoms:
-                    path = etree.SubElement(el, "path")
+                    path = etree.SubElement(el, "{http://www.w3.org/2000/svg}path")
                     if isinstance(geom, shapely.Polygon):
                         d = (
                             "M"
@@ -963,8 +963,10 @@ class CreateDrawing(bpy.types.Operator):
                 is_closed_polygon = False
                 for path in el.findall("{http://www.w3.org/2000/svg}path"):
                     for subpath in path.attrib["d"].split("M")[1:]:
-                        subpath = "M" + subpath.strip()
-                        coords = [[round(float(o), 1) for o in co[1:].split(",")] for co in subpath.split()]
+                        subpath_co = "M" + subpath.strip(" Z")
+                        coords = [[round(float(o), 1) for o in co[1:].split(",")] for co in subpath_co.split()]
+                        if subpath.strip().lower().endswith("z"):
+                            coords.append(coords[0])
                         if len(coords) > 2 and coords[0] == coords[-1]:
                             is_closed_polygon = True
                             polygons.append(shapely.Polygon(coords))