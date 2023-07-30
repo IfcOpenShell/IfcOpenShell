356a10eac9c9c67d9bef7fe8c8c49f78e8909595
Wei Yang <w.yang.cn@outlook.com>
2023-07-30 12:34:50 +0200

    Add UV texture support for IfcTessellatedFaceSet (#3523)
    * Add texture support for IfcIndexedTextureMap
    
    * fix description.
    
    * Fix posible duplicate coordinate index issue
    
    * Clean up code.
    
    * revert default clean_mesh.
    
    * Fix errors.
    
    * change to EXTEND mode when not RepeatS/T
    
    * remove tris merge to support mix quads and tris.

--- src/blenderbim/blenderbim/bim/prop.py
+++ src/blenderbim/blenderbim/bim/prop.py
@@ -427,6 +427,7 @@ class BIMMaterialProperties(PropertyGroup):
     attributes: CollectionProperty(name="Attributes", type=Attribute)
     # In Blender, a material object can map to an IFC material, IFC surface style, or both
     ifc_style_id: IntProperty(name="IFC Style ID")
+    ifc_coordinate_id: IntProperty(name="IFC Coordinate ID")
     shading_checksum: StringProperty(name="Shading Checksum")
 
 
