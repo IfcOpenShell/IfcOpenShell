8f6946b572a17cd17bea87e9ca0252269cb8d5ac
Dion Moult <dion@thinkmoult.com>
2023-08-18 00:03:46 +1000

    Fix #3596. Fix bug where objects marked as edited were not unmarked after saving when toggling mode. Also fix bug where updating representation didn't check whether or not the meshlike representation has openings applied (which would bake the openings, instead of leaving them dynamic).

--- src/blenderbim/blenderbim/bim/prop.py
+++ src/blenderbim/blenderbim/bim/prop.py
@@ -435,6 +435,7 @@ class BIMMeshProperties(PropertyGroup):
     ifc_definition_id: IntProperty(name="IFC Definition ID")
     ifc_boolean_id: IntProperty(name="IFC Boolean ID")
     obj: bpy.props.PointerProperty(type=bpy.types.Object)
+    has_openings_applied: BoolProperty(name="Has Openings Applied", default=True)
     is_native: BoolProperty(name="Is Native", default=False)
     is_swept_solid: BoolProperty(name="Is Swept Solid")
     is_parametric: BoolProperty(name="Is Parametric", default=False)
