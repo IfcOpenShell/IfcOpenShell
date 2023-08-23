eb326fe52ed225d6faaf1c63bf6d25b608f5ebd9
Dion Moult <dion@thinkmoult.com>
2023-08-23 17:46:47 +1000

    The copy property tool now works on enumerated properties. Hooray for copying over statuses!

--- src/blenderbim/blenderbim/bim/module/pset/operator.py
+++ src/blenderbim/blenderbim/bim/module/pset/operator.py
@@ -455,7 +455,13 @@ class CopyPropertyToSelection(bpy.types.Operator, Operator):
         else:
             is_pset = context.active_object.PsetProperties.active_pset_type == "PSET"
         pset_name = context.active_object.PsetProperties.active_pset_name
-        prop_value = context.active_object.PsetProperties.properties.get(self.name).metadata.get_value()
+        prop = context.active_object.PsetProperties.properties.get(self.name)
+        if prop.value_type == "IfcPropertySingleValue":
+            prop_value = prop.metadata.get_value()
+        elif prop.value_type == "IfcPropertyEnumeratedValue":
+            value_name = prop.metadata.get_value_name()
+            prop_value = [e[value_name] for e in prop.enumerated_value.enumerated_values if e.is_selected]
+
         for obj in tool.Blender.get_selected_objects():
             core.copy_property_to_selection(
                 tool.Ifc,
