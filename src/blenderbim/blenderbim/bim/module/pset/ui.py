7a7ac30faf9a75240e926695dbddc1451383fa56
Dion Moult <dion@thinkmoult.com>
2023-08-23 17:46:47 +1000

    The copy property tool now works on enumerated properties. Hooray for copying over statuses!

--- src/blenderbim/blenderbim/bim/module/pset/ui.py
+++ src/blenderbim/blenderbim/bim/module/pset/ui.py
@@ -71,6 +71,9 @@ def draw_enumerated_property(prop, layout, copy_operator=None):
         grid = layout.column_flow(columns=3)
         for e in prop.enumerated_value.enumerated_values:
             grid.prop(e, "is_selected", text=str(e[value_name]))
+    if copy_operator:
+        op = layout.operator(f"{copy_operator}", text="", icon="COPYDOWN")
+        op.name = prop.metadata.name
 
 
 def get_active_pset_obj_name(context, obj_type):
