7bd08468d6b7d1efc7e983dd78efb4ad7c979e01
Dushyant Basson <onizu.db@gmail.com>
2023-09-02 02:29:52 +0530

    application file-icon and window-icon set on ms-windows

--- src/qtviewer/main.cpp
+++ src/qtviewer/main.cpp
@@ -6,11 +6,14 @@ int main(int argc, char *argv[])
 {
   QApplication app(argc, argv);
 
-  QIcon appIcon(":/resources/ifcopenshell_logo.png");
+  // Set the icon in the window title-bar
+  // "icon" alias defined in .qrc file
+  // (.rc file sets the file icon on mswindows)
+  QIcon appIcon(":/icon");
+  app.setWindowIcon(appIcon);
 
   MainWindow window;
 
-  window.setWindowIcon(appIcon);
   window.show();
 
   return app.exec();
