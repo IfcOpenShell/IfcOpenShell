2a303b87ba2b19d8d1cd42f785aeb61e65777724
dushyant basson <39452934+dushyant-basson@users.noreply.github.com>
2023-09-01 14:58:18 +0530

    adding qt resources

--- src/qtviewer/main.cpp
+++ src/qtviewer/main.cpp
@@ -6,7 +6,11 @@ int main(int argc, char *argv[])
 {
   QApplication app(argc, argv);
 
+  QIcon appIcon(":/resources/ifcopenshell_logo.png");
+
   MainWindow window;
+
+  window.setWindowIcon(appIcon);
   window.show();
 
   return app.exec();
