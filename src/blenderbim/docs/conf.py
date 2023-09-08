8e2daa20cf8b6ae5af7979ab181d21e3c6e089cb
Dion Moult <dion@thinkmoult.com>
2023-09-08 12:06:47 +1000

    Enable Sphinx auto section labels. Minor doc link fix.

--- src/blenderbim/docs/conf.py
+++ src/blenderbim/docs/conf.py
@@ -48,7 +48,10 @@ release = "0.0.220504"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosectionlabel"]
+
+# Auto add document prefixes to help guarantee uniqueness of automatic section references.
+autosectionlabel_prefix_document = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
