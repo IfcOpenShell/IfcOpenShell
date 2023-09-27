986ffe66f815b558bf6ba9fe1497eb5095a0a577
Dion Moult <dion@thinkmoult.com>
2023-09-27 18:21:20 +1000

    New introduction section in IOS docs

--- src/ifcopenshell-python/docs/conf.py
+++ src/ifcopenshell-python/docs/conf.py
@@ -36,7 +36,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = "IfcOpenShell"
-copyright = "2020-2022, IfcOpenShell Contributors"
+copyright = "2011-2023, IfcOpenShell Contributors"
 author = "IfcOpenShell Contributors"
 
 # The full version, including alpha/beta/rc tags
