ce892fa90cd7fbb3935461a24c1344466eccad57
Dion Moult <dion@thinkmoult.com>
2023-11-15 16:47:42 +1100

    Minor alignment of Basic FM preset to IFC property naming

--- src/ifcfm/ifcfm/basic.py
+++ src/ifcfm/ifcfm/basic.py
@@ -104,8 +104,9 @@ def get_space_data(ifc_file, element):
         "CreationDate": get_owner_creation_date(element),
         "ModelSoftware": get_owner_application(element),
         "ModelID": element.GlobalId,
-        "AreaGross": get_property(psets, "Qto_SpaceBaseQuantities", "GrossFloorArea", decimals=2),
-        "AreaNet": get_property(psets, "Qto_SpaceBaseQuantities", "NetFloorArea", decimals=2),
+        "FinishCeilingHeight": get_property(psets, "Qto_SpaceBaseQuantities", "FinishCeilingHeight", decimals=2),
+        "GrossFloorArea": get_property(psets, "Qto_SpaceBaseQuantities", "GrossFloorArea", decimals=2),
+        "NetFloorArea": get_property(psets, "Qto_SpaceBaseQuantities", "NetFloorArea", decimals=2),
     }
 
 
@@ -297,8 +298,9 @@ config = {
                 "CreationDate",
                 "ModelSoftware",
                 "ModelID",
-                "AreaGross",
-                "AreaNet",
+                "FinishCeilingHeight",
+                "GrossFloorArea",
+                "NetFloorArea",
             ],
             "colours": "ppprreeess",
             "sort": [{"name": "LevelName", "order": "ASC"}, {"name": "Name", "order": "ASC"}],
