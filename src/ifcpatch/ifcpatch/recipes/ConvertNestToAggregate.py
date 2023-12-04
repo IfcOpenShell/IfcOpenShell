2afdb5f19a93718554490879c2f470deb7681e12
Dion Moult <dion@thinkmoult.com>
2023-12-04 17:26:57 +1100

    Fix #4072. IDS reports now include filename.

--- src/ifcpatch/ifcpatch/recipes/ConvertNestToAggregate.py
+++ src/ifcpatch/ifcpatch/recipes/ConvertNestToAggregate.py
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with IfcPatch.  If not, see <http://www.gnu.org/licenses/>.
 
+import ifcopenshell
+
 
 class Patcher:
     def __init__(self, src, file, logger):
