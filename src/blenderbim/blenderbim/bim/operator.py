3786cfa666b0c95b720d8f133cca9b7625ed0e91
Andrej730 <azhilenkov@gmail.com>
2023-12-08 18:38:09 +0500

    Fixed issues with linked elements from linked blend files #4081
    In #4081 reported the case when during redo (changing param in operator's popup) `Ifc.rebuild_element_maps` was relinking elements without discarding possible linked blender libraries. It end up accidentally linking some library's objects to the current Ifc file leading to errors.
    
    Also fixed possible similar issue in couple other places.

--- src/blenderbim/blenderbim/bim/operator.py
+++ src/blenderbim/blenderbim/bim/operator.py
@@ -670,6 +670,8 @@ class ReloadIfcFile(bpy.types.Operator, tool.Ifc.Operator):
         # STEP IDs may change, but we assume the GlobalID to be constant
         obj_map = {}
         for obj in bpy.data.objects:
+            if obj.library:
+                continue
             element = tool.Ifc.get_entity(obj)
             if element and hasattr(element, "GlobalId"):
                 obj_map[obj.name] = element.GlobalId
@@ -678,6 +680,8 @@ class ReloadIfcFile(bpy.types.Operator, tool.Ifc.Operator):
         tool.Ifc.set(new)
 
         for obj in bpy.data.objects:
+            if obj.library:
+                continue
             global_id = obj_map.get(obj.name)
             if global_id:
                 try:
