6bbb509c09ba2293b81296f5d1d302b4c57500de
Andrej730 <azhilenkov@gmail.com>
2023-12-12 16:14:19 +0500

    data.py rename for consistency

--- src/blenderbim/blenderbim/bim/module/style/data.py
+++ src/blenderbim/blenderbim/bim/module/style/data.py
@@ -78,7 +78,7 @@ class StyleAttributesData:
     def load(cls):
         cls.data = {
             "ifc_style_id": cls.ifc_style_id(),
-            "attributes": cls.get_attributes(),
+            "attributes": cls.attributes(),
         }
         cls.is_loaded = True
 
@@ -87,7 +87,7 @@ class StyleAttributesData:
         return bpy.context.active_object.active_material.BIMMaterialProperties.ifc_style_id
 
     @classmethod
-    def get_attributes(cls):
+    def attributes(cls):
         style = tool.Ifc.get().by_id(bpy.context.active_object.active_material.BIMMaterialProperties.ifc_style_id)
         results = []
         for name, value in style.get_info().items():
