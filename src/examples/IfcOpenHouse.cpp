beab99bb0d933afec21411b64752a70c37f7e7be
Andrej730 <azhilenkov@gmail.com>
2024-02-07 15:24:56 +0500

    Fix issue building IfcOpenHouse for IFC4

--- src/examples/IfcOpenHouse.cpp
+++ src/examples/IfcOpenHouse.cpp
@@ -341,7 +341,7 @@ int main() {
 		null, 
 		null,
 #ifdef USE_IFC4
-		file.instances_by_type<IfcSchema::IfcWallStandardCase>()->generalize(),
+		file.instances_by_type<IfcSchema::IfcWallStandardCase>()->as<IfcSchema::IfcDefinitionSelect>(),
 #else
 		file.instances_by_type<IfcSchema::IfcWallStandardCase>()->as<IfcSchema::IfcRoot>(),
 #endif
