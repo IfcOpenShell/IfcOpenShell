049c7966292a6907e8ae6edfea31d237cffa439f
Andrej730 <azhilenkov@gmail.com>
2024-02-20 12:31:13 +0500

    IFC4.3 test bootstrap

--- src/ifcopenshell-python/test/bootstrap.py
+++ src/ifcopenshell-python/test/bootstrap.py
@@ -22,6 +22,16 @@ import ifcopenshell.api
 import ifcopenshell.api.owner.settings
 
 
+class IFC4X3:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.file: ifcopenshell.file = ifcopenshell.api.run("project.create_file", version="IFC4X3")
+        ifcopenshell.api.owner.settings.get_user = lambda ifc: (ifc.by_type("IfcPersonAndOrganization") or [None])[0]
+        ifcopenshell.api.owner.settings.get_application = lambda ifc: (ifc.by_type("IfcApplication") or [None])[0]
+        ifcopenshell.api.pre_listeners = {}
+        ifcopenshell.api.post_listeners = {}
+
+
 class IFC4:
     @pytest.fixture(autouse=True)
     def setup(self):
