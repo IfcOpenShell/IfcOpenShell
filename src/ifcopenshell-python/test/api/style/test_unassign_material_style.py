17da72232db66817c82f3e4f3d531036df2317b9
Andrej730 <azhilenkov@gmail.com>
2024-02-21 14:35:11 +0500

    unassign_material_style to handle constituents

--- src/ifcopenshell-python/test/api/style/test_unassign_material_style.py
+++ src/ifcopenshell-python/test/api/style/test_unassign_material_style.py
@@ -45,3 +45,37 @@ class TestAssignMaterialStyle(test.bootstrap.IFC4):
         assert len(self.file.by_type("IfcMaterialDefinitionRepresentation")) == 0
         assert len(self.file.by_type("IfcStyledRepresentation")) == 0
         assert len(self.file.by_type("IfcStyledItem")) == 0
+
+    def test_update_shape_aspect_representaitons_items_styles_if_material_is_part_of_matching_material_constituents(
+        self,
+    ):
+        element = ifcopenshell.api.run("root.create_entity", self.file, ifc_class="IfcWall")
+        name = "Concrete"
+        product_shape = self.file.createIfcProductDefinitionShape()
+        element.Representation = product_shape
+        context = self.file.createIfcGeometricRepresentationContext()
+
+        representation = self.file.createIfcShapeRepresentation()
+        item = self.file.createIfcExtrudedAreaSolid()
+        representation.Items = [item]
+
+        shape_aspect = self.file.createIfcShapeAspect(ShapeRepresentations=(representation,), Name=name)
+        shape_aspect.PartOfProductDefinitionShape = product_shape
+
+        style = self.file.createIfcSurfaceStyle()
+        material = ifcopenshell.api.run("material.add_material", self.file)
+        material_set = ifcopenshell.api.run(
+            "material.add_material_set", self.file, set_type="IfcMaterialConstituentSet"
+        )
+        constituent = ifcopenshell.api.run(
+            "material.add_constituent", self.file, constituent_set=material_set, material=material
+        )
+        constituent.Name = name
+        ifcopenshell.api.run("material.assign_material", self.file, product=element, material=material_set)
+
+        ifcopenshell.api.run("style.assign_material_style", self.file, material=material, style=style, context=context)
+        ifcopenshell.api.run(
+            "style.unassign_material_style", self.file, material=material, style=style, context=context
+        )
+
+        assert len(item.StyledByItem) == 0
