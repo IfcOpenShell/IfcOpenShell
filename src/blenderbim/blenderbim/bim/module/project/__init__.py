c80b4376822ff0f9ca7de440ca8cb7362ab10e5c
Andrej730 <azhilenkov@gmail.com>
2024-03-05 14:47:37 +0500

    load clipping planes decorator reopening .blend file

--- src/blenderbim/blenderbim/bim/module/project/__init__.py
+++ src/blenderbim/blenderbim/bim/module/project/__init__.py
@@ -17,7 +17,7 @@
 # along with BlenderBIM Add-on.  If not, see <http://www.gnu.org/licenses/>.
 
 import bpy
-from . import ui, prop, operator, workspace, gizmo
+from . import ui, prop, operator, workspace, gizmo, decorator
 
 classes = (
     operator.AppendEntireLibrary,
@@ -81,6 +81,7 @@ def register():
     if not bpy.app.background:
         bpy.utils.register_tool(workspace.ExploreTool, after={"builtin.transform"}, separator=True, group=False)
     bpy.types.Scene.BIMProjectProperties = bpy.props.PointerProperty(type=prop.BIMProjectProperties)
+    bpy.app.handlers.load_post.append(decorator.toggle_decorations_on_load)
     bpy.types.TOPBAR_MT_file.prepend(ui.file_menu)
     bpy.types.TOPBAR_MT_file_context_menu.prepend(ui.file_menu)
     wm = bpy.context.window_manager
@@ -101,9 +102,10 @@ def register():
 def unregister():
     if not bpy.app.background:
         bpy.utils.unregister_tool(workspace.ExploreTool)
+    del bpy.types.Scene.BIMProjectProperties
+    bpy.app.handlers.load_post.remove(decorator.toggle_decorations_on_load)
     bpy.types.TOPBAR_MT_file.remove(ui.file_menu)
     bpy.types.TOPBAR_MT_file_context_menu.remove(ui.file_menu)
-    del bpy.types.Scene.BIMProjectProperties
 
     wm = bpy.context.window_manager
     kc = wm.keyconfigs.addon
