da5b38779297dce223b76e8ccc665cf32d1f62fa
Andrej730 <azhilenkov@gmail.com>
2024-03-29 15:11:07 +0500

    import referenced drawings activating the current drawing #4478

--- src/blenderbim/blenderbim/core/drawing.py
+++ src/blenderbim/blenderbim/core/drawing.py
@@ -394,7 +394,7 @@ def sync_references(ifc, collector, drawing_tool, drawing=None):
             elif not reference_obj:
                 should_delete_existing_annotation = True
 
-        if reference_obj and (should_delete_existing_annotation or not annotation):
+        if (should_delete_existing_annotation or not annotation):
             should_create_annotation = True
 
         if should_delete_existing_annotation:
