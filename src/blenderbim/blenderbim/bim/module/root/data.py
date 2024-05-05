6046a4bdd9076e514f39711e32830a56892d8a05
Ryan Schultz <ryan@openingdesign.com>
2024-05-05 14:51:12 -0500

    proposed fix for #4622: adding IfcTypeProduct as classes that can be reassigned

--- src/blenderbim/blenderbim/bim/module/root/data.py
+++ src/blenderbim/blenderbim/bim/module/root/data.py
@@ -59,6 +59,7 @@ class IfcClassData:
             "IfcContext",
             "IfcAnnotation",
             "IfcRelSpaceBoundary",
+            "IfcTypeProduct",
         ]
         version = tool.Ifc.get_schema()
         if version == "IFC2X3":
@@ -70,6 +71,7 @@ class IfcClassData:
                 "IfcStructuralItem",
                 "IfcAnnotation",
                 "IfcRelSpaceBoundary",
+                "IfcTypeProduct",
             ]
         return [(e, e, (get_entity_doc(version, e) or {}).get("description", "")) for e in products]
 
