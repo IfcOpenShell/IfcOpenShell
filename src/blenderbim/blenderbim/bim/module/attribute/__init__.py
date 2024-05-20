6a16f9d973eb6b3356fefeec319f1b500f0d55d6
Dion Moult <dion@thinkmoult.com>
2024-05-20 23:21:13 +1000

    Deprecate material attributes panel in favour of new material manager.

--- src/blenderbim/blenderbim/bim/module/attribute/__init__.py
+++ src/blenderbim/blenderbim/bim/module/attribute/__init__.py
@@ -27,15 +27,12 @@ classes = (
     operator.CopyAttributeToSelection,
     prop.BIMAttributeProperties,
     ui.BIM_PT_object_attributes,
-    ui.BIM_PT_material_attributes,
 )
 
 
 def register():
     bpy.types.Object.BIMAttributeProperties = bpy.props.PointerProperty(type=prop.BIMAttributeProperties)
-    bpy.types.Material.BIMAttributeProperties = bpy.props.PointerProperty(type=prop.BIMAttributeProperties)
 
 
 def unregister():
     del bpy.types.Object.BIMAttributeProperties
-    del bpy.types.Material.BIMAttributeProperties
