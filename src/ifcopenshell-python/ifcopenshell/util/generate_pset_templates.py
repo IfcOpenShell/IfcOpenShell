49c1982b1fcc7992f68ffa49e66c33c674d8f346
Andrej730 <azhilenkov@gmail.com>
2024-05-28 18:01:33 +0500

    regenerate ifc4x3 pset templates after the release
    Change log
    
    Different props in pset 'Pset_BuildingCommon':
    - new props: ElevationOfTerrain, ElevationOfRefHeight
    New template 'Pset_DoorLiningProperties' with props: LiningDepth, LiningThickness, ThresholdDepth, ThresholdThickness, TransomThickness, TransomOffset, LiningOffset, ThresholdOffset, CasingThickness, CasingDepth, LiningToPanelOffsetX, LiningToPanelOffsetY
    New template 'Pset_DoorPanelProperties' with props: PanelDepth, PanelOperation, PanelWidth, PanelPosition
    New template 'Pset_PermeableCoveringProperties' with props: OperationType, PanelPosition, FrameDepth, FrameThickness
    Different props in pset 'Pset_Stationing':
    - new props: HasIncreasingStation
    New template 'Pset_WindowLiningProperties' with props: LiningDepth, LiningThickness, TransomThickness, MullionThickness, FirstTransomOffset, SecondTransomOffset, FirstMullionOffset, SecondMullionOffset, LiningOffset, LiningToPanelOffsetX, LiningToPanelOffsetY
    New template 'Pset_WindowPanelProperties' with props: OperationType, PanelPosition, FrameDepth, FrameThickness

--- src/ifcopenshell-python/ifcopenshell/util/generate_pset_templates.py
+++ src/ifcopenshell-python/ifcopenshell/util/generate_pset_templates.py
@@ -34,9 +34,9 @@ if not RUN_FROM_DEV_REPO:
     import shutil
 
 BASE_MODULE_PATH = Path(__file__).parent
-IFC4x3_HTML_LOCATION = BASE_MODULE_PATH / "IFC4.3-html-iso-release"
 
 if not RUN_FROM_DEV_REPO:
+    IFC4x3_HTML_ZIP_LOCATION = BASE_MODULE_PATH / "annex-a-psd.zip"
     IFC4x3_OUTPUT_PATH = BASE_MODULE_PATH / "schema/Pset_IFC4X3.ifc"
 else:
     IFC4x3_PSD_LOCATION = BASE_MODULE_PATH / "../output/psd"
@@ -68,20 +68,18 @@ class PsetTemplatesGenerator:
         print("Starting parsing data for IFC4X3...")
 
         if not RUN_FROM_DEV_REPO:
-            if not IFC4x3_HTML_LOCATION.is_dir():
+            if not IFC4x3_HTML_ZIP_LOCATION.is_file():
                 raise Exception(
-                    f'ISO release for Ifc4.3.0.1 expected to be in folder "{IFC4x3_HTML_LOCATION.resolve()}\\"\n'
+                    f'ISO release for Ifc4.3.2.0 expected to be located in "{IFC4x3_HTML_ZIP_LOCATION.resolve()}"\n'
                     "For generating ifc pset library please either setup docs as described above \n"
-                    "or change IFC4x3_HTML_LOCATION in the script accordingly.\n"
+                    "or change IFC4x3_HTML_ZIP_LOCATION in the script accordingly.\n"
                     "You can download docs from the repository: \n"
-                    "https://github.com/buildingSMART/IFC4.3-html/releases/tag/sep-13-release"
+                    "https://standards.buildingsmart.org/IFC/RELEASE/IFC4_3/HTML/annex-a-psd.zip"
                 )
             # unzip the data
-            if not RUN_FROM_DEV_REPO:
-                pset_data_zip = IFC4x3_HTML_LOCATION / "IFC/RELEASE/IFC4x3/HTML/annex-a-psd.zip"
-                pset_data_location = BASE_MODULE_PATH / "temp/annex-a-psd"
-                with zipfile.ZipFile(pset_data_zip, "r") as fi_zip:
-                    fi_zip.extractall(pset_data_location)
+            pset_data_location = BASE_MODULE_PATH / "temp/annex-a-psd"
+            with zipfile.ZipFile(IFC4x3_HTML_ZIP_LOCATION, "r") as fi_zip:
+                fi_zip.extractall(pset_data_location)
         else:
             if not IFC4x3_PSD_LOCATION.is_dir():
                 raise Exception(
