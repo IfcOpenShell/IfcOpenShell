3cdec9c055b348ef8fced7dfac884a9b78f872ab
Dion Moult <dion@thinkmoult.com>
2024-06-01 20:30:32 +1000

    Whoops

--- src/ifcopenshell-python/ifcopenshell/util/selector.py
+++ src/ifcopenshell-python/ifcopenshell/util/selector.py
@@ -1057,6 +1057,8 @@ class Selector:
                 value = ifcopenshell.util.element.get_container(value, ifc_class="IfcBuilding")
             elif key == "site":
                 value = ifcopenshell.util.element.get_container(value, ifc_class="IfcSite")
+            elif key == "parent":
+                element = ifcopenshell.util.element.get_parent(element)
             elif key in ("types", "occurrences"):
                 value = ifcopenshell.util.element.get_types(value)
             elif key == "count":
