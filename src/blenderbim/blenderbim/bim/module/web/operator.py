24662d75e48eddc307fb151ba40df7847ffb171a
Ziad-I <68874104+Ziad-I@users.noreply.github.com>
2024-06-11 19:03:12 +0300

    add tool methods to generate port and check port availability

--- src/blenderbim/blenderbim/bim/module/web/operator.py
+++ src/blenderbim/blenderbim/bim/module/web/operator.py
@@ -15,16 +15,25 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with BlenderBIM Add-on.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import bpy
+import blenderbim.core.web as core
+import blenderbim.tool as tool
 
 
 class ConnectToWebsocketServer(bpy.types.Operator):
-    bl_idname = "bim.connect_to_websocket_server"
-    bl_label = "Connect to websocket server"
-    bl_description = "Connect to a websocket server"
+    bl_idname = "bim.connect_websocket_server"
+    bl_label = "Connect/Start websocket server"
+    bl_description = "Start/Connect to a Websocket server"
 
     def execute(self, context):
+        port = context.scene.WebProperties.Webserver_port
+        if port == 0:
+            context.scene.WebProperties.Webserver_port = core.generate_port_number(tool.Web)
+
+        port = context.scene.WebProperties.Webserver_port
+        core.connect_websocket_server(tool.Web, port)
         return {"FINISHED"}
 
 
