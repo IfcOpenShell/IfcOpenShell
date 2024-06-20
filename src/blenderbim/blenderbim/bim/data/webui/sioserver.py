53bd35bcc762bfa93ac4e0fb1520ed40ff39fe14
Ziad-I <68874104+Ziad-I@users.noreply.github.com>
2024-06-20 16:51:51 +0300

    add handling for web operator selection
    where if user click on item in the table it gets selected in blender

--- src/blenderbim/blenderbim/bim/data/webui/sioserver.py
+++ src/blenderbim/blenderbim/bim/data/webui/sioserver.py
@@ -35,11 +35,10 @@ class WebNamespace(socketio.AsyncNamespace):
     async def on_disconnect(self, sid):
         print(f"Web client disconnected: {sid}")
 
-    async def on_operator(self, sid, data):
+    async def on_web_operator(self, sid, data):
         print(f"Web client {sid} operator: {data}")
-        json_data = json.loads(data)
         # TODO: need to add event listeners in blenderbim side for event web_operator
-        # await sio.emit("web_operator", json_data, namespace="/blender", room=json_data["blenderId"])
+        await sio.emit("web_operator", data["operator"], namespace="/blender", room=data["blenderId"])
 
 
 # Blender namespace
@@ -50,6 +49,7 @@ class BlenderNamespace(socketio.AsyncNamespace):
     async def on_connect(self, sid, environ):
         print(f"Blender client connected: {sid}")
         # Notify web client about new connection
+        blender_messages[sid] = {}
         await sio.emit("blender_connect", sid, namespace="/web")
 
     async def on_disconnect(self, sid):
@@ -59,21 +59,15 @@ class BlenderNamespace(socketio.AsyncNamespace):
             del blender_messages[sid]
         await sio.emit("blender_disconnect", sid, namespace="/web")
 
-    async def on_data(self, sid, data):
-        print(f"Data from Blender client {sid}: {data}")
-        # Store the message and forward it to the web client
-        blender_messages[sid] = data
-        await sio.emit("blender_data", {"blenderId": sid, "data": data}, namespace="/web")
-
     async def on_default_data(self, sid, data):
         print(f"Default data from Blender client {sid}: {data}")
-        # blender_messages[sid] = data
+        # blender_messages[sid]["default_data"] = data
         # await sio.emit("default_data", {"blenderId": sid, "data": data}, namespace="/web")
 
     async def on_csv_data(self, sid, data):
         print(f"CSV data from Blender client {sid}:")
         # Store the message and forward it to the web client
-        blender_messages[sid] = data
+        blender_messages[sid]["csv_data"] = data
         await sio.emit("csv_data", {"blenderId": sid, "data": data}, namespace="/web")
 
 
