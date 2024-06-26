f740ca3cf1fd4becd8786a961ea86e42b0ee3391
Dion Moult <dion@thinkmoult.com>
2024-06-26 13:02:59 +1000

    Distribute Ifc5D on PyPI

---
+++ src/ifc5d/ifc5d/__init__.py
@@ -0,0 +1,19 @@
+# Ifc5D - IFC costing utility
+# Copyright (C) 2020-2024 Dion Moult <dion@thinkmoult.com>
+#
+# This file is part of Ifc5D.
+#
+# Ifc5D is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ifc5D is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Ifc5D.  If not, see <http://www.gnu.org/licenses/>.
+
+__version__ = version = "0.0.0"
--- src/ifcopenshell-python/docs/ifc5d.rst
+++ src/ifcopenshell-python/docs/ifc5d.rst
@@ -3,3 +3,10 @@ Ifc5D
 
 Ifc5D is a collection of utilities of manipulating cost-related data to and
 from formats, reports, and optimisation engines.
+
+PyPI
+----
+
+.. code-block::
+
+    pip install ifc5d
