b9269b5a115c5eb2972324d1897fac02b1d98885
Dion Moult <dion@thinkmoult.com>
2024-06-26 13:02:18 +1000

    Distribute Ifc4D on PyPI

---
+++ src/ifc4d/ifc4d/__init__.py
@@ -0,0 +1,19 @@
+# Ifc4D - IFC scheduling utility
+# Copyright (C) 2020-2024 Dion Moult <dion@thinkmoult.com>
+#
+# This file is part of Ifc4D.
+#
+# Ifc4D is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ifc4D is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Ifc4D.  If not, see <http://www.gnu.org/licenses/>.
+
+__version__ = version = "0.0.0"
