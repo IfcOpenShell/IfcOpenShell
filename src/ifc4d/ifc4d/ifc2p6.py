89fef251843463f25d12a8e11a62d1debc1dba0f
Dion Moult <dion@thinkmoult.com>
2024-06-26 13:02:18 +1000

    Distribute Ifc4D on PyPI

--- src/ifc4d/ifc4d/ifc2p6.py
+++ src/ifc4d/ifc4d/ifc2p6.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Ifc4D.  If not, see <http://www.gnu.org/licenses/>.
 
-import math
 import uuid
 import datetime
 import ifcopenshell
