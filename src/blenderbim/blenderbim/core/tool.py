1cdd4c27fc539c8fd1299ca67c7323d5ddfc030f
Andrej730 <azhilenkov@gmail.com>
2024-07-05 17:02:07 +0500

    fix geometry core tests after fccf56d5f

--- src/blenderbim/blenderbim/core/tool.py
+++ src/blenderbim/blenderbim/core/tool.py
@@ -412,6 +412,7 @@ class Geometry:
     def should_use_presentation_style_assignment(cls): pass
     def unresolve_type_representation(cls, representation, element): pass
     def delete_opening_object_placement(cls, opening): pass
+    def switch_from_representation(cls, obj, representation): pass
 
 
 @interface
