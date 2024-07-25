64d3abb7f799c2f9ee85c0155ef8986e12806076
Andrej730 <azhilenkov@gmail.com>
2024-07-25 16:52:55 +0500

    support assigning bsdd classifications in ifc4x3 too

--- src/blenderbim/blenderbim/bim/module/classification/operator.py
+++ src/blenderbim/blenderbim/bim/module/classification/operator.py
@@ -117,20 +117,17 @@ class AddClassificationFromBSDD(bpy.types.Operator, tool.Ifc.Operator):
     bl_options = {"REGISTER", "UNDO"}
 
     def _execute(self, context):
-        is_ifc2x3 = tool.Ifc.get_schema() == "IFC2X3"
-
         props = context.scene.BIMBSDDProperties
         domain = [d for d in props.domains if d.name == props.active_domain][0]
         for element in tool.Ifc.get().by_type("IfcClassification"):
-            if element.Name == props.active_domain or (not is_ifc2x3 and element.Location == domain.uri):
+            if element.Name == props.active_domain or (tool.Classification.get_location(element) == domain.uri):
                 return
         classification = ifcopenshell.api.run(
             "classification.add_classification", tool.Ifc.get(), classification=props.active_domain
         )
         classification.Source = domain.organization_name_owner
         classification.Edition = domain.version
-        if not is_ifc2x3:
-            classification.Location = domain.uri
+        tool.Classification.set_location(classification, domain.uri)
 
 
 class EnableAddingManualClassification(bpy.types.Operator):
@@ -400,8 +397,6 @@ class AddClassificationReferenceFromBSDD(bpy.types.Operator, tool.Ifc.Operator):
     obj_type: bpy.props.StringProperty()
 
     def _execute(self, context):
-        is_ifc2x3 = tool.Ifc.get_schema() == "IFC2X3"
-
         if self.obj_type == "Object":
             if context.selected_objects:
                 objects = [o.name for o in context.selected_objects]
@@ -416,9 +411,8 @@ class AddClassificationReferenceFromBSDD(bpy.types.Operator, tool.Ifc.Operator):
 
         classification = None
         for element in tool.Ifc.get().by_type("IfcClassification"):
-            if (
-                element.Name == bsdd_classification.domain_name
-                or (not is_ifc2x3 and element.Location == bsdd_classification.domain_namespace_uri)
+            if element.Name == bsdd_classification.domain_name or (
+                tool.Classification.get_location(element) == bsdd_classification.domain_namespace_uri
             ):
                 classification = element
                 break
@@ -427,8 +421,7 @@ class AddClassificationReferenceFromBSDD(bpy.types.Operator, tool.Ifc.Operator):
             classification = ifcopenshell.api.run(
                 "classification.add_classification", tool.Ifc.get(), classification=bsdd_classification.domain_name
             )
-            if not is_ifc2x3:
-                classification.Location = bsdd_classification.domain_namespace_uri
+            tool.Classification.set_location(classification, bsdd_classification.domain_namespace_uri)
 
         for obj in objects:
             ifc_definition_id = tool.Blender.get_obj_ifc_definition_id(obj, self.obj_type, context)
