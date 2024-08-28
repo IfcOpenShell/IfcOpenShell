6784ecc879783f776b80f22ce7723e8421b2890a
Bruno Perdigão <bruno@brunopo.com>
2024-08-28 19:02:06 -0300

    Fix 5d2f1d58e

--- src/bonsai/bonsai/bim/module/model/decorator.py
+++ src/bonsai/bonsai/bim/module/model/decorator.py
@@ -708,12 +708,6 @@ class PolylineDecorator:
         if ref_point:
             self.draw_batch("POINTS", ref_point, (1.0, 0.6, 0.0, 1.0))
 
-        # Draw polyline with selected points
-        self.line_shader.uniform_float("lineWidth", 2.0)
-        self.draw_batch("POINTS", polyline_points, decorator_color_selected)
-        if len(polyline_points) > 1:
-            self.draw_batch("LINES", polyline_points, decorator_color_selected, polyline_edges)
-
         # Line between last polyline point and mouse
         edges = [[0, 1]]
         if polyline_points:
@@ -721,3 +715,10 @@ class PolylineDecorator:
                 self.draw_batch("LINES", [polyline_points[-1]] + projection_point, decorator_color_unselected, edges)
             else:
                 self.draw_batch("LINES", [polyline_points[-1]] + mouse_point, decorator_color_unselected, edges)
+
+        # Draw polyline with selected points
+        self.line_shader.uniform_float("lineWidth", 2.0)
+        self.draw_batch("POINTS", polyline_points, decorator_color_selected)
+        if len(polyline_points) > 1:
+            self.draw_batch("LINES", polyline_points, decorator_color_selected, polyline_edges)
+
