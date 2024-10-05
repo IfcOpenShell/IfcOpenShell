5d3ae9a498da4354ec958baebf41ba3851c99209
Bruno Perdigão <bruno@brunopo.com>
2024-10-05 15:25:19 -0300

    Measure tool - allow for pan, orbit and zoom while tool is active.

--- src/bonsai/bonsai/bim/module/project/operator.py
+++ src/bonsai/bonsai/bim/module/project/operator.py
@@ -2398,7 +2398,12 @@ class MeasureTool(bpy.types.Operator, PolylineOperator):
         """
 
     def modal(self, context, event):
-        super().modal(context, event)
+        PolylineDecorator.update(event, self.tool_state, self.input_ui, self.snapping_points[0])
+        tool.Blender.update_viewport()
+
+        if event.type in {"MIDDLEMOUSE", "WHEELUPMOUSE", "WHEELDOWNMOUSE"}:
+            self.handle_mouse_move(context, event)
+            return {"PASS_THROUGH"}
 
         self.handle_instructions(context)
 
