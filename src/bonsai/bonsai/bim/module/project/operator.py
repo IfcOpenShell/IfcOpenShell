ddd17d519ebe2511bda408476d998f9b4f836920
Bruno Perdigão <bruno@brunopo.com>
2024-10-09 10:19:51 -0300

    Fix error that was preventing 3d panning while polyline tool is active. This error was introduced by 48784ba

--- src/bonsai/bonsai/bim/module/project/operator.py
+++ src/bonsai/bonsai/bim/module/project/operator.py
@@ -2401,7 +2401,9 @@ class MeasureTool(bpy.types.Operator, PolylineOperator):
         PolylineDecorator.update(event, self.tool_state, self.input_ui, self.snapping_points[0])
         tool.Blender.update_viewport()
 
-        if not event.shift and event.type in {"MIDDLEMOUSE", "WHEELUPMOUSE", "WHEELDOWNMOUSE"}:
+        self.handle_lock_axis(context, event)
+
+        if event.type in {"MIDDLEMOUSE", "WHEELUPMOUSE", "WHEELDOWNMOUSE"}:
             self.handle_mouse_move(context, event)
             return {"PASS_THROUGH"}
 
@@ -2411,7 +2413,6 @@ class MeasureTool(bpy.types.Operator, PolylineOperator):
 
         self.choose_axis(event, z=True)
 
-        self.handle_lock_axis(context, event)
 
         self.choose_plane(event)
 
