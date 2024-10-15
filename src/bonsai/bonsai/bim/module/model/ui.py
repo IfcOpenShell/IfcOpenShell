ab4d3a9f77f9091ae938785a0aecd93f0531bad6
tim <tjrhyder@gmail.com>
2024-10-14 23:47:30 -0400

    Type Manager Thumbnails
    toggle enable row to highlight active selection

--- src/bonsai/bonsai/bim/module/model/ui.py
+++ src/bonsai/bonsai/bim/module/model/ui.py
@@ -108,13 +108,14 @@ class LaunchTypeManager(bpy.types.Operator):
     def draw(self, context):
         props = context.scene.BIMModelProperties
         row = self.layout.row(align=True)
+        
+        active_relating_type_id = int(context.scene.BIMModelProperties.relating_type_id) if context.scene.BIMModelProperties.relating_type_id else None
+        
         if AuthoringData.data["total_types"] > 1:
             text = f"{AuthoringData.data['total_types']} {AuthoringData.data['ifc_element_type']}s"
         else:
             text = f"{AuthoringData.data['total_types']} {AuthoringData.data['ifc_element_type']}"
         row.label(text=text, icon="FILE_VOLUME")
-        # prop_with_search(row, props, "ifc_class", text="", should_click_ok_to_validate=True)
-        
         row.menu("BIM_MT_type_manager_menu", text="", icon="PREFERENCES")
         row = self.layout.row(align=True)
         row.operator("bim.launch_add_element", text=f"Create New {AuthoringData.data['ifc_element_type']}", icon="ADD")
@@ -122,9 +123,6 @@ class LaunchTypeManager(bpy.types.Operator):
         columns = self.layout.column_flow(columns=3)
         row = columns.row(align=True)
         row.alignment = "CENTER"
-        ### In case you want something here in the future
-
-        ###
 
         row = columns.row(align=True)
         row.alignment = "RIGHT"
@@ -144,10 +142,15 @@ class LaunchTypeManager(bpy.types.Operator):
             box = outer_col.box()
 
             row = box.row(align=True)
-            op = row.operator("bim.set_active_type", text=relating_type["name"], icon="BLANK1", emboss=False)
+            split = row.split(factor=0.33)
+            col1 = split.column()
+            col1.label(text="", icon="BLANK1")
+            col2 = split.column()
+            op = col2.operator("bim.set_active_type", text=relating_type["name"], emboss=False)
             op.relating_type = relating_type["id"]
-
-            op = row.operator("bim.launch_type_menu", icon="DOWNARROW_HLT", text="", emboss=False)
+            col2.enabled = (relating_type["id"] == active_relating_type_id)
+            col3 = split.column()
+            op = col3.operator("bim.launch_type_menu", icon="DOWNARROW_HLT", text="", emboss=False)
             op.relating_type_id = relating_type["id"]
 
             row = box.row()
@@ -176,12 +179,6 @@ class LaunchTypeManager(bpy.types.Operator):
             op = row.operator("bim.set_active_type", text=relating_type["predefined_type"], emboss=False)
             op.relating_type = relating_type["id"]
             row.enabled=(relating_type["id"] == active_relating_type_id)
-
-            row = box.row()
-            row.alignment = "CENTER"
-            op = row.operator("bim.set_active_type", text=relating_type["predefined_type"], emboss=False)
-            op.relating_type = relating_type["id"]
-
 
 class BIM_PT_authoring(Panel):
     bl_label = "Architectural"
