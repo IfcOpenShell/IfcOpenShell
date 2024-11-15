c133f11b82d5bf612a961085228adc6d2a4f59f3
Bruno Perdigão <bruno@brunopo.com>
2024-11-15 10:03:57 -0300

    Add aggregate constraints to import_ifc.py

--- src/bonsai/bonsai/bim/import_ifc.py
+++ src/bonsai/bonsai/bim/import_ifc.py
@@ -254,6 +254,7 @@ class IfcImporter:
         self.place_objects_in_collections()
         self.profile_code("Place objects in collections")
         self.setup_arrays()
+        self.setup_aggregates()
         self.profile_code("Setup arrays")
         self.lock_scales()
         self.profile_code("Lock objects scales")
@@ -1185,6 +1186,16 @@ class IfcImporter:
                     for i in range(len(data)):
                         tool.Blender.Modifier.Array.set_children_lock_state(element, i, True)
                         tool.Blender.Modifier.Array.constrain_children_to_parent(element)
+
+    def setup_aggregates(self):
+        elements = set(self.file.by_type("IfcElement"))
+        for element in elements:
+            parts = ifcopenshell.util.element.get_parts(element)
+            if parts:
+                relating_obj = tool.Ifc.get_object(element)
+                if relating_obj:
+                    tool.Aggregate.constrain_parts_to_aggregate(relating_obj)
+
 
     def lock_scales(self) -> None:
         elements = set(self.file.by_type("IfcProduct"))
