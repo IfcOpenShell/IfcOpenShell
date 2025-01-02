15e1efea7f5c2553aa982cf0b9c367b063b8ee78
Gorgious56 <nathan.hild@gmail.com>
2025-01-02 10:27:52 +0100

    Fix #5849 : Use Keymap item instead of modal operator for override escape in 3D viewport - Enables the vanilla Blender autosave feature back, since a running modal operator deactivates it - ESC is not used by any other vanilla keymap in the 3D viewport (may be conflict with other addons) - Hitting ESC over another type of editor (eg enum field in the properties editor) will not trigger disabling ifc item mode - You can now hit ESC in IFC edit mode to cancel editing the geometry and go back to ifc object mode

--- src/bonsai/bonsai/bim/module/geometry/operator.py
+++ src/bonsai/bonsai/bim/module/geometry/operator.py
@@ -1848,17 +1848,12 @@ class OverrideEscape(bpy.types.Operator):
     bl_label = "Override Escape"
 
     def execute(self, context):
-        self.report({"INFO"}, "Operator executed")
-        return {"FINISHED"}
-
-    def modal(self, context, event):
-        if event.type == "ESC" and context.scene.BIMGeometryProperties.mode == "ITEM":
+        if context.scene.BIMGeometryProperties.mode == "ITEM":
             tool.Geometry.disable_item_mode()
-        return {"PASS_THROUGH"}
-
-    def invoke(self, context, event):
-        context.window_manager.modal_handler_add(self)
-        return {"RUNNING_MODAL"}
+        if context.scene.BIMGeometryProperties.mode == "EDIT":
+            bpy.ops.object.editmode_toggle()
+            tool.Geometry.disable_item_mode()
+        return {"FINISHED"}
 
 
 class OverrideModeSetEdit(bpy.types.Operator, tool.Ifc.Operator):
