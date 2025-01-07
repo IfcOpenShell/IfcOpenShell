e1d92511b47322ece78630c6dc875eab096b36b5
Thomas Krijnen <t.krijnen@gmail.com>
2025-01-07 20:46:31 +0100

    wasm wheel fixes

--- pyodide/setup.py
+++ pyodide/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='ifcopenshell-python',
+setup(name='ifcopenshell',
       version='0.8.0',
       description='IfcOpenShell is an open source (LGPL) software library for working with the Industry Foundation Classes (IFC) file format.',
       author='Thomas Krijnen',
       author_email='thomas@aecgeeks.com',
       url='http://ifcopenshell.org',
       packages=find_packages(),
-      package_data={'': ['*.so']},
+      package_data={'': ['*.so', '*.json']},
 )
