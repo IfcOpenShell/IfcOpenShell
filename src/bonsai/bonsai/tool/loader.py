IfcOpenShell/IfcOpenShell

commit bc3a1ee4346e2dc19ff0583bcb76b4f4c1a073b2

Fix slowness when rendering dense meshes with the opening decorator
It uses a custom boolean attribute on the mesh to decide whether an edge should be displayed or not. I think later on it can be used to get rid of the dictionary accessors which make the blend file size skyrocket.

src/bonsai/bonsai/tool/loader.py
@@ -937,7 +937,8 @@ class Loader(bonsai.core.tool.Loader):
             # ios_edges holds true edges that aren't triangulated.
             #
             # we do `.tolist()` because Blender can't assign `np.int32` to it's custom attributes
-            mesh["ios_edges"] = list(set(tuple(e) for e in ifcopenshell.util.shape.get_edges(geometry).tolist()))
+            ios_edges = list(set(tuple(e) for e in ifcopenshell.util.shape.get_edges(geometry).tolist()))
+            mesh["ios_edges"] = ios_edges
             mesh["ios_item_ids"] = ifcopenshell.util.shape.get_faces_representation_item_ids(geometry).tolist()
 
             mesh.vertices.add(num_vertices)
@@ -979,6 +980,20 @@ class Loader(bonsai.core.tool.Loader):
                 # For now, not necessary to load maps in Item mode
                 if rep.is_a("IfcShapeRepresentation"):
                     tool.Loader.load_indexed_colour_map(rep, mesh)
+
+            ios_edges_indices = [(e[0], e[1]) for e in ios_edges]
+            attribute_ios_edges = mesh.attributes.get("ios_edges")
+            if not attribute_ios_edges:
+                attribute_ios_edges = mesh.attributes.new("ios_edges", domain="EDGE", type="BOOLEAN")
+
+            attribute_ios_edges.data.foreach_set(
+                "value",
+                [
+                    (e.vertices[0], e.vertices[1]) in ios_edges_indices
+                    or (e.vertices[1], e.vertices[0]) in ios_edges_indices
+                    for e in mesh.edges
+                ],
+            )
         else:
             e = geometry.edges
             v = verts

src/bonsai/bonsai/bim/module/model/opening.py
@@ -1184,12 +1184,14 @@ class DecorationsHandler:
                 bm.from_mesh(obj.data)
 
                 verts = [tuple(obj.matrix_world @ v.co) for v in bm.verts]
-                edges = [tuple([v.index for v in e.verts]) for e in bm.edges]
-                ios_edges = [(edge[0], edge[1]) for edge in obj.data.get("ios_edges", [])]
-                if ios_edges:
-                    edges = [e for e in edges if (e[0], e[1]) in ios_edges or (e[1], e[0]) in ios_edges]
+                if ios_edges_attribute := obj.data.attributes.get("ios_edges"):
+                    edges = [e for i, e in enumerate(bm.edges) if ios_edges_attribute.data[i].value]
+                else:
+                    edges = bm.edges
+                edges_indices = [tuple([v.index for v in e.verts]) for e in edges]
+
                 color = selected_elements_color if obj in context.selected_objects else special_elements_color
-                self.draw_batch("LINES", verts, color, edges)
+                self.draw_batch("LINES", verts, color, edges_indices)
 
             obj.data.calc_loop_triangles()
             tris = [tuple(t.vertices) for t in obj.data.loop_triangles]

commit 74e00c6b369d4504b48c0a18ce5de095bc356a16

Fix 3x slower major speed regression introduced in bc3a1ee4346e2dc19ff0583bcb76b4f4c1a073b2
The commit that introduced the problem was trying to optimise rendering
dense meshes with the opening decorator. However, it introduced more
calculation in the initial project load and stored more data on every
object. This resulted in models loading 3x slower for me which is pretty
major. The original problem needs further investigation.

src/bonsai/bonsai/tool/loader.py
@@ -936,8 +936,7 @@ class Loader(bonsai.core.tool.Loader):
             # ios_edges holds true edges that aren't triangulated.
             #
             # we do `.tolist()` because Blender can't assign `np.int32` to it's custom attributes
-            ios_edges = list(set(tuple(e) for e in ifcopenshell.util.shape.get_edges(geometry).tolist()))
-            mesh["ios_edges"] = ios_edges
+            mesh["ios_edges"] = list(set(tuple(e) for e in ifcopenshell.util.shape.get_edges(geometry).tolist()))
             ios_item_ids = ifcopenshell.util.shape.get_faces_representation_item_ids(geometry).tolist()
             mesh["ios_item_ids"] = ios_item_ids
 
@@ -981,11 +980,6 @@ class Loader(bonsai.core.tool.Loader):
                 if rep.is_a("IfcShapeRepresentation"):
                     tool.Loader.load_indexed_colour_map(rep, mesh)
 
-            ios_edges_values = [
-                (e.vertices[0], e.vertices[1]) in ios_edges or (e.vertices[1], e.vertices[0]) in ios_edges
-                for e in mesh.edges
-            ]
-            tool.Blender.Attribute.fill_attribute(mesh, "ios_edges", "EDGE", "BOOLEAN", ios_edges_values)
             tool.Blender.Attribute.fill_attribute(mesh, "ios_item_ids", "FACE", "INT", ios_item_ids)
             tool.Blender.Attribute.fill_attribute(mesh, "ios_material_ids", "FACE", "INT", geometry.material_ids)
         else: