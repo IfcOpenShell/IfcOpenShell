6aba164b039476dabfbef87282cdb0046a6e239f
falken10 <falken10vdl@gmail.com>
2025-03-13 14:58:59 +0100

    add print statement to ifcopenshell_wrapper.py

--- src/bonsai/bonsai/tool/system.py
+++ src/bonsai/bonsai/tool/system.py
@@ -242,7 +242,7 @@ class System(bonsai.core.tool.System):
 
     @classmethod
     def get_decoration_data(cls) -> dict[str, Any]:
-        print("get_decoration_data")
+        print("get_decoration_data in system.py")
         all_vertices = []
         preview_edges = []
         special_vertices = []
