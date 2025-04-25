32ba23435a6a12628d30a8aaa1a7c35a8bfda17d
Andrej730 <azhilenkov@gmail.com>
2025-04-25 14:56:33 +0500

    core.duplicate_drawing - remove debug print statements and unused code

--- src/bonsai/bonsai/core/drawing.py
+++ src/bonsai/bonsai/core/drawing.py
@@ -318,11 +318,8 @@ def duplicate_drawing(
         for new_elements in old_to_new.values():
             # Remove the Blender object, since we haven't actually activated the duplicated drawing
             for new_element in new_elements:
-                print("new element", new_element)
-                print("gonna remove", ifc.get_object(new_element))
                 blender.remove_object(ifc.get_object(new_element))
             new_annotations.extend(new_elements)
-        new_annotation = set(new_annotations)
         ifc.run("group.unassign_group", group=group, products=new_annotations)
         ifc.run("group.assign_group", group=new_group, products=new_annotations)
 
