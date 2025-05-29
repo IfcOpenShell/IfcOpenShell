639aae29195e6ea96ae8e124083b29b3325e42ad
falken10 <falken10vdl@gmail.com>
2025-05-30 01:39:41 +0200

    dms2dd and dd2dms functions to use microseconds and handle negative degrees

--- src/ifcopenshell-python/ifcopenshell/util/geolocation.py
+++ src/ifcopenshell-python/ifcopenshell/util/geolocation.py
@@ -40,38 +40,44 @@ class HelmertTransformation(NamedTuple):
 
 
 def dms2dd(degrees: int, minutes: int, seconds: int, ms: int = 0) -> float:
-    """Convert degrees, minutes, and (milli)seconds to decimal degrees
+    """Convert degrees, minutes, and (micro)seconds to decimal degrees
 
     :param degrees: The degrees component
     :param minutes: The minutes component
     :param seconds: The seconds component
-    :param ms: The milliseconds component
+    :param ms: The microseconds component
     :return: The angle in decimal degrees.
     """
-    dd = float(degrees) + float(minutes) / 60.0 + float(seconds) / (3600.0) + float(ms / 3600000000.0)
-    return dd
+    sign = -1 if degrees < 0 else 1
+    dd = abs(float(degrees)) + float(minutes) / 60.0 + float(seconds) / 3600.0 + float(ms) / 3600000000.0
+    return sign * dd
 
 
 def dd2dms(dd: float, use_ms: bool = False) -> Union[tuple[float, float, float, float], tuple[float, float, float]]:
-    """Convert decimal degrees to degrees, minutes, and (milli)seconds format
+    """Convert decimal degrees to degrees, minutes, and (micro)seconds format
 
     :param dd: The decimal degrees
-    :param use_ms: True if to include milliseconds and false otherwise. Defaults to false.
-    :return: The angle in a tuple of either 3 or 4 values, being degrees,
-        minutes, seconds, and optionally milliseconds.
+    :param use_ms: True if to include microseconds and false otherwise. Defaults to false.
+    :return: The angle in a tuple of either 3 or 4 values,
+        4 values: integer number of degrees, integer number of minutes, integer number of seconds and integer number of microseconds
+        3 values: integer number of degrees, integer number of minutes, and a float number for seconds
     """
-    dd = float(dd)
-    sign = 1 if dd >= 0 else -1
+    sign = -1 if dd < 0 else 1
     dd = abs(dd)
+
+    degrees = int(dd)
+    minutes_float = (dd - degrees) * 60
+    minutes = int(minutes_float)
+    seconds_float = (minutes_float - minutes) * 60
+
     if use_ms:
-        seconds, ms = divmod(dd * 60 * 60 * 1000000, 1000000)
-    minutes, seconds = divmod(dd * 60 * 60, 60)
-    degrees, minutes = divmod(minutes, 60)
-    if dd < 0:
-        degrees = -degrees
-    if use_ms:
-        return (int(degrees) * sign, int(minutes) * sign, int(seconds) * sign, int(ms) * sign)
-    return (int(degrees) * sign, int(minutes) * sign, int(seconds) * sign)
+        seconds = int(seconds_float)
+        microseconds = int(round((seconds_float - seconds) * 1_000_000))
+        result = (sign * degrees, minutes, seconds, microseconds)
+    else:
+        result = (sign * degrees, minutes, seconds_float)
+
+    return result
 
 
 def xyz2enh(
