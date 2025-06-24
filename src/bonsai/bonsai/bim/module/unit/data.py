fe81dc7f2145811172edc95883181956c790001b
Andrej730 <azhilenkov@gmail.com>
2025-06-24 18:59:53 +0500

    Units UI - match icons in enum with icons in ui list
    To build a visual association
    Example (previously enum had no icons)
    https://files.catbox.moe/0ks3jj.png
    https://files.catbox.moe/y1yrv5.png

--- src/bonsai/bonsai/bim/module/unit/data.py
+++ src/bonsai/bonsai/bim/module/unit/data.py
@@ -61,23 +61,21 @@ class UnitsData:
             return ifcopenshell.util.unit.get_full_unit_name(unit)
 
     @classmethod
-    def unit_classes(cls):
+    def unit_classes(cls) -> tool.Blender.BLENDER_ENUM_ITEMS:
         assert (entity := tool.Ifc.schema().declaration_by_name("IfcNamedUnit").as_entity())
         declarations = ifcopenshell.util.schema.get_subtypes(entity)
         version = tool.Ifc.get_schema()
+        classes = sorted([d.name() for d in declarations]) + ["IfcDerivedUnit", "IfcMonetaryUnit"]
         results = [
-            (c, c, get_entity_doc(version, c).get("description", "")) for c in sorted([d.name() for d in declarations])
+            (
+                c,
+                c,
+                get_entity_doc(version, c).get("description", ""),
+                tool.Unit.get_icon_for_unit_class(c),
+                i,
+            )
+            for i, c in enumerate(classes)
         ]
-        results.extend(
-            [
-                ("IfcDerivedUnit", "IfcDerivedUnit", get_entity_doc(version, "IfcDerivedUnit").get("description", "")),
-                (
-                    "IfcMonetaryUnit",
-                    "IfcMonetaryUnit",
-                    get_entity_doc(version, "IfcMonetaryUnit").get("description", ""),
-                ),
-            ]
-        )
         return results
 
     @classmethod
