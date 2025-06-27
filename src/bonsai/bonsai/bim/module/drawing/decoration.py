7280c6102cb6859f8c94350b2b5d2f3e28528c9c
Ryan Schultz <ryan@openingdesign.com>
2025-06-27 08:13:52 -0500

    ran Black

--- src/bonsai/bonsai/bim/module/drawing/decoration.py
+++ src/bonsai/bonsai/bim/module/drawing/decoration.py
@@ -1206,17 +1206,17 @@ class PlanLevelDecorator(BaseDecorator):
             def get_text():
                 abs_z = verts[0].z
                 z = abs_z
-                
+
                 if element:
                     spatial_container = ifcopenshell.util.element.get_container(element)
-                    
+
                     if spatial_container:
                         container_obj = tool.Ifc.get_object(spatial_container)
-                        
+
                         if container_obj:
                             spatial_container_z = container_obj.matrix_world.translation.z
                             z = abs_z - spatial_container_z
-                
+
                 rl = self.format_value(context, z)
                 text = "{}{}".format("" if z < 0 else "+", rl)
                 return text
