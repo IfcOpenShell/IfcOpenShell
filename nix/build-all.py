5acacaa9d8c0365b16fcfb841e8a02956902a22c
Andrej730 <azhilenkov@gmail.com>
2025-07-30 11:46:12 +0500

    build-all - fix verbose logging
    verbose logging was setting log level only for `logger`, never setting for the `logger`'s handler, so `-v` option, it seems, never worked. For simplicity now handler will just inherit log level from the `logger`.

--- nix/build-all.py
+++ nix/build-all.py
@@ -111,7 +111,6 @@ except:
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
 logger.addHandler(ch)
 
 PROJECT_NAME = "IfcOpenShell"
