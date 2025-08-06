e52e68602df3c41f4363591434a9f4d1c62cbe60
Robin Quint <rob2309@hotmail.de>
2025-08-06 20:37:31 +0200

    Added ChangeSwingDirection operator for parametric doors

--- src/bonsai/bonsai/bim/module/model/__init__.py
+++ src/bonsai/bonsai/bim/module/model/__init__.py
@@ -84,6 +84,7 @@ classes = (
     product.MirrorElements,
     product.TrueMirrorElements,
     product.SetActiveType,
+    product.ChangeSwingDirection,
     workspace.Hotkey,
     workspace.BIM_MT_add_representation_item,
     wall.AddPerpendicularWall,
