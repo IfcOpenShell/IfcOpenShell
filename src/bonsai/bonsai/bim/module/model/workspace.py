281f50b9cec1f9ebe8ff173245a96ff695b3015c
Robin Quint <rob2309@hotmail.de>
2025-08-07 17:09:38 +0200

    changed hotkey to ctrl + f, added icon

--- src/bonsai/bonsai/bim/module/model/workspace.py
+++ src/bonsai/bonsai/bim/module/model/workspace.py
@@ -75,7 +75,6 @@ class BimTool(WorkSpaceTool):
         ("bim.hotkey", {"type": "E", "value": "PRESS", "ctrl": True}, {"properties": [("hotkey", "C_E")]}),
         ("bim.hotkey", {"type": "F", "value": "PRESS", "shift": True}, {"properties": [("hotkey", "S_F")]}),
         ("bim.hotkey", {"type": "F", "value": "PRESS", "ctrl": True}, {"properties": [("hotkey", "C_F")]}),
-        ("bim.hotkey", {"type": "F", "value": "PRESS", "shift": True, "ctrl": True}, {"properties": [("hotkey", "S_C_F")]}),
         ("bim.hotkey", {"type": "G", "value": "PRESS", "shift": True}, {"properties": [("hotkey", "S_G")]}),
         ("bim.hotkey", {"type": "K", "value": "PRESS", "shift": True}, {"properties": [("hotkey", "S_K")]}),
         ("bim.hotkey", {"type": "M", "value": "PRESS", "shift": True}, {"properties": [("hotkey", "S_M")]}),
@@ -1094,6 +1093,8 @@ class EditObjectUI:
     def draw_flip(cls, ui_context, layout) -> None:
         row = cls.layout.row(align=True) if ui_context != "TOOL_HEADER" else layout
         add_layout_hotkey_operator(row, "Flip", "S_F", bpy.ops.bim.flip_object.__doc__, ui_context)
+        row = cls.layout.row(align=True) if ui_context != "TOOL_HEADER" else layout
+        add_layout_hotkey_operator(row, "Mirror Geometry", "C_F", bpy.ops.bim.mirror_geometry.__doc__, ui_context)
 
     @classmethod
     def draw_mirror_geometry(cls, ui_context, layout) -> None:
@@ -1300,11 +1301,6 @@ class Hotkey(bpy.types.Operator, tool.Ifc.Operator):
             bpy.ops.bim.flip_object(flip_local_axes="XZ")
 
     def hotkey_C_F(self):
-        if not bpy.context.selected_objects:
-            return
-        bpy.ops.bim.mirror_geometry(keep_original=getattr(self, "shift", False))
-
-    def hotkey_S_C_F(self):
         if not bpy.context.selected_objects:
             return
         bpy.ops.bim.mirror_geometry()
