70183c25c74959c1ed0bc1df6ff74e29bad2c5e2
Massimo Fabbro <maxfb87@yahoo.it>
2025-08-11 16:50:56 +0200

    See #6570. Now the rate cost item pop up shows also the cost schedule

--- src/bonsai/bonsai/bim/module/cost/ui.py
+++ src/bonsai/bonsai/bim/module/cost/ui.py
@@ -734,6 +734,9 @@ class BIM_UL_cost_items_trait:
         if cost_item["AssignedCostRate"] is not None:
             op = row.operator("bim.show_assigned_cost_rate", text="", emboss=False, icon="ZOOM_IN")
             identification = cost_item["AssignedCostRate"].Identification
+            op.parent_cost_schedule_name = CostSchedulesData.data["cost_items"][cost_item["AssignedCostRate"].id()][
+                "ParentCostSchedule"
+            ].Name
             op.assigned_rate_identification = identification if identification is not None else "XXX"
             op.assigned_rate_name = cost_item["AssignedCostRate"].Name or ""
             op.assigned_rate_description = cost_item["AssignedCostRate"].Description or ""
