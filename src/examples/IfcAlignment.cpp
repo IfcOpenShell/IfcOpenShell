95a42ea99c9de0869fe6b2253c1ca624515148a5
Thomas Krijnen <t.krijnen@gmail.com>
2025-08-26 10:18:13 +0200

    Fixes to header writing

--- src/examples/IfcAlignment.cpp
+++ src/examples/IfcAlignment.cpp
@@ -46,7 +46,7 @@ double to_radian(double deg) { return PI * deg / 180; }
 Schema::IfcProject* setup_project(IfcHierarchyHelper<Schema>& file) {
     std::vector<std::string> file_description;
     file_description.push_back("ViewDefinition[Alignment-basedReferenceView]");
-    file.header().file_description().description(file_description);
+    file.header().file_description()->setdescription(file_description);
 
     auto project = file.addProject();
     project->setName(std::string("FHWA Bridge Geometry Manual Example Alignment"));
