21137de8a0fa6633a885b9fd0ea200fc1322bcf8
Andrej730 <azhilenkov@gmail.com>
2025-09-05 18:25:39 +0500

    Fix error reapplying selection during assign_class #7082
    It appeared in case if user had IfcTypeProduct collection disabled.

--- src/bonsai/bonsai/bim/module/root/operator.py
+++ src/bonsai/bonsai/bim/module/root/operator.py
@@ -385,13 +385,8 @@ class AssignClass(bpy.types.Operator, tool.Ifc.Operator):
         # TODO: reload representation might lead to the object being replaced by object of the other type.
         # We probably should track it somehow and keep the original selection.
 
-        # Validation selection.
-        new_selected_objects = list(filter(tool.Blender.is_valid_data_block, current_selection[2]))
-        active_object = current_selection[1]
-        if active_object and not tool.Blender.is_valid_data_block(active_object):
-            active_object = None
-        current_selection = (current_selection[0], active_object, new_selected_objects)
-
+        # Validate selection and reapply it.
+        current_selection = tool.Blender.validate_object_selection(*current_selection)
         tool.Blender.set_objects_selection(*current_selection)
 
 
