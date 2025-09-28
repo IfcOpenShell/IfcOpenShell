d959434b1258a0618fa2df0c74407e7ae740454c
Dion Moult <dion@thinkmoult.com>
2025-09-28 18:15:13 +1000

    See #7131. Rollback to prevent instant segfaults on Linux.

--- src/ifcopenshell-python/ifcopenshell/__init__.py
+++ src/ifcopenshell-python/ifcopenshell/__init__.py
@@ -185,7 +185,10 @@ def open(
         return sqlite(path)
     if should_stream:
         return stream(path)
-    f = ifcopenshell_wrapper.open(str(path.absolute()), readonly)
+    if readonly: # Temporary conditional see #7131. Remove once newer builds don't segfault on Linux.
+        f = ifcopenshell_wrapper.open(str(path.absolute()), readonly)
+    else:
+        f = ifcopenshell_wrapper.open(str(path.absolute()))
     return file(f)
 
 
