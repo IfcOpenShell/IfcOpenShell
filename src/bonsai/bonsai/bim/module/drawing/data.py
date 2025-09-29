b58021d7609ee479a21b7ab075d78f89e3c27102
Robin Quint <rob2309@hotmail.de>
2025-09-29 15:40:35 +0200

    Plan view fixes (#7022)

--- src/bonsai/bonsai/bim/module/drawing/data.py
+++ src/bonsai/bonsai/bim/module/drawing/data.py
@@ -241,6 +241,7 @@ class DecoratorData:
         cls.is_loaded = True
         cls.cut_cache = {}
         cls.layerset_cache = {}
+        cls.fill_cache = {}
 
         text = {}
         dimension = {}
