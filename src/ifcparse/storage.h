e15f222a19ffea62e12d19b8f2a51f7f7925230c
Thomas Krijnen <t.krijnen@gmail.com>
2025-10-08 14:17:47 +0200

    Address segfault #7215 #7185

--- src/ifcparse/storage.h
+++ src/ifcparse/storage.h
@@ -189,7 +189,10 @@ namespace IfcParse {
 
     namespace impl {
         struct IFC_PARSE_API in_memory_file_storage {
-            std::vector<IfcUtil::IfcBaseClass*> read_simple_type_instances;
+            std::vector<std::unique_ptr<IfcUtil::IfcBaseClass>> read_simple_type_instances;
+            std::vector<std::unique_ptr<IfcUtil::IfcBaseClass>> steal_instances() {
+                return std::move(read_simple_type_instances);
+            }
 
             IfcParse::IfcSpfLexer* tokens;
             // IfcParse::IfcSpfStream* stream;
@@ -214,13 +217,9 @@ namespace IfcParse {
             typedef entity_instance_by_name_t::iterator iterator;
 
             in_memory_file_storage(IfcParse::IfcFile* f = nullptr) : tokens(nullptr), file(f), schema(nullptr) {}
-            ~in_memory_file_storage() {
-                for (auto* inst : read_simple_type_instances) {
-                    delete inst;
-                }
-            }
             in_memory_file_storage(const in_memory_file_storage&) = delete;
             in_memory_file_storage(const in_memory_file_storage&&) = delete;
+
 
             class type_iterator : public entities_by_type_t::const_iterator {
             public:
