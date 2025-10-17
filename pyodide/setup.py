b1fb5419121d6ec4a7455ff77ec9957be6e3622b
Andrej730 <azhilenkov@gmail.com>
2025-10-17 16:15:06 +0500

    pyodide setup.py - ensure we have correct `pyodide_2025_0_wasm32` suffix
    Adding extension, but avoiding adding sources as we're not actually building.
    As a result build will have a correct suffix `pyodide_2025_0_wasm32.whl` that's pyodide is currently using and this is pyodide is plan to use when it's going to release to PyPI, see https://peps.python.org/pep-0783/

--- pyodide/setup.py
+++ pyodide/setup.py
@@ -1,11 +1,21 @@
-from setuptools import setup, find_packages
+from setuptools import setup, Extension, find_packages
 
-setup(name='ifcopenshell',
-      version='0.8.0',
-      description='IfcOpenShell is an open source (LGPL) software library for working with the Industry Foundation Classes (IFC) file format.',
-      author='Thomas Krijnen',
-      author_email='thomas@aecgeeks.com',
-      url='http://ifcopenshell.org',
-      packages=find_packages(),
-      package_data={'ifcopenshell': ['util/schema/*.json', 'util/schema/*.ifc'], '': ['*.so', '*.json', '*.ifc']},
+setup(
+    name="ifcopenshell",
+    version="0.8.0",
+    description=(
+        "IfcOpenShell is an open source (LGPL) software library "
+        "for working with the Industry Foundation Classes (IFC) file format."
+    ),
+    author="Thomas Krijnen",
+    author_email="thomas@aecgeeks.com",
+    url="https://ifcopenshell.org",
+    packages=["ifcopenshell"],
+    package_data={
+        # "*.so" is needed to include prebuilt binary extension. Otherwise it would try to build it and fail.
+        "ifcopenshell": ["util/schema/*.json", "util/schema/*.ifc", "*.so"],
+        "": ["*.json", "*.ifc"],
+    },
+    # Has to provide extension to get the correct wheel suffix.
+    ext_modules=[Extension("ifcopenshell._ifcopenshell_wrapper", sources=[])],
 )
