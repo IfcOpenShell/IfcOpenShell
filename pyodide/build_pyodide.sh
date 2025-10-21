61b03dc60c16f7a2372e3717c521fdce9e47ab2d
Andrej730 <azhilenkov@gmail.com>
2025-10-21 12:42:56 +0500

    build_pyodide - pack cached dependencies

--- pyodide/build_pyodide.sh
+++ pyodide/build_pyodide.sh
@@ -9,4 +9,7 @@ cp IfcOpenShell/pyodide/meta.yaml packages/ifcopenshell
 export PYODIDE_ROOT=/src/pyodide
 # Ensure emsdk tools are in PATH.
 export PATH=$PYODIDE_ROOT/emsdk/emsdk:$PYODIDE_ROOT/emsdk/emsdk/node/22.16.0_64bit/bin:$PYODIDE_ROOT/emsdk/emsdk/upstream/emscripten:$PATH
+# Use custom build ifcopenshell directory in build-all to make caching simpler
+# Otherwise pyodide build path typically includes package version, so cached cmake configs might break.
+export BUILD_DIR=/src/ifcopenshell_build
 pyodide build-recipes ifcopenshell --install
