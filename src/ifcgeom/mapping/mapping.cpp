9e2c7e653f3f0e99329c12bb4d21fe349b04b2fb
Thomas Krijnen <t.krijnen@gmail.com>
2025-10-22 14:37:10 +0200

    Commit suggestion in #7276

--- src/ifcgeom/mapping/mapping.cpp
+++ src/ifcgeom/mapping/mapping.cpp
@@ -373,7 +373,7 @@ const IfcUtil::IfcBaseEntity* mapping::get_single_material_association(const Ifc
                 }
 #endif
 
-#ifdef SCHEMA_HAS_IfcMaterialConstituent
+#ifdef SCHEMA_HAS_IfcMaterialConstituentSet
                 if (associated_material->as<IfcSchema::IfcMaterialConstituentSet>()) {
                     IfcSchema::IfcMaterialConstituentSet* constituentset = associated_material->as<IfcSchema::IfcMaterialConstituentSet>();
                     if (settings_.get<settings::LayersetFirst>().value ? constituentset->MaterialConstituents()->get()->size() >= 1 : constituentset->MaterialConstituents()->get()->size() == 1) {
