90ccaf4aab1bf5f05a6b586e01b5cf06a7afc60a
Dion Moult <dion@thinkmoult.com>
2025-10-26 20:46:32 +1100

    Let users manually create automatic references too.

--- src/bonsai/bonsai/core/drawing.py
+++ src/bonsai/bonsai/core/drawing.py
@@ -487,7 +487,8 @@ def sync_references(
             continue
         if (obj := ifc.get_object(element)) and ifc.is_moved(obj):
             drawing_tool.sync_object_placement(obj)
-        reference_element = drawing_tool.get_assigned_product(element)
+        if not (reference_element := drawing_tool.get_assigned_product(element)):
+            continue
         reference_obj = ifc.get_object(reference_element)
         if reference_element not in potential_reference_elements:
             # It was auto created, so it makes sense to auto delete
