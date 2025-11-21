7e48d712a20d94b2e97f0cbeb2bd85fb46804bb4
Andrej730 <azhilenkov@gmail.com>
2025-11-21 17:20:23 +0500

    cmake - fix error building with GLTF_SUPPORT=OFF (487a4f3, #7390)

--- src/serializers/JsonSerializer.cpp
+++ src/serializers/JsonSerializer.cpp
@@ -1,3 +1,24 @@
+/********************************************************************************
+*                                                                              *
+* This file is part of IfcOpenShell.                                           *
+*                                                                              *
+* IfcOpenShell is free software: you can redistribute it and/or modify         *
+* it under the terms of the Lesser GNU General Public License as published by  *
+* the Free Software Foundation, either version 3.0 of the License, or          *
+* (at your option) any later version.                                          *
+*                                                                              *
+* IfcOpenShell is distributed in the hope that it will be useful,              *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of               *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 *
+* Lesser GNU General Public License for more details.                          *
+*                                                                              *
+* You should have received a copy of the Lesser GNU General Public License     *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.         *
+*                                                                              *
+********************************************************************************/
+
+#ifdef WITH_GLTF
+
 #include "JsonSerializer.h"
 
 #include <boost/algorithm/string/case_conv.hpp>
@@ -40,3 +61,5 @@ JsonSerializerFactory::Factory& JsonSerializerFactory::implementations() {
     static JsonSerializerFactory::Factory impl;
     return impl;
 }
+
+#endif
