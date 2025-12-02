cb3b1d92b317b6403ed65f3066c10ba7adde126e
Ryan Schultz <ryan@openingdesign.com>
2025-12-02 11:13:47 -0600

    some old vestige of a bygone era.

--- src/bonsai/bonsai/bim/module/project/ui.py
+++ src/bonsai/bonsai/bim/module/project/ui.py
@@ -664,12 +664,4 @@ class BIM_PT_purge(Panel):
         row.operator("bim.purge_unused_elements_by_class", text="Purge Orphaned", icon="TRASH")
         row.operator("bim.print_unused_elements_stats", text="", icon="INFO")
 
-        if context.active_object and (data := context.active_object.data):
-            mprops = tool.Geometry.get_mesh_props(data)
-            row = layout.row()
-            row.operator("bim.get_representation_ifc_parameters")
-            for index, ifc_parameter in enumerate(mprops.ifc_parameters):
-                row = layout.row(align=True)
-                row.prop(ifc_parameter, "name", text="")
-                row.prop(ifc_parameter, "value", text="")
-                row.operator("bim.update_parametric_representation", icon="FILE_REFRESH", text="").index = index
+
