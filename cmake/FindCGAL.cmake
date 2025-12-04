1165e3a440b01d1df3c40272df568ae094afc35e
Andrej730 <azhilenkov@gmail.com>
2025-12-04 18:39:15 +0500

    cmake - move all cgal related stuff to `CGAL::CGAL` target

--- cmake/FindCGAL.cmake
+++ cmake/FindCGAL.cmake
@@ -9,9 +9,13 @@
 # If input variables are not specified, try to find HDF5 config.
 # Input variables could also be provided as environment variables.
 #
-# Output variables:
-# - `CGAL_INCLUDE_DIR`
+# Output targets:
+# - `CGAL::CGAL`
 #
+
+if(TARGET CGAL::CGAL)
+    return()
+endif()
 
 UNIFY_ENVVARS_AND_CACHE(CGAL_INCLUDE_DIR)
 UNIFY_ENVVARS_AND_CACHE(CGAL_LIBRARY_DIR)
@@ -20,7 +24,21 @@ UNIFY_ENVVARS_AND_CACHE(GMP_LIBRARY_DIR)
 UNIFY_ENVVARS_AND_CACHE(MPFR_INCLUDE_DIR)
 UNIFY_ENVVARS_AND_CACHE(MPFR_LIBRARY_DIR)
 
-if(NOT CGAL_INCLUDE_DIR)
+if(CGAL_INCLUDE_DIR)
+    find_library(libGMP NAMES gmp mpir PATHS ${GMP_LIBRARY_DIR} NO_DEFAULT_PATH)
+    find_library(libMPFR NAMES mpfr PATHS ${MPFR_LIBRARY_DIR} NO_DEFAULT_PATH)
+    if(NOT libGMP)
+        message(FATAL_ERROR "Unable to find GMP library files, aborting")
+    endif()
+    if(NOT libMPFR)
+        message(FATAL_ERROR "Unable to find MPFR library files, aborting")
+    endif()
+
+    add_library(CGAL::CGAL INTERFACE IMPORTED)
+    target_include_directories(CGAL::CGAL INTERFACE "${CGAL_INCLUDE_DIR}")
+    target_include_directories(CGAL::CGAL INTERFACE "${GMP_INCLUDE_DIR}" "${MPFR_INCLUDE_DIR}")
+    target_link_libraries(CGAL::CGAL INTERFACE "${libMPFR}" "${libGMP}")
+else()
     # CGAL is not respecting default Boost_USE_STATIC_LIBS value
     # and sometiems it's getting in the way.
     if(NOT DEFINED Boost_USE_STATIC_LIBS)
@@ -28,8 +46,8 @@ if(NOT CGAL_INCLUDE_DIR)
     else()
         set(CGAL_Boost_USE_STATIC_LIBS "${Boost_USE_STATIC_LIBS}")
     endif()
-    find_package(CGAL CONFIG REQUIRED)
-    if(NOT CGAL_DIR)
+    find_package(CGAL CONFIG)
+    if(NOT CGAL::CGAL)
         message(
             FATAL_ERROR
             "CGAL_SUPPORT enabled, but CGAL_INCLUDE_DIR wasn't provided and CGAL package couldn't be found."
@@ -38,5 +56,5 @@ if(NOT CGAL_INCLUDE_DIR)
     message(STATUS "CGAL: found config at '${CGAL_DIR}'.")
 endif()
 
-add_definitions(-DIFOPSH_WITH_CGAL)
+target_compile_definitions(CGAL::CGAL INTERFACE IFOPSH_WITH_CGAL)
 set(SWIG_DEFINES ${SWIG_DEFINES} -DIFOPSH_WITH_CGAL)
