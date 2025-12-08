a9af9341b9b1f7f693505c7227bcd638edcf39a8
Josef Wienerroither <spacefrog@frogsinspace.at>
2025-12-08 14:43:46 +0100

    chore: update ifcmax cmakelists

---
+++ src/ifcmax/resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by IfcMax.rc
+//
+#define IDS_LIBDESCRIPTION              101
+#define IDS_LIBDESCRIPTION_FREE         102
+#define IDS_SHORT_DESCRIPTION           103
+#define IDS_LONG_DESCRIPTION            104
+#define IDS_AUTHOR_NAME                 105
+#define IDS_COPYRIGHT_MESSAGE 			106
+#define IDS_IMPORT_CANCEL_CAPTION       107
+#define IDS_IMPORT_CANCEL_MESSAGE       108
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
