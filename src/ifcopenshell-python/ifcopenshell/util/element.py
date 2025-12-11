b2ce4a93c582b63876983acaf5f1d73ce5705332
A. R. S. <andreas.schiefer+github@gmail.com>
2025-12-11 10:55:39 +0100

    Fix getting elements by predefined property sets in IFC2X3.

--- src/ifcopenshell-python/ifcopenshell/util/element.py
+++ src/ifcopenshell-python/ifcopenshell/util/element.py
@@ -477,7 +477,7 @@ def get_elements_by_pset(pset: ifcopenshell.entity_instance) -> set[ifcopenshell
     """Retrieve the elements (or element types) that are using the provided property set."""
     is_ifc2x3 = pset.file.schema == "IFC2X3"
     elements = set()
-    if pset.is_a("IfcPropertySet") or pset.is_a("IfcPreDefinedPropertySet") or pset.is_a("IfcElementQuantity"):
+    if pset.is_a("IfcPropertySetDefinition"):
         rels = pset.PropertyDefinitionOf if is_ifc2x3 else pset.DefinesOccurrence
         for rel in rels:
             elements.update(rel.RelatedObjects)
