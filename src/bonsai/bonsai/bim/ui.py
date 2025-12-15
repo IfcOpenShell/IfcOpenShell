478ba705676a20d98fbd60e5bc3a2c52e1ea9b1d
falken10vdl <falken10vdl@gmail.com>
2025-12-15 13:39:46 +0100

    Add preference for mass and time units visibility in project wizard

--- src/bonsai/bonsai/bim/ui.py
+++ src/bonsai/bonsai/bim/ui.py
@@ -684,6 +684,12 @@ class BIM_ADDON_preferences(bpy.types.AddonPreferences):
         default=False,
     )
 
+    mass_time_units_in_wizard: BoolProperty(
+        name="Mass and time units in project wizard",
+        description="Show mass and time units section in the new project wizard panel",
+        default=False,
+    )
+
     if TYPE_CHECKING:
         svg2pdf_command: str
         svg2dxf_command: str
@@ -720,6 +726,7 @@ class BIM_ADDON_preferences(bpy.types.AddonPreferences):
         doc: DocPreferences
         default_parameters: DefaultParameters
         container_hide_show_isolate: bool
+        mass_time_units_in_wizard: bool
 
     def draw(self, context: bpy.types.Context) -> None:
         layout = self.layout
@@ -901,6 +908,7 @@ class BIM_ADDON_preferences(bpy.types.AddonPreferences):
 
     def draw_extras_settings(self, layout: bpy.types.UILayout, context: bpy.types.Context) -> None:
         layout.prop(self, "container_hide_show_isolate")
+        layout.prop(self, "mass_time_units_in_wizard")
 
 
 # Scene panel groups
