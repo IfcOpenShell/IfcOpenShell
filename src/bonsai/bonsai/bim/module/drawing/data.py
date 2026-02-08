dffa3515c0a87d717b66ee0588d3087206de52bf
Dion Moult <dion@thinkmoult.com>
2026-02-08 19:17:12 +1100

    Reimplement sort / reverse / join function to format language, simplify text annotation variables, add tests
    Previously, sort, reverse list, and join functionality was implemented
    as special cases in Bonsai itself. Given that it has usecases
    (especially in material lists, but any sort of list applies) I've moved
    this function into the IOS formatting language.
    
    The IOS formatting language previously wasn't capable of this, but the
    awesome addition by @falken10vdl made the formatting language accept
    queries inline, so that means it can handle lists. I also added tests
    for all the new functions and expression syntax (+-*/ operators).
    
    I simplified the code that gets the evaluated text literal - previously
    it seems to call format() multiple times.

--- src/bonsai/bonsai/bim/module/drawing/data.py
+++ src/bonsai/bonsai/bim/module/drawing/data.py
@@ -365,15 +365,10 @@ class DecoratorData:
 
         for literal in literals:
             literal_value = literal.Literal
-            try:
-                eval_value = cls.evaluate_formatting_expressions(literal_value, product)
-                current_value = tool.Drawing.replace_text_literal_variables(eval_value, product)
-            except Exception:
-                current_value = literal_value
             literal_data = {
                 "Literal": literal_value,
                 "BoxAlignment": literal.BoxAlignment,
-                "CurrentValue": current_value,
+                "CurrentValue": tool.Drawing.replace_text_literal_variables(literal_value, product),
             }
             literals_data.append(literal_data)
 
@@ -398,21 +393,6 @@ class DecoratorData:
             return assigned_product
 
         return element
-
-    @classmethod
-    def evaluate_formatting_expressions(cls, text: str, element=None) -> str:
-        """Evaluate formatting expressions wrapped in backticks using ifcopenshell.util.selector.format, always passing element context"""
-        import re
-
-        def evaluate_expression(match):
-            try:
-                expression = match.group(1)
-                result = ifcopenshell.util.selector.format(expression, element)
-                return str(result)
-            except Exception as e:
-                return match.group(0)
-
-        return re.sub(r"``([^`]+)``", evaluate_expression, text)
 
     @classmethod
     def get_element_value_by_key(cls, element: ifcopenshell.entity_instance, key: str):
