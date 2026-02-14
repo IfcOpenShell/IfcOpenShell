2cad70e2c2a0792db37e9b056f36fb5d345de827
falken10vdl <falken10vdl@gmail.com>
2026-02-14 11:06:06 +0100

    Remove geolocation option from AppendLibraryElement operator

--- src/bonsai/bonsai/bim/module/project/operator.py
+++ src/bonsai/bonsai/bim/module/project/operator.py
@@ -600,7 +600,6 @@ class AppendLibraryElement(bpy.types.Operator, tool.Ifc.Operator):
     definition: bpy.props.IntProperty()
     prop_index: bpy.props.IntProperty()
     assume_unique_by_name: bpy.props.BoolProperty(name="Assume Unique By Name", default=True, options={"SKIP_SAVE"})
-    use_geolocation: bpy.props.BoolProperty(name="Use Geolocation", default=True, options={"SKIP_SAVE"})
 
     if TYPE_CHECKING:
         definition: int
@@ -630,7 +629,6 @@ class AppendLibraryElement(bpy.types.Operator, tool.Ifc.Operator):
             library=library_file,
             element=library_file.by_id(self.definition),
             assume_asset_uniqueness_by_name=self.assume_unique_by_name,
-            use_geolocation=self.use_geolocation,
         )
         if not element:
             return {"FINISHED"}
