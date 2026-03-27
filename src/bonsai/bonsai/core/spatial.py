IfcOpenShell/IfcOpenShell

commit ff35666ad9284a9259c887c687fe3d418e793b86

Reimplement feature to reassign inherited containers if you select a child element
This reimplements @theoryshaw 's commit 9adbd4 but has a few upgrades:

 - Considers all parent / child relationships, not just aggregates
 - Puts business logic in core where it belongs and tool code in tool
 - Uses existing utils where possible like get_decomposition
 - Does not use name based collection checking which is fragile
 - Reuses tool.Collector
 - Makes container assignment handle the API's capability to do things
in bulk instead of one by one in a loop, so it's faster
 - Tests

src/bonsai/bonsai/core/spatial.py
@@ -52,15 +52,22 @@ def assign_container(
     collector: type[tool.Collector],
     spatial: type[tool.Spatial],
     container: ifcopenshell.entity_instance,
-    element_obj: Optional[bpy.types.Object] = None,
+    objs: Optional[bpy.types.Object] = None,
 ) -> Union[ifcopenshell.entity_instance, None]:
-    if not spatial.can_contain(container, element_obj):
-        return
-    assert element_obj  # Type checker.
-    rel = ifc.run("spatial.assign_container", products=[ifc.get_entity(element_obj)], relating_structure=container)
-    spatial.disable_editing(element_obj)
-    collector.assign(element_obj)
-    return rel
+    root_elements = set()
+    all_elements = set()
+    for obj in objs:
+        if not (element := ifc.get_entity(obj)):
+            continue
+        root_element = spatial.get_root_element(element)
+        root_elements.add(root_element)
+        spatial.disable_editing(obj)
+        all_elements.add(root_element)
+        all_elements.update(spatial.get_decomposition(root_element))
+    if products := [e for e in root_elements if spatial.can_contain(container, root_element)]:
+        ifc.run("spatial.assign_container", products=products, relating_structure=container)
+    for element in all_elements:
+        collector.assign(ifc.get_object(element))
 
 
 def enable_editing_container(spatial: type[tool.Spatial], obj: bpy.types.Object) -> None:
@@ -98,7 +105,7 @@ def copy_to_container(
         copied_obj = spatial.duplicate_object_and_data(obj)
         spatial.set_relative_object_matrix(copied_obj, to_container_obj, matrix)
         result_objs.append(spatial.run_root_copy_class(obj=copied_obj))
-        spatial.run_spatial_assign_container(container=to_container, element_obj=copied_obj)
+        spatial.run_spatial_assign_container(container=to_container, objs=[copied_obj])
     spatial.disable_editing(obj)
     return result_objs
 

src/bonsai/bonsai/tool/spatial.py
@@ -74,9 +74,25 @@ class Spatial(bonsai.core.tool.Spatial):
         return bpy.context.scene.BIMGridProperties
 
     @classmethod
-    def can_contain(cls, container: ifcopenshell.entity_instance, element_obj: Union[bpy.types.Object, None]) -> bool:
-        if not (element := tool.Ifc.get_entity(element_obj)):
-            return False
+    def get_decomposition(cls, element: ifcopenshell.entity_instance) -> list(ifcopenshell.entity_instance):
+        return ifcopenshell.util.element.get_decomposition(element)
+
+    @classmethod
+    def get_root_element(cls, element: ifcopenshell.entity_instance) -> ifcopenshell.entity_instance:
+        while True:
+            if parent := (
+                ifcopenshell.util.element.get_aggregate(element)
+                or ifcopenshell.util.element.get_nest(element)
+                or ifcopenshell.util.element.get_filled_void(element)
+                or ifcopenshell.util.element.get_voided_element(element)
+            ):
+                element = parent
+            else:
+                break
+        return element
+
+    @classmethod
+    def can_contain(cls, container: ifcopenshell.entity_instance, element: ifcopenshell.entity_instance) -> bool:
         if tool.Ifc.get_schema() == "IFC2X3":
             if not container.is_a("IfcSpatialStructureElement"):
                 return False
@@ -147,10 +163,10 @@ class Spatial(bonsai.core.tool.Spatial):
 
     @classmethod
     def run_spatial_assign_container(
-        cls, container: ifcopenshell.entity_instance, element_obj: bpy.types.Object
+        cls, container: ifcopenshell.entity_instance, objs: list[bpy.types.Object]
     ) -> Union[ifcopenshell.entity_instance, None]:
         return bonsai.core.spatial.assign_container(
-            tool.Ifc, tool.Collector, tool.Spatial, container=container, element_obj=element_obj
+            tool.Ifc, tool.Collector, tool.Spatial, container=container, objs=objs
         )
 
     @classmethod

src/bonsai/bonsai/bim/module/aggregate/operator.py
@@ -224,7 +224,7 @@ class BIM_OT_add_aggregate(bpy.types.Operator, tool.Ifc.Operator):
                     tool.Collector,
                     tool.Spatial,
                     container=current_container,
-                    element_obj=aggregate,
+                    objs=[aggregate],
                 )
             core.assign_object(tool.Ifc, tool.Aggregate, tool.Collector, relating_obj=aggregate, related_obj=obj)
 

src/bonsai/bonsai/bim/module/geometry/operator.py
@@ -1730,7 +1730,7 @@ class RefreshLinkedAggregate(bpy.types.Operator, tool.Ifc.Operator):
                         tool.Collector,
                         tool.Spatial,
                         container=original_data[matching_group_id][index]["Container"],
-                        element_obj=obj,
+                        objs=[obj],
                     )
                     for part in ifcopenshell.util.element.get_parts(tool.Ifc.get_entity(obj)):
                         tool.Collector.assign(tool.Ifc.get_object(part))

src/bonsai/bonsai/bim/module/model/product.py
@@ -470,7 +470,7 @@ class AddOccurrence(bpy.types.Operator, tool.Ifc.Operator):
                 parent = ifcopenshell.util.element.get_container(building_element)
                 if parent:
                     bonsai.core.spatial.assign_container(
-                        tool.Ifc, tool.Collector, tool.Spatial, container=parent, element_obj=obj
+                        tool.Ifc, tool.Collector, tool.Spatial, container=parent, objs=[obj]
                     )
 
         # set occurrences properties for the types defined with modifiers
@@ -493,7 +493,7 @@ class AddOccurrence(bpy.types.Operator, tool.Ifc.Operator):
         else:
             if self.container_obj:
                 bonsai.core.spatial.assign_container(
-                    tool.Ifc, tool.Collector, tool.Spatial, container=self.container, element_obj=obj
+                    tool.Ifc, tool.Collector, tool.Spatial, container=self.container, objs=[obj]
                 )
                 if props.rl_mode == "BOTTOM":
                     obj.location.z = self.container_obj.location.z - tool.Blender.get_object_bounding_box(obj)["min_z"]

src/bonsai/bonsai/bim/module/spatial/operator.py
@@ -171,28 +171,9 @@ class AssignContainer(bpy.types.Operator, tool.Ifc.Operator):
         else:
             return
 
-        objs: list[bpy.types.Object] = []
-        # In IFC element can be either contained of aggregated,
-        # tehrefore we skip aggregated elements here to prevent confusion.
-        # Can't handle it in `poll` since user might just select bunch of elements
-        # and try to assign a container to them
-        # and excluding aggregates because of the `poll` failing might get awkward.
-        skipped_aggregates = 0
-        for obj in tool.Blender.get_selected_objects():
-            if not (element := tool.Ifc.get_entity(obj)):
-                continue
-            if ifcopenshell.util.element.get_aggregate(element):
-                skipped_aggregates += 1
-                continue
-            objs.append(obj)
-
-        for element_obj in objs:
-            core.assign_container(tool.Ifc, tool.Collector, tool.Spatial, container=container, element_obj=element_obj)
-
-        aggregates_msg = ""
-        if skipped_aggregates:
-            aggregates_msg = f" {skipped_aggregates} aggregated elements skipped."
-        self.report({"INFO"}, f"{len(objs)} elements assigned.{aggregates_msg}")
+        core.assign_container(
+            tool.Ifc, tool.Collector, tool.Spatial, container=container, objs=tool.Blender.get_selected_objects()
+        )
 
 
 class EnableEditingContainer(bpy.types.Operator):

src/bonsai/bonsai/bim/module/spatial/prop.py
@@ -175,8 +175,8 @@ def poll_container_obj(self: "BIMObjectSpatialProperties", container_obj: bpy.ty
     obj = self.id_data
     if (
         (container := tool.Ifc.get_entity(container_obj))
-        and (tool.Ifc.get_entity(obj))
-        and tool.Spatial.can_contain(container, obj)
+        and (element := tool.Ifc.get_entity(obj))
+        and tool.Spatial.can_contain(container, element)
     ):
         return True
     return False

src/bonsai/bonsai/core/tool.py
@@ -954,12 +954,14 @@ class Spatial:
     def get_object_matrix(cls, obj): pass
     def get_relative_object_matrix(cls, target_obj, relative_to_obj): pass
     def get_selected_product_types(cls): pass
+    def get_root_element(cls, element): pass
+    def get_decomposition(cls, element): pass
     def get_selected_products(cls): pass
     def import_spatial_decomposition(cls): pass
     def import_spatial_element(cls, element, level_index): pass
     def load_contained_elements(cls): pass
     def run_root_copy_class(cls, obj): pass
-    def run_spatial_assign_container(cls, container, element_obj): pass
+    def run_spatial_assign_container(cls, container, objs): pass
     def run_spatial_import_spatial_decomposition(cls): pass
     def select_object(cls, obj): pass
     def select_products(cls, products, unhide=False): pass

commit e8ab30ab3ed5e7776dd5ac7b21fec475261ef65c

Fix for #7861 and #7801: Fix error in assign_container for unloaded elements
Decomposed elements collected via get_decomposition may not have a
corresponding Blender object if they are not loaded into the scene.
Guard against None before calling collector.assign.

Regression introduced in ff35666ad.

Generated with the assistance of an AI coding tool.

src/bonsai/bonsai/core/spatial.py
@@ -67,7 +67,8 @@ def assign_container(
     if products := [e for e in root_elements if spatial.can_contain(container, root_element)]:
         ifc.run("spatial.assign_container", products=products, relating_structure=container)
     for element in all_elements:
-        collector.assign(ifc.get_object(element))
+        if obj := ifc.get_object(element):
+            collector.assign(obj)
 
 
 def enable_editing_container(spatial: type[tool.Spatial], obj: bpy.types.Object) -> None: