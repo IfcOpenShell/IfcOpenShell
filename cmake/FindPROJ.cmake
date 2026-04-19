046ceb452a664f1c39e8abf51b07b11d332df239
Thomas Krijnen <t.krijnen@gmail.com>
2026-04-19 12:32:18 +0200

    Tighten scope of cmake vars and dirs

--- cmake/FindPROJ.cmake
+++ cmake/FindPROJ.cmake
@@ -6,7 +6,7 @@
 # Input variables could also be provided as environment variables.
 #
 # Output targets:
-# - `PROJ::proj`
+# - `proj::proj`
 #
 
 # To avoid cyclic calls to this file
@@ -34,10 +34,12 @@ if((NOT PROJ_INCLUDE_DIR AND NOT PROJ_LIBRARIES))
             message(FATAL_ERROR "Unable to find PROJ libraries in: ${PROJ_LIBRARY_DIR}")
         endif()
 
-        add_library(PROJ::proj INTERFACE IMPORTED)
-        target_include_directories(PROJ::proj INTERFACE "${PROJ_INCLUDE_DIR}")
-        target_link_libraries(PROJ::proj INTERFACE ${PROJ_LIBRARIES})
-        target_link_directories(PROJ::proj INTERFACE "${PROJ_LIBRARY}")
+        if(NOT TARGET proj::proj)
+            add_library(proj::proj INTERFACE IMPORTED)
+            target_include_directories(proj::proj INTERFACE "${PROJ_INCLUDE_DIR}")
+            target_link_libraries(proj::proj INTERFACE ${PROJ_LIBRARIES})
+            target_link_directories(proj::proj INTERFACE "${PROJ_LIBRARY}")
+        endif()
     endif()
 else()
     find_library(PROJ_LIBRARY NAMES proj PATHS ${PROJ_LIBRARY_DIR})
@@ -50,7 +52,11 @@ else()
 
     set(PROJ_INCLUDE_DIR ${PROJ_INCLUDE_DIR} CACHE FILEPATH "PROJ header files")
     message(STATUS "Looking for PROJ include files in: ${PROJ_INCLUDE_DIR}")
-    include_directories(${PROJ_INCLUDE_DIR})
+    if(NOT TARGET proj::proj)
+        add_library(proj::proj INTERFACE IMPORTED)
+        target_include_directories(proj::proj INTERFACE "${PROJ_INCLUDE_DIR}")
+        target_link_libraries(proj::proj INTERFACE ${PROJ_LIBRARIES})
+    endif()
 endif()
 
 list(PREPEND CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR})
