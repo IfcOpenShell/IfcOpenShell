7a901c1fce6b2fdef90c3d723d4f1ac72bfb97e5
Thomas Krijnen <t.krijnen@gmail.com>
2026-05-08 15:00:30 +0200

    Reduce log noise on materials without styles #7947

--- src/ifcgeom/mapping/mapping.cpp
+++ src/ifcgeom/mapping/mapping.cpp
@@ -578,6 +578,7 @@ taxonomy::ptr mapping::map_impl(const IfcSchema::IfcMaterial& material) {
             }
             // Check if it's failed or just some unsupported case.
             if (failed_on_purpose_.find(styled_item) == failed_on_purpose_.end()) {
+                failed_on_purpose_.insert(material);
                 return nullptr;
             }
             logger::warning("Skipping unsupported material style for material: ", material);
@@ -585,6 +586,7 @@ taxonomy::ptr mapping::map_impl(const IfcSchema::IfcMaterial& material) {
     }
 
     // When material does not have a representation we don't create a style from it
+    failed_on_purpose_.insert(material);
     return nullptr;
 
     /*
