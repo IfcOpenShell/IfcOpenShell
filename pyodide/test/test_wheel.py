424e70ac86b9de6506551a3eb11991173343ab4d
Thomas Krijnen <t.krijnen@gmail.com>
2026-05-10 20:57:46 +0200

    Remove svgfill test in pyodide wheel test

--- pyodide/test/test_wheel.py
+++ pyodide/test/test_wheel.py
@@ -24,12 +24,5 @@ def test_ifcopenshell_import(selenium):
         wall.Name = "Test"
         assert wall.Name == "Test", f"Entity name wasn't changed: {{wall}}"
         print(wall)
-
-        wrapper = ifcopenshell.ifcopenshell_wrapper
-        svg = '<svg xmlns="http://www.w3.org/2000/svg"><g><path d="M 0 0 L 1 0 L 0 1 Z"/></g></svg>'
-        segments = wrapper.svg_to_line_segments(svg, None)
-        polygons = wrapper.line_segments_to_polygons(wrapper.EXACT_PREDICATES, 0.0, segments)
-        assert len(polygons) == 1, f"Unexpected svgfill polygon groups: {{polygons}}"
-        assert len(polygons[0]) == 1, f"Unexpected svgfill polygons: {{polygons}}"
         """
     )
