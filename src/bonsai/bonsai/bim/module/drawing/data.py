d2f18709cbdc80b99a8008c92962a05df5625251
Ryan Schultz <ryan@openingdesign.com>
2026-05-15 10:20:02 -0500

    Closes #8063: ordinate dimensioning
    Generated with the assistance of an AI coding tool.

--- src/bonsai/bonsai/bim/module/drawing/data.py
+++ src/bonsai/bonsai/bim/module/drawing/data.py
@@ -800,6 +800,7 @@ class DecoratorData:
         show_description_only = pset_data.get("ShowDescriptionOnly", False)
         suppress_zero_inches = pset_data.get("SuppressZeroInches", False)
         suppress_zero_feet = pset_data.get("SuppressZeroFeet", False)
+        is_ordinate = pset_data.get("IsOrdinate", False)
         text_prefix = pset_data.get("TextPrefix", None) or ""
         text_suffix = pset_data.get("TextSuffix", None) or ""
         custom_units = list(pset_data.get("CustomUnit", None) or [])
@@ -810,6 +811,7 @@ class DecoratorData:
             "show_description_only": show_description_only,
             "suppress_zero_inches": suppress_zero_inches,
             "suppress_zero_feet": suppress_zero_feet,
+            "is_ordinate": is_ordinate,
             "text_prefix": text_prefix,
             "text_suffix": text_suffix,
             "fill_bg": fill_bg,
