508b99cb731af604e5adab921dacb58793cd4c91
Geert Hesselink <54070862+Ghesselink@users.noreply.github.com>
2026-05-18 22:17:45 +0200

    Fix lint failures and add missing pyparsing dependency (#8048)
    * unblock voxel schema loading, add test for express
    
    * Apply black formatting
    
    * Fix lint failures and add missing pyparsing dependency
    
    * align ty -> 0.0.34

--- src/bonsai/test/tool/test_cost.py
+++ src/bonsai/test/tool/test_cost.py
@@ -17,19 +17,19 @@
 # along with Bonsai.  If not, see <http://www.gnu.org/licenses/>.
 
 
-import test.bim.bootstrap
 import ifcopenshell.api.cost
 
 import bonsai.core.tool
 import bonsai.tool as tool
 import test.bim.bootstrap
+from bonsai.tool.cost import Cost as subject
 from test.bim.bootstrap import NewFile
 
-from bonsai.tool.cost import Cost as subject
 
 class TestImplementsTool(NewFile):
     def test_run(self):
         assert isinstance(subject(), bonsai.core.tool.Cost)
+
 
 class TestDisableEditingCostItemParent(NewFile):
     def test_avoid_recursion_error(newfile, monkeypatch):
@@ -39,11 +39,7 @@ class TestDisableEditingCostItemParent(NewFile):
                 self.active_cost_item_id = 5
 
         props = DummyProps()
-        monkeypatch.setattr(
-            "bonsai.tool.Cost.get_cost_props",
-            lambda: props
-        )
+        monkeypatch.setattr("bonsai.tool.Cost.get_cost_props", lambda: props)
         subject.disable_editing_cost_item_parent()
         assert props.active_cost_item_id == 0
         assert props.change_cost_item_parent is not False
-
