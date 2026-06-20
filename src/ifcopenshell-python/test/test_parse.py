312be203c9cbdae0b541e6db445232a4e98fa064
Thomas Krijnen <t.krijnen@gmail.com>
2026-06-20 21:30:31 +0200

    SYN004 test case

---
+++ src/ifcopenshell-python/test/test_parse.py
@@ -0,0 +1,19 @@
+import ifcopenshell
+
+def test_skip_over_non_entity_instance():
+    data = """
+ISO-10303-21;
+HEADER;
+FILE_DESCRIPTION((''),'2;1');
+FILE_NAME('','',(''),(''),'','','');
+FILE_SCHEMA(('IFC2X3'));
+ENDSEC;
+DATA;
+#1=IFCLENGTHMEASURE(0.1);
+#5=IFCCARTESIANPOINT((0.,0.));
+ENDSEC;
+END-ISO-10303-21;
+"""
+    f = ifcopenshell.file.from_string(data)
+    print(ifcopenshell.get_log())
+    f.by_id(5)
