IfcOpenShell/IfcOpenShell

commit b35f99e63f75510a3c09e567cc14d9f460ee5092

ty: detect unresolved references

src/bonsai/bonsai/tool/polyline.py
@@ -168,6 +168,7 @@ class Polyline(bonsai.core.tool.Polyline):
         distance = (mouse_vector - last_point).length
         if distance < 0:
             return
+        angle, orientation_angle, angle_round_threshold = None, None
         if distance > 0:
             angle = tool.Cad.angle_3_vectors(
                 second_to_last_point, last_point, mouse_vector, new_angle=None, degrees=True
@@ -188,6 +189,7 @@ class Polyline(bonsai.core.tool.Polyline):
             angle = 0
             orientation_angle = 0
         if input_ui:
+            assert angle is not None and orientation_angle is not None and angle_round_threshold is not None
             if should_round:
                 angle_snap = tool.Snap.get_angle_snap_value(context)
                 angle = angle_snap * round(angle / angle_snap) if distance < angle_round_threshold else angle

commit 21ea58b0e6ecdca2ea417df31e15de2e4857397f

Fix Bonsai polyline not enough values to unpack error
Typo was introduced in b35f99e

src/bonsai/bonsai/tool/polyline.py
@@ -168,7 +168,7 @@ class Polyline(bonsai.core.tool.Polyline):
         distance = (mouse_vector - last_point).length
         if distance < 0:
             return
-        angle, orientation_angle, angle_round_threshold = None, None
+        angle, orientation_angle, angle_round_threshold = None, None, None
         if distance > 0:
             angle = tool.Cad.angle_3_vectors(
                 second_to_last_point, last_point, mouse_vector, new_angle=None, degrees=True