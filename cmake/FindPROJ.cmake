c9e49c81861b2160dfdb5c0f50ae97ec75f7d08f
Andrej730 <azhilenkov@gmail.com>
2026-09-18 18:42:22 +0500

    cmake: pin minimum proj version

--- cmake/FindPROJ.cmake
+++ cmake/FindPROJ.cmake
@@ -30,9 +30,24 @@ else()
     find_path(PROJ_INCLUDE_DIR proj.h PATHS ${PROJ_INCLUDE_DIR} /usr/include/proj REQUIRED)
     message(STATUS "Found PROJ include files in: ${PROJ_INCLUDE_DIR}")
 
+    file(STRINGS ${PROJ_INCLUDE_DIR}/proj.h PROJ_MAJOR REGEX "#define PROJ_VERSION_MAJOR.*")
+    string(REGEX MATCH "[0-9]+" PROJ_MAJOR ${PROJ_MAJOR})
+    file(STRINGS ${PROJ_INCLUDE_DIR}/proj.h PROJ_MINOR REGEX "#define PROJ_VERSION_MINOR.*")
+    string(REGEX MATCH "[0-9]+" PROJ_MINOR ${PROJ_MINOR})
+    file(STRINGS ${PROJ_INCLUDE_DIR}/proj.h PROJ_PATCH REGEX "#define PROJ_VERSION_PATCH.*")
+    string(REGEX MATCH "[0-9]+" PROJ_PATCH ${PROJ_PATCH})
+    set(PROJ_VERSION "${PROJ_MAJOR}.${PROJ_MINOR}.${PROJ_PATCH}")
+    message(STATUS "Found PROJ version: ${PROJ_VERSION}")
+
     find_library(PROJ_LIBRARY NAMES proj PATHS ${PROJ_LIBRARY_DIR} /usr/lib/x86_64-linux-gnu REQUIRED)
     message(STATUS "PROJ libraries ${PROJ_LIBRARY} found in: ${PROJ_LIBRARY_DIR}")
     set(PROJ_LIBRARIES ${PROJ_LIBRARY})
+
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(PROJ
+        REQUIRED_VARS PROJ_INCLUDE_DIR PROJ_LIBRARY
+        VERSION_VAR PROJ_VERSION
+    )
 
     if(NOT TARGET proj::proj)
         add_library(proj::proj INTERFACE IMPORTED)
