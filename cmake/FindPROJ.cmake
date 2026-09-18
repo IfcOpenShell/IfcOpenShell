f54213fd7b3bcbed1623f42ece57b5093e5a4f6a
Andrej730 <azhilenkov@gmail.com>
2026-09-18 18:57:04 +0500

    cmake: simplify FindPROJ

--- cmake/FindPROJ.cmake
+++ cmake/FindPROJ.cmake
@@ -1,7 +1,7 @@
 #
 # Input variables:
 # - `PROJ_INCLUDE_DIR`
-# - `PROJ_LIBRARIES`
+# - `PROJ_LIBRARY_DIR`
 # If input variables are not specified, try to find PROJ config.
 # Input variables could also be provided as environment variables.
 #
@@ -13,56 +13,32 @@
 list(REMOVE_ITEM CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR})
 
 UNIFY_ENVVARS_AND_CACHE(PROJ_INCLUDE_DIR)
-UNIFY_ENVVARS_AND_CACHE(PROJ_LIBRARIES)
+UNIFY_ENVVARS_AND_CACHE(PROJ_LIBRARY_DIR)
 
-if((NOT PROJ_INCLUDE_DIR AND NOT PROJ_LIBRARIES))
+if(NOT PROJ_INCLUDE_DIR AND NOT PROJ_LIBRARY_DIR)
     find_package(PROJ QUIET CONFIG)
+endif()
 
-    if(PROJ_FOUND)
-        # cmake configs only define `PROJ::proj`.
-        # Guarded since find_package(PROJ) is invoked once per serializer plugin,
-        # and re-defining the alias on a later call would error out.
-        if(NOT TARGET proj::proj)
-            add_library(proj::proj ALIAS PROJ::proj)
-        endif()
-    else()
-        find_path(PROJ_INCLUDE_DIR proj.h PATHS /usr/include/proj REQUIRED)
-        if(PROJ_INCLUDE_DIR)
-            message(STATUS "Found PROJ include files in: ${PROJ_INCLUDE_DIR}")
-        else()
-            message(FATAL_ERROR "Unable to find PROJ include directory, specify PROJ_INCLUDE_DIR manually.")
-        endif()
-
-        find_library(PROJ_LIBRARY NAMES proj PATHS /usr/lib/x86_64-linux-gnu)
-        if(PROJ_LIBRARY)
-            message(STATUS "PROJ libraries ${PROJ_LIBRARY} found in: ${PROJ_LIBRARY_DIR}")
-            set(PROJ_LIBRARIES ${PROJ_LIBRARY})
-        else()
-            message(FATAL_ERROR "Unable to find PROJ libraries in: ${PROJ_LIBRARY_DIR}")
-        endif()
-
-        if(NOT TARGET proj::proj)
-            add_library(proj::proj INTERFACE IMPORTED)
-            target_include_directories(proj::proj INTERFACE "${PROJ_INCLUDE_DIR}")
-            target_link_libraries(proj::proj INTERFACE ${PROJ_LIBRARIES})
-            target_link_directories(proj::proj INTERFACE "${PROJ_LIBRARY}")
-        endif()
+if(PROJ_FOUND)
+    # cmake configs only define `PROJ::proj`.
+    # Guarded since find_package(PROJ) is invoked once per serializer plugin,
+    # and re-defining the alias on a later call would error out.
+    if(NOT TARGET proj::proj)
+        add_library(proj::proj ALIAS PROJ::proj)
     endif()
 else()
-    find_library(PROJ_LIBRARY NAMES proj PATHS ${PROJ_LIBRARY_DIR})
-    if(PROJ_LIBRARY)
-        message(STATUS "PROJ libraries ${PROJ_LIBRARY} found in: ${PROJ_LIBRARY_DIR}")
-        set(PROJ_LIBRARIES ${PROJ_LIBRARY})
-    else()
-        message(FATAL_ERROR "Unable to find PROJ libraries in: ${PROJ_LIBRARY_DIR}")
-    endif()
+    find_path(PROJ_INCLUDE_DIR proj.h PATHS ${PROJ_INCLUDE_DIR} /usr/include/proj REQUIRED)
+    message(STATUS "Found PROJ include files in: ${PROJ_INCLUDE_DIR}")
 
-    set(PROJ_INCLUDE_DIR ${PROJ_INCLUDE_DIR} CACHE FILEPATH "PROJ header files")
-    message(STATUS "Looking for PROJ include files in: ${PROJ_INCLUDE_DIR}")
+    find_library(PROJ_LIBRARY NAMES proj PATHS ${PROJ_LIBRARY_DIR} /usr/lib/x86_64-linux-gnu REQUIRED)
+    message(STATUS "PROJ libraries ${PROJ_LIBRARY} found in: ${PROJ_LIBRARY_DIR}")
+    set(PROJ_LIBRARIES ${PROJ_LIBRARY})
+
     if(NOT TARGET proj::proj)
         add_library(proj::proj INTERFACE IMPORTED)
         target_include_directories(proj::proj INTERFACE "${PROJ_INCLUDE_DIR}")
         target_link_libraries(proj::proj INTERFACE ${PROJ_LIBRARIES})
+        target_link_directories(proj::proj INTERFACE "${PROJ_LIBRARY}")
     endif()
 endif()
 
