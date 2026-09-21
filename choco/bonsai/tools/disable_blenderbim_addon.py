d08b177b326c5fc61070af71ff6b29333db37096
Andrej730 <azhilenkov@gmail.com>
2026-09-21 14:16:47 +0500

    ruff-format: remove mostly historical exceptions

--- choco/bonsai/tools/disable_blenderbim_addon.py
+++ choco/bonsai/tools/disable_blenderbim_addon.py
@@ -1,7 +1,6 @@
 import bpy
 
-bpy.ops.preferences.addon_disable(module='blenderbim')
+bpy.ops.preferences.addon_disable(module="blenderbim")
 bpy.ops.wm.save_userpref()
 
 print("disabled blenderbim addon successfully.")
-
--- conda/update_version_init.py
+++ conda/update_version_init.py
@@ -16,18 +16,11 @@ def update_version(file_path: str, version: str) -> None:
 
     print(f"Updated version in {file_path} to {version}")
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Update the version string in a Python file.")
-    parser.add_argument(
-        "version",
-        type=str,
-        help="The version string to replace '0.0.0' with (e.g., '1.2.3')."
-    )
-    parser.add_argument(
-        "file",
-        type=str,
-        help="The path to the __init__.py file where the version will be updated."
-    )
+    parser.add_argument("version", type=str, help="The version string to replace '0.0.0' with (e.g., '1.2.3').")
+    parser.add_argument("file", type=str, help="The path to the __init__.py file where the version will be updated.")
 
     args = parser.parse_args()
     update_version(args.file, args.version)
