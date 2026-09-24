4febb88cccdfdf25bf9df127d203d84e3d850bcb
Andrej730 <azhilenkov@gmail.com>
2026-09-24 10:24:15 +0500

    build-all: drop stale apple toolset comments
    Since we're moved far past those toolset versions.

--- nix/build-all.py
+++ nix/build-all.py
@@ -479,9 +479,6 @@ if WASM:
 
 TOOLSET = None
 if APPLE:
-    # C++11 features used in OCCT 7+ need a more recent stdlib
-    # TOOLSET = "10.9" if USE_OCCT else "10.6"
-    # /Users/runner/work/IfcOpenShell/IfcOpenShell/build/Darwin/arm64/10.9/build/rocksdb/cache/clock_cache.cc:732:14: error: aligned allocation function of type 'void *(std::size_t, std::align_val_t)' is only available on macOS 10.13 or newer
     # /Users/runner/work/IfcOpenShell/IfcOpenShell/src/ifcparse/IfcFile.cpp:539:14: error: 'exists' is unavailable: introduced in macOS 10.15
     TOOLSET = "10.15"
 
